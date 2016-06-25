4e48329f7d9995b7919555cabe10857e1db54321
juliansader <julian.sader@gmail.com>
2016-06-25 03:44:16 +0200

    Update js_Split selected notes by drawing a line with the mouse.lua

--- MIDI Editor/js_Split selected notes by drawing a line with the mouse.lua
+++ MIDI Editor/js_Split selected notes by drawing a line with the mouse.lua
@@ -1,6 +1,6 @@
 --[[
- * ReaScript Name:  js_Split notes by drawing a line with the mouse
- * Description:  Split (slice) multiple notes by drawing a "cutting line" with the mouse in the MIDI editor piano roll.
+ * ReaScript Name:  js_Split selected notes by drawing a line with the mouse.lua
+ * Description:  Split (slice) multiple selected notes by drawing a "cutting line" with the mouse in the MIDI editor piano roll.
  *               Notes that intersect the cutting line will be split at the position of intersection.
  *               If snap-to-grid is enabled in the MIDI editor, the notes will be split at the grid.
  *
