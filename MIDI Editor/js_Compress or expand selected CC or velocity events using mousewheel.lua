f7f3655008736b226e57dc813cc36b7f10776eec
juliansader <julian.sader@gmail.com>
2016-07-05 16:27:45 +0200

    Update js_Compress or expand selected CC or velocity events using mousewheel.lua

--- MIDI Editor/js_Compress or expand selected CC or velocity events using mousewheel.lua
+++ MIDI Editor/js_Compress or expand selected CC or velocity events using mousewheel.lua
@@ -48,12 +48,11 @@
  * Licence: GPL v3
  * Forum Thread: 
  * Forum Thread URL: http://forum.cockos.com/showthread.php?t=176878
- * Version: 2.0
+ * Version: 2.01
  * REAPER: 5.20
  * Extensions: SWS/S&M 2.8.3
 ]]
  
-
 --[[
  Changelog:
  * v1.0 (2016-05-15)
