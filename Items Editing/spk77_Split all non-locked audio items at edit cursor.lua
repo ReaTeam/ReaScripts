5228233b118927e8496aa31c0ee37f7da253ba3a
spk1977 <spk1977@users.noreply.github.com>
2016-07-07 20:31:18 +0300

    Add spk77 split scripts

--- Items Editing/spk77_Split all non-locked audio items at edit cursor.lua
+++ Items Editing/spk77_Split all non-locked audio items at edit cursor.lua
@@ -1,6 +1,6 @@
 --[[
-  * ReaScript Name: Split all items at edit cursor
-  * Description: - splits all non-locked items at edit cursor
+  * ReaScript Name: Split all non-locked audio items at edit cursor
+  * Description: - splits all non-locked audio items at edit cursor
   *              - locked items are ignored
   *              - item selection remain untouched
   * Instructions:
@@ -11,8 +11,8 @@
   * Author URI: http://forum.cockos.com/member.php?u=49553
   * Licence: GPL v3
   * Forum Thread: 
-  * Forum Thread URL: http://forum.cockos.com/showthread.php?t=177434
-  * Version: 1.1
+  * Forum Thread URL:
+  * Version: 0.1
   * REAPER:
   * Extensions:
 ]]
@@ -20,9 +20,8 @@
 
 --[[
  Changelog:
- * v1.0 (2015-05-29)
+ * v0.1 (2015-07-07)
     + Initial Release
-    + Locked items are ignored
 ]]  
 
 
@@ -34,7 +33,7 @@ function get_items_under_edit_cursor(cursor_pos, num_items)
     local item = r.GetMediaItem(0, i-1)
     if item ~= nil then
       local take = r.GetActiveTake(item)
-      if take ~= nil and r.GetMediaItemInfo_Value(item, "C_LOCK") == 0.0 then
+      if take ~= nil and not r.TakeIsMIDI(take) and r.GetMediaItemInfo_Value(item, "C_LOCK") == 0.0 then
         local length = r.GetMediaItemInfo_Value(item, "D_LENGTH")
         local pos = r.GetMediaItemInfo_Value(item, "D_POSITION")
         if pos < cursor_pos and pos+length > cursor_pos then
@@ -53,7 +52,7 @@ function split(item_table, split_pos)
     r.SplitMediaItem(item, split_pos)
   end
   r.UpdateArrange()
-  r.Undo_EndBlock("Split all items at edit cursor (ignore locked items)", -1)
+  r.Undo_EndBlock("Split all non-locked audio items at edit cursor", -1)
 end
 
 
