ab90ee028b06a1193fa464f86177cae471b48c0c
juliansader <julian.sader@gmail.com>
2016-07-17 19:09:40 +0200

    Add files via upload

--- MIDI Editor/js_Option - Switching active take sets channel for new events to channel of existing events.lua
+++ MIDI Editor/js_Option - Switching active take sets channel for new events to channel of existing events.lua
@@ -18,7 +18,7 @@
  * Licence: GPL v3
  * Forum Thread: 
  * Forum Thread URL: http://forum.cockos.com/showthread.php?t=176878, http://forum.cockos.com/showthread.php?t=178256
- * Version: 1.0
+ * Version: 1.01
  * REAPER: 5.20
  * Extensions: 
 ]]
@@ -27,6 +27,8 @@
  Changelog:
  * v1.0 (2016-07-15)
     + Initial release
+ * v1.01 (2016-07-17)
+    + Additional tests to verify REAPER's channel values
 ]]
 
 tableTakes = {} -- The active take of each MIDI editor will be stored in this table
@@ -42,13 +44,14 @@ function exit()
 end -- function exit
 
 ----------------------------
-function loopGetSetChannel()
+function loopGetSetTakeChannel()
     local editor = reaper.MIDIEditor_GetActive()
     if editor ~= nil then 
         local take = reaper.MIDIEditor_GetTake(editor)
         if take ~= nil and tableTakes[editor] ~= take then
             tableTakes[editor] = take
-            -- Strangely, if using MIDI_GetEvt, REAPER sometimes returns a msg that is simply a blank string "".
+            -- Strangely, if using MIDI_GetEvt, REAPER sometimes returns a msg that is simply a blank string "",
+            --    or sometimes other weird strings with event types = 0, etc.
             -- Therefore search through 50 events or until an event is found with usable channel info
             --    or until no event is found at index.
             local index = 0
@@ -56,16 +59,23 @@ function loopGetSetChannel()
             repeat
                 returnOK, _, _, _, msg = reaper.MIDI_GetEvt(take, index, true, true, 0, "")
                 index = index + 1
-            until returnOK == false or index == 50 or (returnOK == true and type(msg) == "string" and msg ~= "")
+            until returnOK == false 
+                  or index > 50 -- Don't search more than 50 events
+                  or (returnOK == true 
+                      and type(msg) == "string" 
+                      and msg ~= ""
+                      and msg:len() == 3 
+                      and ((tonumber(string.byte(msg:sub(1,1))))>>4) >= 8 -- MIDI event types are >= 8.  REAPER sometimes returns a 0.
+                     )                    
 
-            if not (returnOK == false or index == 50) then
+            if not (returnOK == false or index > 50) then
                 local newChannel = (tonumber(string.byte(msg:sub(1,1))))&15
                 reaper.MIDIEditor_OnCommand(editor, 40482+newChannel) -- Set channel for new events to 0+newChannel
             end
         end
     end
-    reaper.runloop(loopGetSetChannel)
-end -- function loop GetSetChannel
+    reaper.runloop(loopGetSetTakeChannel)
+end -- function loop GetSetTakeChannel
 
 --------------------------------------------------------------------
 -- Here the code execution starts
@@ -81,4 +91,4 @@ if not (sectionID == nil or ownCommandID == nil or sectionID == -1 or ownCommand
     reaper.RefreshToolbar2(sectionID, ownCommandID)
 end
 
-reaper.runloop(loopGetSetChannel)
+reaper.runloop(loopGetSetTakeChannel)
