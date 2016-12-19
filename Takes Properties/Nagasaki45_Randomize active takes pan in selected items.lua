eac8db760746510f53a5eb0e044818c9a227f376
Tom Gurion <nagasaki45@gmail.com>
2016-12-19 01:19:31 +0000

    New 'Randomize pan of selected items' script (#13)
    * New 'Randomize pan of selected items' script
    
    * Rename script and add author per PR comments
    
    * Update randomize pan script description to script name

---
+++ Takes Properties/Nagasaki45_Randomize active takes pan in selected items.lua
@@ -0,0 +1,12 @@
+-- @description Randomize active takes pan in selected items
+-- @version 0.1
+-- @author Nagasaki45
+
+selected_item_count = reaper.CountSelectedMediaItems(0)
+for i = 0, selected_item_count - 1 do
+    item = reaper.GetSelectedMediaItem(0, i)
+    item_take = reaper.GetActiveTake(item)
+    random_pan = math.random() * 2 - 1
+    reaper.SetMediaItemTakeInfo_Value(item_take, 'D_PAN', random_pan)
+    reaper.UpdateItemInProject(item)
+end
