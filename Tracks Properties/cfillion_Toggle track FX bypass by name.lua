b349630dffe77d1dfb0aefd0abb1425697c6fef6
cfillion <cfillion@users.noreply.github.com>
2016-12-20 02:33:23 -0500

    release cfillion_Toggle track FX bypass by name.lua v1.1

--- Tracks Properties/cfillion_Toggle track FX bypass by name.lua
+++ Tracks Properties/cfillion_Toggle track FX bypass by name.lua
@@ -1,9 +1,10 @@
 -- @description Toggle track FX bypass by name
--- @version 1.0
+-- @version 1.1
+-- @changelog Add track filter feature (by name or selection)
 -- @author cfillion
 -- @link http://forum.cockos.com/showthread.php?t=184623
 -- @screenshot
---   Basic Usage http://i.imgur.com/ryIg1j9.gif
+--   Basic Usage https://i.imgur.com/jVgwbi3.gif
 --   Undo Points https://i.imgur.com/dtNwlsn.png
 -- @about
 --   # Toggle track FX bypass by name
@@ -12,29 +13,54 @@
 --   project. The search is case insensitive. Bypass is toggled for all matching
 --   FXs. Undo points are consolidated into one.
 
-local ok, filter = reaper.GetUserInputs("Toggle track FX bypass by name", 1,
-  "Toggle track FX bypass matching:,extrawidth=100", "")
+if not reaper.GetTrackName then
+  -- for REAPER prior to v5.30 (native GetTrackName returns "Track N" when it's empty)
+  function reaper.GetTrackName(track, _)
+    return reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', '', false)
+  end
+end
 
-if not ok or filter:len() < 1 then
+function matchTrack(track, filter)
+  if filter == '/selected' then
+    return reaper.IsTrackSelected(track)
+  else
+    local _, name = reaper.GetTrackName(track, '')
+    return name:lower():find(filter)
+  end
+end
+
+local defaultTrackFilter = ''
+if reaper.CountSelectedTracks() > 0 then
+  defaultTrackFilter = '/selected'
+end
+
+local ok, csv = reaper.GetUserInputs("Toggle track FX bypass by name", 2,
+  "Toggle track FX bypass matching:,On tracks (name or /selected):,extrawidth=100",
+  ',' .. defaultTrackFilter)
+
+if not ok or csv:len() < 1 then
   reaper.defer(function() end) -- no undo point if nothing to do
   return
 end
 
-filter = filter:lower()
+local fx_filter, track_filter = csv:match("^(.*),(.*)$")
+fx_filter, track_filter = fx_filter:lower(), track_filter:lower()
 
 reaper.Undo_BeginBlock()
 
 for ti=0,reaper.CountTracks()-1 do
   local track = reaper.GetTrack(0, ti)
 
-  for fi=0,reaper.TrackFX_GetCount(track)-1 do
-    local _, fxname = reaper.TrackFX_GetFXName(track, fi, '')
-    if fxname:lower():find(filter) then
-      reaper.TrackFX_SetEnabled(track, fi,
-        not reaper.TrackFX_GetEnabled(track, fi))
+  if matchTrack(track, track_filter) then
+    for fi=0,reaper.TrackFX_GetCount(track)-1 do
+      local _, fx_name = reaper.TrackFX_GetFXName(track, fi, '')
+      if fx_name:lower():find(fx_filter) then
+        reaper.TrackFX_SetEnabled(track, fi,
+          not reaper.TrackFX_GetEnabled(track, fi))
+      end
     end
   end
 end
 
 reaper.Undo_EndBlock(
-  string.format("Toggle track FX bypass matching '%s'", filter), -1)
+  string.format("Toggle track FX bypass matching '%s'", fx_filter), -1)
