4bcd10559ca899076a663314b28443797909433f
juliansader <julian.sader@gmail.com>
2016-12-20 03:23:26 +0200

    Add files via upload

--- MIDI Editor/js_Option - Selecting single note or CC in active take sets channel for new events.lua
+++ MIDI Editor/js_Option - Selecting single note or CC in active take sets channel for new events.lua
@@ -1,38 +1,46 @@
 --[[
- * ReaScript Name:  js_Option - Selecting single note or CC in active take sets channel for new events.lua
- * Description: Inserting events in the wrong channel is an all-too-easy mistake to make, particularly after 
- *                  switching the active track. With this new option, the user can switch to the correct 
- *                  channel by simply clicking a note or CC of the new track. 
- *              It is intended to be similar to the built-in action "Option: Drawing or selecting 
- *                  a note sets the new note length" (but for channel instead of note length).
- *              WARNING:  This script causes the folowing artefacts:
- *                  ~ If an event in a *new* channel is selected by left-clicking on the event, 
- *                    the event can not be edited immediately; instead, the mouse button must be released first.
- *                  ~ While this script is running, it will interact with the manual settings of the MIDI editor's 
- *                    channel filter dropdown menu.
- * Instructions: This script can be linked to a toolbar button for easy starting and stopping.
- *               While activated, the toolbar button will light up.
- *               (The first time that the script is stopped, REAPER will pop up a dialog box 
- *                    asking whether to terminate or restart the script.  Select "Terminate"
- *                    and "Remember my answer for this script".) * Screenshot: 
- * Notes: 
- * Category: 
- * Author: juliansader
- * Licence: GPL v3
- * Forum Thread: 
- * Forum Thread URL: http://forum.cockos.com/showthread.php?t=176878, http://forum.cockos.com/showthread.php?t=178256
- * Version: 1.0
- * REAPER: 5.20
- * Extensions:
+ReaScript name:  js_Option - Selecting single note or CC in active take sets channel for new events.lua
+Version: 2.00
+Author: juliansader
+Website: http://forum.cockos.com/showthread.php?t=178256
+About:
+  # Description 
+  Inserting events in the wrong channel is an all-too-easy mistake to make, particularly after 
+       switching the active track. With this new option, the user can switch to the correct 
+       channel by simply clicking a note or CC of the new track. 
+       
+   It is intended to be similar to the built-in action "Option: Drawing or selecting 
+       a note sets the new note length" (but for channel instead of note length).
+       
+   NOTE: This script causes the following artefacts:
+       ~ If an event in a *new* channel is selected by left-clicking on the event, 
+         the event can not be edited immediately; instead, the mouse button must be released first.
+         (This can be a useful indicator that the active channel has been changed.)
+       ~ While this script is running, it may interact with the manual settings of the MIDI editor's 
+         channel filter dropdown menu.
+         
+  # Instructions
+  This script can be linked to a toolbar button for easy starting and stopping.
+  While activated, the toolbar button will light up.
+  (The first time that the script is stopped, REAPER will pop up a dialog box 
+       asking whether to terminate or restart the script.  Select "Terminate"
+       and "Remember my answer for this script".)Screenshot: 
 ]]
  
 --[[
  Changelog:
  * v1.0 (2016-07-15)
     + Initial release
+ * v2.00 (2016-12-19)
+    + Improved speed and responsiveness.
+    + Requires REAPER v5.30.
 ]]
 
-prevChannel = 0
+local prevChannel = 0
+
+local matchCCorNote = string.char(91,1,3,93,91,2,3,93,0,0,0,91,0x90,45,0xDF,93) -- [13][23]000[0x90-0xDF]
+local matchNoteOn   = string.char(91,1,3,93,     3,   0,0,0,91,0x90,45,0x9F,93) -- [13]3000[0x90-0x9F]
+local matchCC       = string.char(91,1,3,93,91,2,3,93,0,0,0,91,0xB0,45,0x9F,93) -- [13][23]000[0xB0-0x9F]
 
 ---------------
 function exit()
@@ -46,62 +54,59 @@ end -- function exit
 
 ----------------------------
 function loopGetSetChannel()
+
     local editor = reaper.MIDIEditor_GetActive()
     if editor ~= nil then 
+    
         local take = reaper.MIDIEditor_GetTake(editor)
-        if take ~= nil then
-            --[[
-            -- The following code does not limit selection to a single note or CC:
-            local firstSelectedIndex = reaper.MIDI_EnumSelEvts(take, -1)
-            if firstSelectedIndex > -1 then
-                local returnOK, _, _, _, msg = reaper.MIDI_GetEvt(take, firstSelectedIndex, true, true, 0, "")
-                if returnOK == true and type(msg) == "string" and msg ~= "" then
-                    local newChannel=(tonumber(string.byte(msg:sub(1,1))))&15
-                    -- The following action causes artefacts in the MIDI editor if called while editing with mouse, 
-                    --    so should only be called once when channel changes
-                    if newChannel ~= prevChannel then
-                        reaper.MIDIEditor_OnCommand(editor, 40482+newChannel)
-                        prevChannel = newChannel
+        if reaper.ValidatePtr(take, "MediaItem_Take*") then
+        
+            local gotAllOK, MIDIstring = reaper.MIDI_GetAllEvts(take, "")
+            if gotAllOK then
+            
+                local newChannel = nil
+                -- Note: Why use MIDIstring:byte instead of returning the short sub-string as the third value of string.find?
+                --    Because string.find's sub-string extraction is surprisingly slow.
+                local pos1 = MIDIstring:find(matchCCorNote, 1)
+                if not pos1 then -- Found no selected (and non-muted) notes or CCs
+                    -- nothing   
+                    
+                -- Notes take precedence, so if note, ignore CCs and only check whether there is more than one note selected
+                -- Why do notes take precedence?  Because if "CC selection follows note selection" is switched on,
+                --    selecting a single note may automatically select many CCs.
+                elseif MIDIstring:byte(pos1+5)&0xF0 == 0x90 then -- Note-On?
+                    local pos2 = MIDIstring:find(matchNoteOn, pos1+1)
+                    if not pos2 then -- found only one selected note, so use channel
+                        newChannel = MIDIstring:byte(pos1+5)&0x0F
+                    end
+                    
+                -- Got CC, but still check whether any notes are selected
+                else
+                    -- Check whether there are any selected notes
+                    local pos2 = MIDIstring:find(matchNoteOn, pos1+1)
+                    if pos2 then -- Got one selected note, check whether more than one
+                        local pos3 = MIDIstring:find(matchNoteOn, pos2+1)
+                        if not pos3 then -- Couldn't find more than one note
+                            newChannel = MIDIstring:byte(pos2+5)&0x0F
+                        end
+                    -- No selected notes, so check whether only one selected CC
+                    else
+                        pos2 = MIDIstring:find(matchCC, pos1+1)
+                        if not pos2 then -- Only one selected CC, so use channel
+                            newChannel = MIDIstring:byte(pos1+5)&0x0F
+                        end
                     end
                 end
-            end
-            ]]
-            
-            -- Check whether number of selected notes is exactly one
-            local firstNote = reaper.MIDI_EnumSelNotes(take, -1)
-            local secondNote = reaper.MIDI_EnumSelNotes(take, firstNote)
-            if firstNote ~= -1 and secondNote == -1 then 
-                local noteOK, _, _, newPPQstart, _, newChannel, newPitch, _ = reaper.MIDI_GetNote(take, firstNote)
-                -- The selected note should only change the channel if it is a *newly* selected note, therefore
-                --    check whether this note is different from previously selected note
-                if noteOK == true and not (newPPQstart == prevPPQstart and newPitch == prevPitch and newChannel == prevChannel) then
-                    prevPPQstart = newPPQstart; prevPitch = newPitch; prevChannel = newChannel
-                    -- Check whether different from default channel
+                
+                if newChannel then
                     local defaultChannel = reaper.MIDIEditor_GetSetting_int(editor, "default_note_chan")
                     if newChannel ~= defaultChannel then
                         reaper.MIDIEditor_OnCommand(editor, 40482+newChannel)
                     end
                 end
-                
-            -- Number of selected notes is note not exactly one, so let's check if there is exactly one selected CC
-            else
-                local firstCC = reaper.MIDI_EnumSelCC(take, -1)
-                local secondCC = reaper.MIDI_EnumSelCC(take, firstCC)
-                if firstCC ~= -1 and secondCC == -1 then
-                    local ccOK, _, _, newPPQ, _, newChannel, _, _ = reaper.MIDI_GetCC(take, firstCC)
-                    -- The selected CC should only change the channel if it is a *newly* selected CC, therefore
-                    --    check whether this CC is different from previously selected CC
-                    if ccOK == true and not (newPPQ == prevPPQ and newChannel == prevChannel) then
-                        prevPPQ = newPPQ; prevChannel = newChannel
-                        -- Check whether different from default channel
-                        local defaultChannel = reaper.MIDIEditor_GetSetting_int(editor, "default_note_chan")
-                        if newChannel ~= defaultChannel then
-                            reaper.MIDIEditor_OnCommand(editor, 40482+newChannel)
-                        end
-                    end     
-                end
-            end
-        end
+          
+            end -- if gotAllOK
+        end -- if reaper.ValidatePtr(take, "MediaItem_Take*")
     end
     reaper.runloop(loopGetSetChannel)
 end -- function loop GetSetChannel
@@ -110,6 +115,12 @@ end -- function loop GetSetChannel
 -- Here the code execution starts
 --------------------------------------------------------------------
 -- function main()
+
+-- Check whether the required version of REAPER is available
+if not reaper.APIExists("MIDI_GetAllEvts") then
+    reaper.ShowMessageBox("This script requires REAPER v5.30 or higher.", "ERROR", 0)
+    return(false)
+end
 
 reaper.atexit(exit)
 
