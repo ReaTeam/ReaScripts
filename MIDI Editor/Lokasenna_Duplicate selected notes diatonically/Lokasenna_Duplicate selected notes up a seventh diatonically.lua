3c87ee212d85f8acd9cf738919555f930e4bc8c3
Lokasenna <jalovatt@hotmail.com>
2017-01-08 19:30:26 -0700

    Merge all duplicate notes in two bundled packages (#19)

---
+++ MIDI Editor/Lokasenna_Duplicate selected notes diatonically/Lokasenna_Duplicate selected notes up a seventh diatonically.lua
@@ -0,0 +1,11 @@
+--[[
+	This script is part of Lokasenna_Duplicate selected notes diatonically.lua
+	NoIndex: true
+--]]
+
+-- Licensed under the GNU GPL v3
+
+interval = 7
+
+local script_path = debug.getinfo(1,'S').source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "../Lokasenna_Duplicate selected notes diatonically.lua")
---
+++ MIDI Editor/Lokasenna_Duplicate selected notes diatonically/Lokasenna_Duplicate selected notes up an eleventh diatonically.lua
@@ -0,0 +1,11 @@
+--[[
+	This script is part of Lokasenna_Duplicate selected notes diatonically.lua
+	NoIndex: true
+--]]
+
+-- Licensed under the GNU GPL v3
+
+interval = 11
+
+local script_path = debug.getinfo(1,'S').source:match[[^@?(.*[\/])[^\/]-$]]
+dofile(script_path .. "../Lokasenna_Duplicate selected notes diatonically.lua")
