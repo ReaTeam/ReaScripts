b7e3811aab3620bbf8273160d0c16e95186a85be
juliansader <julian.sader@gmail.com>
2017-01-14 07:10:52 +0200

    Update js_LFO Tool (MIDI editor version, insert CCs under selected notes in lane under mouse).lua

--- MIDI Editor/js_LFO Tool (MIDI editor version, insert CCs under selected notes in lane under mouse).lua
+++ MIDI Editor/js_LFO Tool (MIDI editor version, insert CCs under selected notes in lane under mouse).lua
@@ -71,7 +71,7 @@ About:
 --[[
   Changelog:
   v2.00 (2017-01-15)
-    + Much faster execution in large takes with hundreds aof thousands of events.
+    + Much faster execution in large takes with hundreds of thousands of events.
     + Keyboard shortcuts "a", "c" and "r" to switch GUI views.
     + LFO can be applied to existing events - including velocities - instead of inserting new CCs.
     + Requires REAPER v5.32 or later.  
