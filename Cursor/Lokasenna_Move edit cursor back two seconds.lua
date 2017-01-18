010b7a52af573a3fdae2447b7e67427cd108bd55
Lokasenna <jalovatt@hotmail.com>
2017-01-18 13:57:05 -0700

    Add files via upload

---
+++ Cursor/Lokasenna_Move edit cursor back two seconds.lua
@@ -0,0 +1,15 @@
+--[[
+Description: Move edit cursor back two seconds
+Version: 1.0
+Author: Lokasenna
+Donation: https://paypal.me/Lokasenna
+Changelog:
+Links:
+	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+About: 
+Extensions:
+--]]
+
+-- Licensed under the GNU GPL v3
+
+reaper.MoveEditCursor(-2, false)
