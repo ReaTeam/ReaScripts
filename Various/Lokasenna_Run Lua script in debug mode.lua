3bde81f8ccdc3be7c1aae4693437117ce36c47f4
Lokasenna <jalovatt@hotmail.com>
2017-02-09 22:01:23 -0700

    Update Lokasenna_Run Lua script in debug mode.lua

--- Various/Lokasenna_Run Lua script in debug mode.lua
+++ Various/Lokasenna_Run Lua script in debug mode.lua
@@ -1,9 +1,10 @@
 --[[
 Description: Lokasenna_Run Lua script in debug mode
-Version: 1.0.0
+Version: 1.1.0
 Author: Lokasenna
 Donation: https://paypal.me/Lokasenna
 Changelog:
+	Stores the last script to an ExtState; for companion script
 Links:
 	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
 About: 
@@ -27,5 +28,6 @@ debug_mode = true
 local ret, debug_file = reaper.GetUserFileNameForRead("", "Choose a ReaScript", ".lua")
 if not ret or not debug_file then return 0 end
 local debug_name = string.match(debug_file, "[\\/]([^\\/]*)$")
+reaper.SetExtState("Lokasenna_Debug mode", "last", debug_file, true)
 reaper.ShowConsoleMsg("Running '"..tostring(debug_name).."' in debug mode...\n")
 dofile(debug_file)
