b009516a050c47dcfc49eb9a5e44b1fd7196787b
Beau Dobbin <beau@beaunus.com>
2017-02-20 19:05:13 +0900

    Added 2 new scripts (#20)
    * Added "Add markers for lyrics in selected items"
    
    * Added "Name item takes by last marker to cut item"

---
+++ Items Editing/beaunus_Name item takes by last marker to cut item.lua
@@ -0,0 +1,38 @@
+--[[
+  * ReaScript Name: Name item takes by last marker to cut item
+  * Description:    Names item takes according to the name of the marker that
+  *                 is the last marker to cut the items.
+  * Instructions:   - Select items
+  *                 - Run the script
+  * Author: beaunus
+  * Licence: GPL v3
+  * REAPER: 5.0
+  * Version: 1.0
+]]
+
+--[[
+ Changelog:
+ * v1.0 (2017-02-20)
+    + Initial Release
+]]
+
+-- Count the number of selected items.
+num_selected_items = reaper.CountSelectedMediaItems()
+
+-- Iterate through all selected items.
+for i = 0, num_selected_items - 1 do
+  -- Get the media item
+  item = reaper.GetSelectedMediaItem(0, i)
+
+  -- Get the active take
+  take = reaper.GetActiveTake(item)
+
+  -- Find the closest marker to beginning of take
+  item_start_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+  item_end_position = item_start_position + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+  markeridx, regionidx = reaper.GetLastMarkerAndCurRegion(0, item_end_position)
+  retval, isrgn, pos, rgnend, marker_name = reaper.EnumProjectMarkers(markeridx)
+
+    -- Apply new name
+  reaper.GetSetMediaItemTakeInfo_String(take, 'P_NAME', marker_name, true)
+end
