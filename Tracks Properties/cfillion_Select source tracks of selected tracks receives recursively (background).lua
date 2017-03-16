78d02b17af007aa1e483eb009df23ee623edba1f
cfillion <cfillion@users.noreply.github.com>
2017-03-15 21:56:22 -0400

    hotfix for Select source tracks of selected tracks receives recursively (wrong included file)

--- Tracks Properties/cfillion_Select source tracks of selected tracks receives recursively (background).lua
+++ Tracks Properties/cfillion_Select source tracks of selected tracks receives recursively (background).lua
@@ -5,4 +5,4 @@ background = true
 destination = false
 
 local dir = ({reaper.get_action_context()})[2]:match("^(.*[/\\])")
-dofile(dir .. 'cfillion_Select destination tracks of selected tracks sends recursively (logic).lua')
+dofile(dir .. 'cfillion_Select source tracks of selected tracks receives recursively (logic).lua')
