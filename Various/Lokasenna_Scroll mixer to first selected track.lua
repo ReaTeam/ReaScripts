0e36947a54f24706bbbab160e7908083d23a800d
Lokasenna <jalovatt@hotmail.com>
2017-04-04 16:52:25 -0600

    Add files via upload

---
+++ Various/Lokasenna_Scroll mixer to first selected track.lua
@@ -0,0 +1,18 @@
+--[[
+Description: Scroll mixer to first selected track
+Version: 1.0
+Author: Lokasenna
+Donation: https://paypal.me/Lokasenna
+Changelog:
+	Initial release
+Links:
+	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+About: 
+	Scrolls the mixer so that the first selected track
+	is at the left-hand side, if possible.
+Extensions:
+--]]
+
+-- Licensed under the GNU GPL v3
+
+reaper.SetMixerScroll( reaper.GetSelectedTrack(0, 0) )
