e9d78fa58507fde4400bd877ef30e5fd2499f5a5
juliansader <julian.sader@gmail.com>
2017-07-03 03:36:07 +0200

    Update js_Time selection - Insert empty beats at time selection (moving later items).lua

--- Time Selection/js_Time selection - Insert empty beats at time selection (moving later items).lua
+++ Time Selection/js_Time selection - Insert empty beats at time selection (moving later items).lua
@@ -1,6 +1,6 @@
 --[[
 ReaScript name: js_Time selection - Insert empty beats at time selection (moving later items).lua
-Version: 0.92
+Version: 0.93
 Author: juliansader
 Website: http://forum.cockos.com/showthread.php?t=191210
 Donation: https://www.paypal.me/juliansader
@@ -28,6 +28,8 @@ About:
   * v0.91 (2017-06-30)
     + Changed the header Description a little.
   * v0.92 (2017-07-02)
+    + Workaround for "Add edge points" bug in REAPER that deletes timesig markers at insertion point.
+  * v0.93 (2017-07-03)
     + Workaround for "Add edge points" bug in REAPER that deletes timesig markers at insertion point.
 ]]
 
@@ -58,16 +60,18 @@ end
 --    if "Options: Add edge points when ripple editing or inserting time" is ON.
 -- The pre-existing timesig marker is overwritten by the edge node.
 -- This script will therefore deactive the option before inserting time.
+--[[
 stateInsertEdgePoints = reaper.GetToggleCommandStateEx(0, 40649) -- Options: Add edge points when ripple editing or inserting time
 if stateInsertEdgePoints == 1 then
     reaper.Main_OnCommandEx(40649, -1, 0)
 end
---[[ Make doubly sure, by finding and later re-inserting any timesig change at start
+]]
+-- Make doubly sure, by finding and later re-inserting any timesig change at start
 startTempoMarkerIndex = reaper.FindTempoTimeSigMarker(0, timeStart+0.001)
 if startTempoMarkerIndex ~= prevTempoMarkerIndex then
     startOK, startTimePos, _, _, startBPM, startTimesig_num, startTimesig_denom, startLineartempo = reaper.GetTempoTimeSigMarker(0, startTempoMarkerIndex)
 end
-]]
+
 
 newTimeEnd = timeStart + (beatEnd-beatStart) * (60.00/tempoAtTimeStart)
 
@@ -77,17 +81,21 @@ reaper.PreventUIRefresh(1)
 _, _ = reaper.GetSet_LoopTimeRange2(0, true, false, timeStart, newTimeEnd, true)
 reaper.Main_OnCommandEx(40200, -1, 0) -- Time selection: Insert empty space at time selection (moving later items)
 
---[[ Replace the edge node that may have destroyed pre-existing timesig markers.
+-- Replace the edge node that may have destroyed pre-existing timesig markers.
 if startOK then
     buggyIndex = reaper.FindTempoTimeSigMarker(0, newTimeEnd + 0.01)
     if buggyIndex ~= -1 then
         local buggyOK, buggyTimePos, buggyMeasurePos, buggyBeatPos, buggyBPM, buggyTimesig_num, buggyTimesig_denom, buggyLineartempo = reaper.GetTempoTimeSigMarker(0, buggyIndex)
         if buggyOK and buggyTimePos > newTimeEnd - 0.01 then
             reaper.SetTempoTimeSigMarker(0, buggyIndex, newTimeEnd, -1, -1, startBPM, startTimesig_num, startTimesig_denom, startLineartempo)
+            changedMarker = true
         end
     end
+    -- If "Insert empty space" completely removed the marker at start
+    if not changedMarker then
+        reaper.SetTempoTimeSigMarker(0, -1, newTimeEnd, -1, -1, startBPM, startTimesig_num, startTimesig_denom, startLineartempo)
+    end
 end
-]]
 
 -- Toggle state back on
 if stateInsertEdgePoints == 1 then
