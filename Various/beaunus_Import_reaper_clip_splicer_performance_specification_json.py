9a26a9fe3b9ea4233cf3366cdb19b121f046a313
Beau Dobbin <beau@beaunus.com>
2017-07-26 20:30:18 +0900

    Use dict.items() instead of dict.iteritems()

--- Various/beaunus_Import_reaper_clip_splicer_performance_specification_json.py
+++ Various/beaunus_Import_reaper_clip_splicer_performance_specification_json.py
@@ -4,25 +4,25 @@
 ReaScript Name: Import Clip Splicer Performance Specification JSON file
 About:
   # beaunus REAPER Clip Splicer
-  
-  This module imports a _performance specification_ JSON document into the 
+
+  This module imports a _performance specification_ JSON document into the
   current REAPER session.
-  
-  The project was created for making _listen and repeat_-style CDs for a 
-  language school.  In such CD projects, there are hundreds of audio clips 
-  that need to be aligned and sorted by track.  This automated process saves 
+
+  The project was created for making _listen and repeat_-style CDs for a
+  language school.  In such CD projects, there are hundreds of audio clips
+  that need to be aligned and sorted by track.  This automated process saves
   most of the time needed for producing the CDs.
-  
+
   ## Define the _performance specification_
-  
+
   Use a Google Sheet to define the discs and tracks.
-  
+
   https://docs.google.com/spreadsheets/d/1FlhfMCX0HRUWVk8Or2GJZTPwxc_uaci3KM-0VbspnIY/edit#gid=0
-  
+
   ### Understanding the JSON representation
-  
+
   The specification is represented by a dictionary.
-  
+
       {
         // Disc 01
         "Disc_01 Title": [
@@ -60,15 +60,15 @@ About:
           ]
         ],
       }
-  
+
   ### Adding templates
-  
-  The real power of the spreadsheet is creating _templates_.  A _template_ 
-  uses a custom Google Apps Script to create an array of components, given a 
+
+  The real power of the spreadsheet is creating _templates_.  A _template_
+  uses a custom Google Apps Script to create an array of components, given a
   simple set of template arguments.
-  
+
   Here's an example of a template from the spreadsheet.
-  
+
   <table>
       <tr>
           <th>Disc</th>
@@ -97,15 +97,15 @@ About:
           </td>
       </tr>
   </table>
-  
-  In order to interpret the above data, you will need to adjust the 
+
+  In order to interpret the above data, you will need to adjust the
   spreadsheet's `templates.gs` script in the following ways:
-  
+
   1. In the function `template()`, add a case for the template `"Repeat Words"`.
   1. Add a function `templateRepeatWords()`.
-  
+
   The function should return an array of components.  For example:
-  
+
       [
         "Love [John Lennon]",
         "_PAUSE_AFTER_PAGE_NUMBER",
@@ -114,16 +114,16 @@ About:
         "Love [John Lennon]",
         "_PAUSE_AFTER_PAGE_NUMBER"
       ]
-  
+
   ## Import the specification into a REAPER session.
-  
+
   1. Run the script `Script: beaunus_import_reaper_clip_splicer_json.py`
   1. Select a .json file that contains a REAPER Clip Splicer specification.
   1. Examine the generated report to see what needs to be recorded.
       - The report can be found in the same folder as the .json file.
-  
+
   ## Record the clips
-  
+
   1. If the _performance sequence_ is simple enough to manually trim each clip,
   simply record the clips without a lyrics track.
   1. If you want to automated the trimming process:
@@ -131,35 +131,35 @@ About:
       session.
       1. Set a reasonable tempo.
       1. Record the clips in order.
-  
+
   ## Trim and save the clips
-  
+
   Performances should be __loudness normalized__ to -23LUFS.
-  
+
   If you have used a lyrics track:
-  
-  1. Convert the lyrics to markers.  
+
+  1. Convert the lyrics to markers.
     + `beaunus_Add markers for lyrics in selected items.lua`
   1. Split the performance using dynamic split.
   1. Trim silence at the beginnings and ends of clips.
-  1. Name the takes according to the marker that cuts them.  
+  1. Name the takes according to the marker that cuts them.
     + `beaunus_Name item takes by last marker to cut item.lua`
   1. SWS: Create regions from selected items (name by active take)
   1. Render regions.
-  
+
   Finished clips should be named according to the following convention:
-  
+
   `[label] [[performer]].[file extension]`
-  
+
   `$region [$track]`
-  
+
   For example:
-  
+
   `Love [John Lennon].wav`
-  
+
   Clips should be put in the folder `"clips"` relative to the .json file.
-  
-  After all the clips have been recorded, you can again 'Import the 
+
+  After all the clips have been recorded, you can again 'Import the
   specification into a REAPER session.'
 Author: beaunus
 Licence: GPL v3
@@ -441,7 +441,7 @@ class ReaperClipSplicer:
         """
         # Iterate over each disc
         # pylint: disable=unused-variable
-        for disc_name, disc_val in self.discs.iteritems():
+        for disc_name, disc_val in self.discs.items():
             beginning_of_disc = cursor_position
             # Iterate over each track
             track_index = 1
@@ -520,7 +520,7 @@ class ReaperClipSplicer:
         """Creates a file that reports the available and unavailable files
         for the project.
         """
-        
+
         # Prompt user if the report should be sorted
         captions_csv = 'YES / NO'
         retvals_csv = ''
@@ -570,4 +570,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
