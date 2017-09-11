7782d6f989492506d1699b7699bf010b0db047d2
fnaFnaFnAFNA <ibjd@charter.net>
2017-09-11 17:00:44 -0400

    FnA trying to get connected (#68)

---
+++ Various/FnA_Ensure toggle OFF (item grouping).lua
@@ -0,0 +1,29 @@
+-- Description: Ensure toggle OFF (item grouping).lua
+-- Version: 1.0
+-- Author: FnA
+-- Changelog: Initial release
+-- Link: Forum Thread (somewhat indirectly) http://forum.cockos.com/showthread.php?t=191368
+-- About:
+--   An example of a script which toggles a Toggle action (having an ON/OFF "State" in the Action List)
+--   only if the action is ON, thus either setting it OFF, or leaving it OFF. Generally for use in custom actions.
+
+-- User Variables ---------------------------------
+-- Paste Command ID from Action List between quotes:
+local Command_ID = "1156"
+-- End of User Variables --------------------------
+
+local r = reaper
+local x = tonumber(Command_ID)
+if x then -- REAPER factory actions
+  if r.GetToggleCommandState(x) == 1 then
+    r.Main_OnCommand(x, 0)
+  end
+else -- SWS actions
+  local x = r.NamedCommandLookup(Command_ID)
+  if r.GetToggleCommandState(x, 0) == 1 then
+    r.Main_OnCommand(x, 0)
+  end
+end
+
+function noundo() end
+r.defer(noundo)
