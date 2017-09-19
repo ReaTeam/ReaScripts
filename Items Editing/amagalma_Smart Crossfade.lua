d0a65f838554f494f25e246ed09b40e4b49fa243
amagalma <amagalma@gmail.com>
2017-09-19 10:49:13 +0300

    Update amagalma_Smart Crossfade.lua

--- Items Editing/amagalma_Smart Crossfade.lua
+++ Items Editing/amagalma_Smart Crossfade.lua
@@ -1,12 +1,12 @@
 -- @description amagalma_Smart Crossfade
 -- @author amagalma
--- @version 1.28
+-- @version 1.30
 -- @about
 --   # Crossfades selected items
 --
 --   - If items are adjacent then it creates a crossfade (length defined by user) on the left side of the items' touch point
 --   - If items overlap then it creates a crossfade at the overlapping area
---   - If there is a time selection covering part of both items, then it crossfades at the time selection area
+--   - If there is a time selection covering part of both items, then it crossfades at the time selection area (items do not have to touch necessarily)
 --   - Can be used with as many items in different tracks as you like
 --   - Smart undo point creation (only if there has been at least one change)
 --   - You can set inside the script if you want to keep the time selection or remove it
@@ -15,8 +15,10 @@
 
 --[[
  * Changelog:
- * v1.28 (2017-09-17)
-  + fixed bug when a time selection enclosed more than two items in a row
+ * v1.30 (2017-09-19)
+  + fixed bug when "Trim behind items" was enabled
+  + fixed loosing item selection, in case no change to any items had happened
+  + now items can be crossfaded when there is a time selection touching both items, even if the two items do not touch each other
 --]]
 
 
@@ -79,6 +81,10 @@ end
 xfadetime = xfadetime*0.001
 if item_cnt > 1 then
   reaper.PreventUIRefresh(1)
+  local trimstate = reaper.GetToggleCommandStateEx( 0, 41117) -- get Options: Toggle trim behind items state
+  if trimstate == 1 then
+    reaper.Main_OnCommand(41121, 0) -- Options: Disable trim behind items when editing
+  end
   for i = 0, item_cnt-1 do
     local item = reaper.GetSelectedMediaItem(0, 0)
     sel_item[#sel_item+1] = item
@@ -93,13 +99,15 @@ if item_cnt > 1 then
       local secondend = secondstart + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
       local firststart = reaper.GetMediaItemInfo_Value(previousitem, "D_POSITION")
       local firstend = firststart + reaper.GetMediaItemInfo_Value(previousitem, "D_LENGTH")
-      if firstend < secondstart then -- items do not touch
+      if firstend < secondstart and (selstart == selend or (firstend < selstart and secondstart > selend)) then
+        -- items do not touch and there is no time selection covering parts of both items
         -- do nothing
-      elseif firststart < secondstart and firstend > secondend then -- one item encloses the other
+      elseif firststart < secondstart and firstend > secondend then
+        -- one item encloses the other
         -- do nothing
       else
         -- time selection exists and covers parts of both items
-        if selstart ~= selend and selend > secondstart and selend <= secondend and selstart < firstend and selstart >= firststart then
+        if selstart ~= selend and selend >= secondstart and selend <= secondend and selstart <= firstend and selstart >= firststart then
           timeselexists = 1
           local timesel = selend - selstart
           if FadeInOK(item, timesel) == false or FadeOutOK(previousitem, timesel) == false then
@@ -137,11 +145,18 @@ if item_cnt > 1 then
       end
     end
   end
+  if trimstate == 1 then
+    reaper.Main_OnCommand(41120,0) -- Re-enable trim behind items (if it was enabled)
+  end
   reaper.PreventUIRefresh(-1)
 end
 
 -- Undo point creation ----------------------------------------------------------------------------
 if change == false then
+  -- do not loose item selection
+  for i = 1, #sel_item do
+    reaper.SetMediaItemSelected(sel_item[i], true)
+  end
   function NoUndoPoint() end
   reaper.defer(NoUndoPoint)
 else
