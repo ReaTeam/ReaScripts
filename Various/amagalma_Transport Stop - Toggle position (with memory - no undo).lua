76fa7b8ef6e29ed45ce63666e3cc6208c21bbaea
amagalma <amagalma@gmail.com>
2017-09-25 17:13:28 +0300

    Update amagalma_Transport Stop - Toggle position (with memory - no undo).lua

--- Various/amagalma_Transport Stop - Toggle position (with memory - no undo).lua
+++ Various/amagalma_Transport Stop - Toggle position (with memory - no undo).lua
@@ -1,11 +1,16 @@
 -- @description amagalma_Transport Stop - Toggle position (with memory - no undo)
 -- @author amagalma
--- @version 1.1
+-- @version 1.11
 -- @about
 --   # Stops and toggles between starting and ending position.
 --
 --   - To be used in conjunction with my other "Transport (with memory - no undo)" scripts
 
+--[[
+ Changelog:
+ * v1.11 (2017-09-25)
+  + different behavior depending on "amagalma_Toggle behavior of Play-Stop buttons" state
+--]]
 --------------------------------------------------------------------------------------------
 
 local reaper = reaper
@@ -19,10 +24,19 @@ end
 
 local playstate = reaper.GetPlayState()
 if playstate > 0 then
-  reaper.Main_OnCommand(40434, 0); --View: Move edit cursor to play cursor
-  reaper.Main_OnCommand(1016, 0); --Transport: Stop
-  local pos = reaper.GetCursorPosition()
-  reaper.SetExtState("Play-Stop with memory", "Position2", tostring(pos), 0)
+  -- check "amagalma_Toggle behavior of Play-Stop buttons" state
+  local cmdID = reaper.NamedCommandLookup("_RS4b332fb0ea338b4991de7e2e9cd81032d79647da")
+  local script_state = reaper.GetToggleCommandStateEx(0,cmdID)
+  if script_state == 1 then
+    reaper.Main_OnCommand(40434, 0); --View: Move edit cursor to play cursor
+    reaper.Main_OnCommand(1016, 0); --Transport: Stop
+    local pos = reaper.GetCursorPosition()
+    reaper.SetExtState("Play-Stop with memory", "Position2", tostring(pos), 0)
+  else
+    local pos =  reaper.GetPlayPosition()
+    reaper.Main_OnCommand(1016, 0) --Transport: Stop
+    reaper.SetExtState("Play-Stop with memory", "Position2", tostring(pos), 0)
+  end
 else
   local pos = reaper.GetCursorPosition()
   local HasState = reaper.HasExtState("Play-Stop with memory", "Position")
