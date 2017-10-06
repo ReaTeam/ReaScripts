cab3e76004f673a801f5073bac6041b05b86df26
amagalma <amagalma@gmail.com>
2017-10-06 13:15:02 +0300

    Update amagalma_Insert pooled instances of one automation item in time selection for selected tracks and selected envelope.lua

--- Envelopes/Automation Items/amagalma_Insert pooled instances of one automation item in time selection for selected tracks and selected envelope.lua
+++ Envelopes/Automation Items/amagalma_Insert pooled instances of one automation item in time selection for selected tracks and selected envelope.lua
@@ -1,6 +1,6 @@
 -- @description amagalma_Insert pooled instances of one automation item in time selection for selected tracks and selected envelope
 -- @author amagalma
--- @version 1.01
+-- @version 1.1
 -- @about
 --   # Inserts automation items in time selection for the selected tracks and selected envelope
 --   - There must be a selected envelope and a time selection set for the script to work
@@ -8,26 +8,28 @@
 
 --[[
  * Changelog:
- * v1.01 (2017-10-03)
-  + undo point creation improvement
+ * v1.1 (2017-10-06)
+  + work-around for Reaper bug: https://forum.cockos.com/showthread.php?t=196794
+  + added setting inside the script to remove or keep time selection after action end
 --]]
+
+----------------------------- USER SETTINGS -----------------------------------------------
+
+local removetimesel = 1 -- set to 0 to keep time selection or to 1 to remove it
 
 -------------------------------------------------------------------------------------------
 
 local reaper = reaper
-local done = -1
-
--------------------------------------------------------------------------------------------
-
 local timeStart, timeEnd = reaper.GetSet_LoopTimeRange( false, false, 0, 0, false )
 local sel_env = reaper.GetSelectedEnvelope( 0 )
 local track_cnt = reaper.CountSelectedTracks( 0 )
 -- proceed if time selection exists and there is a selected envelope
 if sel_env and timeStart ~= timeEnd then
+  reaper.Undo_BeginBlock2( 0 )
+  local _, envchunk = reaper.GetEnvelopeStateChunk( sel_env, "", false )
+  local env_name = string.match (envchunk, "[^\n]+")
   local sel_env_track = reaper.Envelope_GetParentTrack( sel_env )
   reaper.InsertAutomationItem( sel_env, -1, timeStart, timeEnd-timeStart )
-  done = 0
-  local _, env_name = reaper.GetEnvelopeName( sel_env, "" )
   local ai_cnt =  reaper.CountAutomationItems( sel_env )
   local pool_id
   for i = 0, ai_cnt-1 do
@@ -40,21 +42,19 @@ if sel_env and timeStart ~= timeEnd then
   for i = 0, track_cnt-1 do
     local track = reaper.GetSelectedTrack( 0, i )
     if track ~= sel_env_track then
-      local env = reaper.GetTrackEnvelopeByName( track, env_name )
+      local env = reaper.GetTrackEnvelopeByChunkName( track, env_name )
       if env then
         reaper.InsertAutomationItem( env, pool_id, timeStart, timeEnd-timeStart )
-        done = done + 1
       end
     end
   end
+  -- the following commands are needed as a work-around for bug: https://forum.cockos.com/showthread.php?t=196794
+  reaper.Main_OnCommand(40915, 0) -- Envelope: Insert new point at current position
+  reaper.Main_OnCommand(40333, 0) -- Envelope: Delete all selected points
+  -- Clear time selection
+  if removetimesel == 1 then  
+    reaper.GetSet_LoopTimeRange( true, false, 0, 0, false )
+  end
+  reaper.Undo_EndBlock2( 0, "Insert pooled instances of one automation item for selected tracks", 1|8 )
 end
 reaper.UpdateArrange()
-
--------------------------------------------------------------------------------------------
-
-if done > 0 then
-  reaper.GetSet_LoopTimeRange( true, false, 0, 0, false )
-  reaper.Undo_OnStateChangeEx2( 0, "Insert pooled instances of one automation item for selected tracks", 1|8 , -1 )
-elseif done == 0 then
-  reaper.Undo_OnStateChangeEx2( 0, "Insert automation item", 1 , -1 )
-end
