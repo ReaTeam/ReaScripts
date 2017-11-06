db123a5a57bc3492c240bacfedd5f05ceeb0d6c6
amagalma <amagalma@gmail.com>
2017-11-06 16:35:07 +0200

    Update amagalma_Toggle Edit Group 4 (amagalma_Edit Group Management Utility).lua

--- Various/amagalma_Toggle Edit Group 4 (amagalma_Edit Group Management Utility).lua
+++ Various/amagalma_Toggle Edit Group 4 (amagalma_Edit Group Management Utility).lua
@@ -1,2 +1,7 @@
+-- @author amagalma
+-- @version 1.0
+-- @about
+--   # to be used with amagalma_Edit Group Management Utility
+
 reaper.SetExtState( "Edit Groups", "Active group", 4, false )
 reaper.defer(function () end )
