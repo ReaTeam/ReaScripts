f0a469f00a239ae13fb11fcbe79378847ea604be
amagalma <amagalma@gmail.com>
2017-11-15 21:32:51 +0200

    Delete amagalma_Toggle Edit Group 7 (amagalma_Edit Group Management Utility).lua

--- Various/amagalma_Toggle Edit Group 7 (amagalma_Edit Group Management Utility).lua
+++
@@ -1,7 +0,0 @@
--- @author amagalma
--- @version 1.0
--- @about
---   # to be used with amagalma_Edit Group Management Utility
-
-reaper.SetExtState( "Edit Groups", "Active group", 7, false )
-reaper.defer(function () end )
