35eb1993e849586690bed63c74a5a46d016b5a0a
EvilDragon <mario.kruselj@gmail.com>
2017-11-27 14:40:43 +0100

    Update js_Remove all CCs, pitch, channel pressure and program change events from all tracks.lua

--- Items Editing/js_Remove all CCs, pitch, channel pressure and program change events from all tracks.lua
+++ Items Editing/js_Remove all CCs, pitch, channel pressure and program change events from all tracks.lua
@@ -1,5 +1,5 @@
 --[[
-ReaScript name: js_Remove all CCs, pitch, channel pressure and program change events from all tracks.lua
+ReaScript name: Remove all CC, pitch, channel pressure and program change events from all tracks
 Version: 2.00
 Author: juliansader
 Website: http://forum.cockos.com/showthread.php?t=179065
