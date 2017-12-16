bcff55a55b86477f16b6703ed4a8c57604a999ae
amagalma <amagalma@gmail.com>
2017-12-16 02:04:01 +0200

    Update amagalma_Move edit cursor to previous visible grid line.lua

--- Cursor/amagalma_Move edit cursor to previous visible grid line.lua
+++ Cursor/amagalma_Move edit cursor to previous visible grid line.lua
@@ -1,16 +1,26 @@
 -- @description amagalma_Move edit cursor to previous visible grid line
 -- @author amagalma
--- @version 1.0
+-- @version 1.01
 -- @about
 --   # Moves the edit cursor to the previous grid line that is visible
 
+--[[
+ * Changelog:
+ * v1.01 (2017-12-16)
+  + fix Reaper crash when running the action with the edit cursor at the project start
+--]]
+
+function NoUndoPoint() end 
 reaper.Main_OnCommand(40755, 0) -- Snapping: Save snap state
 reaper.Main_OnCommand(40754, 0) -- Snapping: Enable snap
 local cursorpos = reaper.GetCursorPosition()
-local grid = cursorpos
-while (grid >= cursorpos) do
-    cursorpos = cursorpos - 0.05
-    grid = reaper.SnapToGrid(0, cursorpos)
+if cursorpos > 0 then
+  local grid = cursorpos
+  while (grid >= cursorpos) do
+      cursorpos = cursorpos - 0.05
+      grid = reaper.SnapToGrid(0, cursorpos)
+  end
+  reaper.SetEditCurPos(grid,1,1)
 end
-reaper.SetEditCurPos(grid,1,1)
 reaper.Main_OnCommand(40756, 0) -- Snapping: Restore snap state
+reaper.defer(NoUndoPoint)
