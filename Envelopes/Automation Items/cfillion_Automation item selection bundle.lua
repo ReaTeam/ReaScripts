4fca84a34b116413bf075af94453d45e1fff3c0e
cfillion <cfillion@users.noreply.github.com>
2018-01-25 13:57:26 -0500

    release automation item selection bundle 1.2

--- Envelopes/Automation Items/cfillion_Automation item selection bundle.lua
+++ Envelopes/Automation Items/cfillion_Automation item selection bundle.lua
@@ -1,8 +1,8 @@
 -- @description Automation item selection bundle
--- @version 1.1
+-- @version 1.2
 -- @changelog
---   Add actions for unselecting all automations items (+ in pool)
---   Add actions selecting/adding to selection the automation item under edit and mouse cursor
+--   Add "all tracks" variants to select/unselect all scripts
+--   Improve "under mouse cursor" variants to work over any envelope
 -- @author cfillion
 -- @provides
 --   . > cfillion_Select and move to next automation item.lua
@@ -20,16 +20,24 @@
 --   . > cfillion_Select all automation items.lua
 --   . > cfillion_Select all automation items in pool.lua
 --   . > cfillion_Select all automation items under edit cursor.lua
---   . > cfillion_Select all automation items under mouse cursor.lua
 --
 --   . > cfillion_Unselect all automation items.lua
 --   . > cfillion_Unselect all automation items in pool.lua
---   . > cfillion_Unselect all automation items under edit cursor.lua
---   . > cfillion_Unselect all automation items under mouse cursor.lua
+
+--   . > cfillion_Select all automation items (all tracks).lua
+--   . > cfillion_Select all automation items in pool (all tracks).lua
+--   . > cfillion_Select all automation items under edit cursor (all tracks).lua
+--
+--   . > cfillion_Unselect all automation items (all tracks).lua
+--   . > cfillion_Unselect all automation items in pool (all tracks).lua
+--   . > cfillion_Unselect all automation items under edit cursor (all tracks).lua
+--
+--   . > cfillion_Select all automation items under mouse cursor (any envelope).lua
+--   . > cfillion_Unselect all automation items under mouse cursor (any envelope).lua
 -- @about
 --   # Automation item selection bundle
 --
---   This package provides a total of 18 actions for selecting or unselecting
+--   This package provides many actions for selecting or unselecting
 --   automation items in the selected envelope lane. See the Contents tab for
 --   the list and for the exact name of the actions.
 --
@@ -51,10 +59,11 @@ local moveMode = name:match('move to')
 local poolMode = name:match('pool')
 local addToSelMode = name:match('Add.+to selection')
 local prevMode = name:match('previous')
-local entireBucketMode = name:match('all')
+local entireBucketMode = name:match('all automation')
 local unselectMode = name:match('Unselect')
 local editCursorMode = name:match('under edit cursor')
 local mouseCursorMode = name:match('under mouse cursor')
+local allTracksMode = name:match('all tracks') or name:match('any envelope')
 
 function testCursorPosition(env, startTime, endTime)
   local curPos
@@ -75,22 +84,55 @@ function testCursorPosition(env, startTime, endTime)
   return startTime <= curPos and endTime >= curPos
 end
 
-local env = reaper.GetSelectedEnvelope(0)
-if not env then
-  reaper.defer(function() end)
-  return
+function enumSelectedEnvelope()
+  local env = reaper.GetSelectedEnvelope(0)
+  if not env then return function() end end
+
+  local i, count = -1, reaper.CountAutomationItems(env)
+
+  return function()
+    i = i + 1
+
+    if i < count then
+      return env, i
+    end
+  end
 end
 
-local count = reaper.CountAutomationItems(env)
-if count < 1 then
-  reaper.defer(function() end)
-  return
+function enumAllEnvelopes()
+  local trackCount = reaper.CountTracks(0)
+
+  local ti, ei, ai = 0, 0, 0
+  local track, env
+  local envCount, aiCount = 0, 0
+
+  return function()
+    while ai >= aiCount do
+      while ei >= envCount do
+        if ti < trackCount then
+          track = reaper.GetTrack(0, ti)
+          ti, ei = ti + 1, 0
+          envCount = reaper.CountTrackEnvelopes(track)
+        else
+          return
+        end
+      end
+
+      env = reaper.GetTrackEnvelope(track, ei)
+      ei, ai = ei + 1, 0
+      aiCount = reaper.CountAutomationItems(env)
+    end
+
+    local id = ai
+    ai = ai + 1
+    return env, id
+  end
 end
 
 local buckets = {}
 local currentSel, currentBucket = {}, 0
 
-for i=0,count-1 do
+for env, i in (allTracksMode and enumAllEnvelopes or enumSelectedEnvelope)() do
   local selected = 1 == reaper.GetSetAutomationItemInfo(env, i, 'D_UISEL', 0, false)
   local bucketId = 0
   local startTime = reaper.GetSetAutomationItemInfo(env, i, 'D_POSITION', 0, false)
@@ -100,17 +142,17 @@ for i=0,count-1 do
   if poolMode then
     bucketId = reaper.GetSetAutomationItemInfo(env, i, 'D_POOL_ID', 0, false)
   end
-  
+
   if selected and poolMode and currentBucket == 0 then
     currentBucket = bucketId
   end
 
   if selected then
-    table.insert(currentSel, i)
+    table.insert(currentSel, {env=env, id=i})
   end
 
   if (not selected or entireBucketMode or unselectMode) and underCursor then
-    local ai = {id=i, pos=startTime}
+    local ai = {env=env, id=i, pos=startTime}
 
     if buckets[bucketId] then
       table.insert(buckets[bucketId], ai)
@@ -138,8 +180,8 @@ end
 -- find next or previous target
 if #currentSel > 0 and not entireBucketMode then
   if prevMode then
-    local firstSel = currentSel[1]
-        
+    local firstSel = currentSel[1].id
+
     for ri=0,#bucket-1 do
       local bid = #bucket - ri
       if bucket[bid].id < firstSel then
@@ -148,8 +190,8 @@ if #currentSel > 0 and not entireBucketMode then
       end
     end
   else
-    local lastSel = currentSel[#currentSel]
-    
+    local lastSel = currentSel[#currentSel].id
+
     for i,ai in ipairs(bucket) do
       if ai.id > lastSel then
         target = i
@@ -163,9 +205,9 @@ reaper.Undo_BeginBlock()
 
 if not addToSelMode and not unselectMode then
   for _,ai in ipairs(currentSel) do
-    reaper.GetSetAutomationItemInfo(env, ai, 'D_UISEL', 0, true)
+    reaper.GetSetAutomationItemInfo(ai.env, ai.id, 'D_UISEL', 0, true)
   end
-  
+
   if moveMode then
     reaper.SetEditCurPos(bucket[target].pos, true, false)
   end
@@ -175,10 +217,10 @@ local sel = unselectMode and 0 or 1
 
 if entireBucketMode then
   for _,ai in ipairs(bucket) do
-    reaper.GetSetAutomationItemInfo(env, ai.id, 'D_UISEL', sel, true)
+    reaper.GetSetAutomationItemInfo(ai.env, ai.id, 'D_UISEL', sel, true)
   end
 else
-  reaper.GetSetAutomationItemInfo(env, bucket[target].id, 'D_UISEL', sel, true)
+  reaper.GetSetAutomationItemInfo(bucket[target].env, bucket[target].id, 'D_UISEL', sel, true)
 end
 
 reaper.Undo_EndBlock(name, UNDO_STATE_TRACKCFG)
