bb331169b423d959f53b98a04e17766ca5ee2fd9
cfillion <cfillion@users.noreply.github.com>
2018-01-30 07:23:51 -0500

    release set timecode at edit cursor v1.1

--- Project Properties/cfillion_Set timecode at edit cursor.lua
+++ Project Properties/cfillion_Set timecode at edit cursor.lua
@@ -1,18 +1,30 @@
 -- @description Set timecode at edit cursor
--- @version 1.0
+-- @version 1.1
+-- @changelog Split into three actions for hh:mm:ss.sss, seconds and h:m:s:f input [p=1947239]
 -- @author cfillion
 -- @links
 --   cfillion.ca https://cfillion.ca
 --   Request Thread https://forum.cockos.com/showthread.php?t=202578
 -- @screenshot https://i.imgur.com/uly6oy5.gif
 -- @donate https://www.paypal.com/cgi-bin/webscr?business=T3DEWBQJAV7WL&cmd=_donations&currency_code=CAD
+-- @provides
+--   . > cfillion_Set timecode at edit cursor (hh:mm:ss.sss).lua
+--   . > cfillion_Set timecode at edit cursor (seconds).lua
+--   . > cfillion_Set timecode at edit cursor (h:m:s:f).lua
 
 local SCRIPT_NAME = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
 
+local MODE = ({
+  ['hh:mm:ss.sss'] = 0,
+  ['seconds']      = 3,
+  ['h:m:s:f']      = 5,
+})[SCRIPT_NAME:match('%(([^%)]+)%)')]
+
+assert(MODE, "Internal error: unknown timecode format")
 assert(reaper.SNM_GetDoubleConfigVar, "SWS is required to use this script")
 
 local curpos = reaper.GetCursorPosition()
-local timecode = reaper.format_timestr_pos(curpos, '', 0)
+local timecode = reaper.format_timestr_pos(curpos, '', MODE)
 local ok, csv = reaper.GetUserInputs(SCRIPT_NAME, 1, "Timecode,extrawidth=50", timecode)
 
 if not ok then
@@ -20,7 +32,7 @@ if not ok then
   return
 end
 
-timecode = reaper.parse_timestr(csv)
+timecode = reaper.parse_timestr_len(csv, 0, MODE)
 
 reaper.SNM_SetDoubleConfigVar('projtimeoffs', timecode - curpos)
 reaper.UpdateTimeline()
