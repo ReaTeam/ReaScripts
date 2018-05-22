287057ef4eb163e9c9b614e64be4593f4002fdb3
Lokasenna <jalovatt@hotmail.com>
2018-05-22 08:02:18 -0600

    Added "Strength" value for Start and Length adjustments, to allow only moving notes partway from their original state Allow Ctrl+Z to undo the applied values Removed "End Position", replaced with "Length"

--- MIDI Editor/Lokasenna_Copy values from selected midi notes.lua
+++ MIDI Editor/Lokasenna_Copy values from selected midi notes.lua
@@ -1,11 +1,13 @@
 --[[
 Description: Copy values from selected MIDI notes
-Version: 1.10
+Version: 1.15
 Author: Lokasenna
 Donation: https://paypal.me/Lokasenna
 Changelog:
-    Remembers the last-selected values
-    Added a small message to confirm that notes were copied
+    Added "Strength" value for Start and Length adjustments, to allow only
+    moving notes partway from their original state
+    Allow Ctrl+Z to undo the applied values
+    Removed "End Position", replaced with "Length"    
 Links:
 	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
 About: 
@@ -565,8 +567,7 @@ GUI.New = function (name, elm, ...)
     local elm = type(elm) == "string"   and GUI[elm]
                                         or  elm
 
-
-    if not elm then
+    if not elm or type(elm) ~= "table" then
 		reaper.ShowMessageBox(  "Unable to create element '"..tostring(name)..
                                 "'.\nClass '"..tostring(elm).."' isn't available.", 
                                 "GUI Error", 0)
@@ -1994,7 +1995,7 @@ end
 GUI.triangle = function (fill, ...)
 	
 	-- Pass any calls for a filled triangle on to the original function
-	if fill then
+	if fill == 1 then
 		
 		gfx.triangle(...)
 		
@@ -2518,7 +2519,7 @@ function Option:new(name, z, x, y, w, h, caption, opts, dir, pad)
     else
     
         local tempidx = 1
-        for word in string.gmatch(opts, '([^,]+)') do
+        for word in string.gmatch(opts, '([^,]*)') do
             option.optarray[tempidx] = word
             tempidx = tempidx + 1
         end
@@ -2635,9 +2636,7 @@ function Option:drawoptions()
 	local pad = self.pad
     
     -- Bump everything down for the caption
-    if self.caption and self.caption ~= "" then 
-        y = y + self.cap_h + 1.5 * pad 
-    end
+    y = y + ((self.caption and self.caption ~= "") and self.cap_h or 0) + 1.5 * pad 
 
     -- Bump the options down more for horizontal options
     -- with the text on top
@@ -2645,9 +2644,9 @@ function Option:drawoptions()
         y = y + self.cap_h + 2*pad 
     end
 
-	local size = self.opt_size
+	local opt_size = self.opt_size
     
-    local adj = size + pad
+    local adj = opt_size + pad
 
     local str, opt_x, opt_y
 
@@ -2663,9 +2662,9 @@ function Option:drawoptions()
             opt_y = y + (i - 1) * (horz and 0 or adj)
                 
 			-- Draw the option bubble
-            self:drawoption(opt_x, opt_y, size, self:isoptselected(i))
+            self:drawoption(opt_x, opt_y, opt_size, self:isoptselected(i))
 
-            self:drawvalue(opt_x,opt_y, size, str)
+            self:drawvalue(opt_x,opt_y, opt_size, str)
             
 		end
 		
@@ -2686,8 +2685,10 @@ end
 
 function Option:drawvalue(opt_x, opt_y, size, str)
 
+    if not str or str == "" then return end
+    
 	GUI.font(self.font_b) 
-
+    
     local str_w, str_h = gfx.measurestr(str)
     
     if self.dir == "h" then
@@ -3378,15 +3379,429 @@ end
 
 ---- End of file: F:/Github Repositories/Lokasenna_GUI/Classes/Class - Frame.lua ----
 
+
+
+---- Beginning of file: F:/Github Repositories/Lokasenna_GUI/Classes/Class - Knob.lua ----
+
+--[[	Lokasenna_GUI - Knob class.
+
+	---- User parameters ----
+	
+	(name, z, x, y, w, caption, min, max, default,[ inc, vals])
+	
+Required:	
+z				Element depth, used for hiding and disabling layers. 1 is the highest.	
+x, y, w			Coordinates of top-left corner, width. Height is fixed.
+caption			Label / question
+min, max		Minimum and maximum values
+default			Default step for the knob. Steps are counted from min to max starting at 0.
+
+
+Optional:
+inc             Amount to increment the value per step. Defaults to 1.
+
+vals			Boolean, defaults to True. Display value labels?
+				For knobs with a lot of steps, i.e. Pan from -100 to +100, set this
+				to false and use a label to read the value, update the Knob's caption
+
+
+                Example:
+                
+                    A knob from 0 to 11, defaulting to 11, with a step size of 0.25:
+                    
+                        min     = 0
+                        max     = 11
+                        default = 44
+                        inc     = 0.25
+
+
+
+Additional:
+bg				Color to be drawn underneath the label. Defaults to "wnd_bg"
+font_a          Caption font
+font_b          Value font
+col_txt         Text color
+col_head        Knob head color
+col_body        Knob body color
+cap_x, cap_y    Offset values for the knob's caption.
+output			Allows the value labels to be modified; accepts several different var types:
+				
+				string		Replaces all of the value labels
+				number
+				table		Replaces each value label with output[step], with the steps
+							being numbered as above
+				functions	Replaces each value with the returned value from
+							output(step), numbered as above
+							
+				Output will always count steps starting from 0, so you'll have to account for minimum
+				values in the final string yourself.
+	
+	
+Extra methods:
+
+GUI.Val()		Returns the current display value of the knob. i.e. 
+				
+					For a Pan knob, -100 to +100, with 201 steps,
+					GUI.Val("my_knob") will return an integer from -100 to +100
+					
+GUI.Val(new)	Sets the display value of the knob, as above.				
+	
+]]--
+
+if not GUI then
+	reaper.ShowMessageBox("Couldn't access GUI functions.\n\nLokasenna_GUI - Core.lua must be loaded prior to any classes.", "Library Error", 0)
+	missing_lib = true
+	return 0
+end
+
+-- Knob - New.
+GUI.Knob = GUI.Element:new()
+function GUI.Knob:new(name, z, x, y, w, caption, min, max, default, inc, vals)
+	
+	local Knob = {}
+	
+	Knob.name = name
+	Knob.type = "Knob"
+	
+	Knob.z = z
+	GUI.redraw_z[z] = true	
+	
+	Knob.x, Knob.y, Knob.w, Knob.h = x, y, w, w
+
+	Knob.caption = caption
+	Knob.bg = "wnd_bg"
+    
+    Knob.cap_x, Knob.cap_y = 0, 0
+	
+	Knob.font_a = 3
+	Knob.font_b = 4
+	
+	Knob.col_txt = "txt"
+	Knob.col_head = "elm_fill"
+	Knob.col_body = "elm_frame"
+    
+	Knob.min, Knob.max = min, max
+    Knob.inc = inc or 1
+    Knob.steps = math.abs(max - min) / Knob.inc
+    
+    function Knob:formatretval(val)
+        
+        local decimal = tonumber(string.match(val, "%.(.*)") or 0)
+        local places = decimal ~= 0 and string.len( decimal) or 0
+        return string.format("%." .. places .. "f", val)
+        
+    end    
+	
+	Knob.vals = vals
+	
+	-- Determine the step angle
+	Knob.stepangle = (3 / 2) / Knob.steps
+	
+	Knob.default, Knob.curstep = default, default
+    
+	Knob.curval = Knob.curstep / Knob.steps    
+	
+    Knob.retval = Knob:formatretval(
+                ((max - min) / Knob.steps) * Knob.curstep + min
+                                    )
+
+
+	
+	setmetatable(Knob, self)
+	self.__index = self
+	return Knob
+
+end
+
+
+function GUI.Knob:init()
+	
+	self.buff = self.buff or GUI.GetBuffer()
+	
+	gfx.dest = self.buff
+	gfx.setimgdim(self.buff, -1, -1)
+
+	-- Figure out the points of the triangle
+
+	local r = self.w / 2
+	local rp = r * 1.5
+	local curangle = 0
+	local o = rp + 1
+	
+	local w = 2 * rp + 2
+
+	gfx.setimgdim(self.buff, 2*w, w)
+
+	local side_angle = (math.acos(0.666667) / GUI.pi) * 0.9
+
+	local Ax, Ay = GUI.polar2cart(curangle, rp, o, o)
+    local Bx, By = GUI.polar2cart(curangle + side_angle, r - 1, o, o)
+	local Cx, Cy = GUI.polar2cart(curangle - side_angle, r - 1, o, o)
+		
+	-- Head
+	GUI.color(self.col_head)
+	GUI.triangle(1, Ax, Ay, Bx, By, Cx, Cy)
+	GUI.color("elm_outline")
+	GUI.triangle(0, Ax, Ay, Bx, By, Cx, Cy)	
+	
+	-- Body
+	GUI.color(self.col_body)
+	gfx.circle(o, o, r, 1)
+	GUI.color("elm_outline")
+	gfx.circle(o, o, r, 0)		
+
+	--gfx.blit(source, scale, rotation[, srcx, srcy, srcw, srch, destx, desty, destw, desth, rotxoffs, rotyoffs] )
+	gfx.blit(self.buff, 1, 0, 0, 0, w, w, w + 1, 0)
+	gfx.muladdrect(w + 1, 0, w, w, 0, 0, 0, GUI.colors["shadow"][4])
+
+end
+
+
+function GUI.Knob:ondelete()
+	
+	GUI.FreeBuffer(self.buff)	
+	
+end
+
+
+-- Knob - Draw
+function GUI.Knob:draw()
+	
+	local x, y = self.x, self.y
+	
+	local r = self.w / 2
+	local o = {x = x + r, y = y + r}
+
+
+	-- Value labels
+	if self.vals then self:drawvals(o, r) end
+
+    if self.caption and self.caption ~= "" then self:drawcaption(o, r) end
+
+
+	-- Figure out where the knob is pointing
+	local curangle = (-5 / 4) + (self.curstep * self.stepangle)
+
+	local blit_w = 3 * r + 2
+	local blit_x = 1.5 * r
+
+	-- Shadow
+	for i = 1, GUI.shadow_dist do
+		
+		gfx.blit(   self.buff, 1, curangle * GUI.pi, 
+                    blit_w + 1, 0, blit_w, blit_w, 
+                    o.x - blit_x + i - 1, o.y - blit_x + i - 1)	
+		
+	end
+	
+	-- Body
+	gfx.blit(   self.buff, 1, curangle * GUI.pi,
+                0, 0, blit_w, blit_w, 
+                o.x - blit_x - 1, o.y - blit_x - 1)
+
+end
+
+
+-- Knob - Get/set value
+function GUI.Knob:val(newval)
+	
+	if newval then
+        
+        self:setcurstep(newval)
+
+		self:redraw()
+
+	else
+		return self.retval
+	end	
+
+end
+
+
+-- Knob - Dragging.
+function GUI.Knob:ondrag()
+	
+	local y = GUI.mouse.y
+	local ly = GUI.mouse.ly
+
+	-- Ctrl?
+	local ctrl = GUI.mouse.cap&4==4
+	
+	-- Multiplier for how fast the knob turns. Higher = slower
+	--					Ctrl	Normal
+	local adj = ctrl and 1200 or 150
+	
+    self:setcurval( GUI.clamp(self.curval + ((ly - y) / adj), 0, 1) )
+    
+    --[[
+	self.curval = self.curval + ((ly - y) / adj)
+	if self.curval > 1 then self.curval = 1 end
+	if self.curval < 0 then self.curval = 0 end
+	
+    
+    
+	self.curstep = GUI.round(self.curval * self.steps)
+	
+	self.retval = GUI.round(((self.max - self.min) / self.steps) * self.curstep + self.min)
+    ]]--
+	self:redraw()
+
+end
+
+
+-- Knob - Doubleclick
+function GUI.Knob:ondoubleclick()
+	--[[
+	self.curstep = self.default
+	self.curval = self.curstep / self.steps
+	self.retval = GUI.round(((self.max - self.min) / self.steps) * self.curstep + self.min)
+	]]--
+    
+    self:setcurstep(self.default)
+    
+	self:redraw()
+	
+end
+
+
+-- Knob - Mousewheel
+function GUI.Knob:onwheel()
+
+	local ctrl = GUI.mouse.cap&4==4
+	
+	-- How many steps per wheel-step
+	local fine = 1
+	local coarse = math.max( GUI.round(self.steps / 30), 1)
+
+	local adj = ctrl and fine or coarse
+    
+    self:setcurval( GUI.clamp( self.curval + (GUI.mouse.inc * adj / self.steps), 0, 1))
+    
+	self:redraw()
+
+end
+
+
+
+------------------------------------
+-------- Drawing methods -----------
+------------------------------------
+
+function GUI.Knob:drawcaption(o, r)
+    
+    local str = self.caption
+    
+	GUI.font(self.font_a)
+	local cx, cy = GUI.polar2cart(1/2, r * 2, o.x, o.y)
+	local str_w, str_h = gfx.measurestr(str)
+	gfx.x, gfx.y = cx - str_w / 2 + self.cap_x, cy - str_h / 2  + 8 + self.cap_y
+	GUI.text_bg(str, self.bg)
+	GUI.shadow(str, self.col_txt, "shadow")
+    
+end
+
+
+function GUI.Knob:drawvals(o, r)        
+        
+    for i = 0, self.steps do
+        
+        local angle = (-5 / 4 ) + (i * self.stepangle)
+        
+        -- Highlight the current value
+        if i == self.curstep then
+            GUI.color(self.col_head)
+            GUI.font({GUI.fonts[self.font_b][1], GUI.fonts[self.font_b][2] * 1.2, "b"})
+        else
+            GUI.color(self.col_txt)
+            GUI.font(self.font_b)
+        end
+        
+        --local output = (i * self.inc) + self.min
+        local output = self:formatretval( i * self.inc + self.min )
+
+        if self.output then
+            local t = type(self.output)
+
+            if t == "string" or t == "number" then
+                output = self.output
+            elseif t == "table" then
+                output = self.output[output]
+            elseif t == "function" then
+                output = self.output(output)
+            end
+        end
+        
+        -- Avoid any crashes from weird user data
+        output = tostring(output)
+
+        if output ~= "" then
+            
+            local str_w, str_h = gfx.measurestr(output)
+            local cx, cy = GUI.polar2cart(angle, r * 2, o.x, o.y)		
+            gfx.x, gfx.y = cx - str_w / 2, cy - str_h / 2
+            GUI.text_bg(output, self.bg)
+            gfx.drawstr(output)
+        end			
+            
+    end
+
+end
+
+
+
+
+------------------------------------
+-------- Value helpers -------------
+------------------------------------
+
+function GUI.Knob:setcurstep(step)
+
+    self.curstep = step
+    self.curval = self.curstep / self.steps
+    self:setretval()
+
+end
+
+
+function GUI.Knob:setcurval(val)
+    
+    self.curval = val
+    self.curstep = GUI.round(val * self.steps)
+    self:setretval()
+    
+end
+
+
+function GUI.Knob:setretval()
+
+    self.retval = self:formatretval(self.inc * self.curstep + self.min)
+    
+end
+
+---- End of file: F:/Github Repositories/Lokasenna_GUI/Classes/Class - Knob.lua ----
+
 ---- End of libraries ----
 
 
 if missing_lib then return 0 end
 
-GUI.elms_hide[4] = true
+GUI.elms_hide[5] = true
+
+
+
+local function show_warning(show)
+    
+    GUI.elms.frm_nope.z = show and 2 or 5
+    GUI.elms.lbl_nope.z = show and 1 or 5
+    GUI.redraw_z[1] = true    
+    
+end
 
 
 local copied_notes = {}
+
+
+
 
 local function btn_get()
     
@@ -3394,12 +3809,14 @@ local function btn_get()
     local hwnd = reaper.MIDIEditor_GetActive()
     if not hwnd then
         if not startup then reaper.MB("This script needs an open MIDI editor.", "Whoops!", 0) end
+        show_warning(true)
         return
     end
 
     local take = reaper.MIDIEditor_GetTake( hwnd )
     if not take then
         if not startup then reaper.MB("No MIDI item found.", "Whoops!", 0) end
+        show_warning(true)
         return
     end
     
@@ -3428,6 +3845,7 @@ local function btn_get()
     
     if #copied_notes == 0 then
         reaper.MB("No selected notes found.", "Whoops!", 0)
+        show_warning(true)
         return
     end
     
@@ -3436,10 +3854,8 @@ local function btn_get()
     local _, track_name = reaper.GetTrackName(track, "")
     local item_name = reaper.GetTakeName(take)
     GUI.Val("lbl_got", "Got " .. #copied_notes .. " notes from " .. tostring(track_name) .. ", " .. tostring(item_name))
-    
-    GUI.elms.frm_nope.z = 4
-    GUI.elms.lbl_nope.z = 4
-    GUI.redraw_z[1] = true    
+
+    show_warning(false)
     
 end
 
@@ -3458,7 +3874,15 @@ local function btn_set()
         return
     end
 
-    local opts = GUI.Val("chk_vals")
+    -- Read the values out with the indices offset to match GetNote/SetNote
+    local opts = {}
+    for k, v in pairs( GUI.Val("chk_vals") ) do
+        opts[k + 2] = v
+    end
+    
+    local strength = GUI.Val("knb_strength") / 100
+
+    reaper.Undo_BeginBlock()
 
     local count = 0
     local idx = -2
@@ -3471,15 +3895,10 @@ local function btn_set()
         if idx == -1 or count == #copied_notes then break end
 
         local note = {reaper.MIDI_GetNote(take, idx)}
+        local copy = copied_notes[idx + 1]
 
         --GUI.Msg("working on idx " .. idx)
 
-
-        for i = 3, 8 do
-            --GUI.Msg("copied[idx+1][i] = " .. (not not copied_notes[idx+1] and tostring(copied_notes[idx+1][i]) or "nil"))
-            note[i] = opts[i - 2] and copied_notes[idx + 1][i] or note[i]
-            
-        end
 
         --[[
             note[1] = retval        boolean
@@ -3491,8 +3910,42 @@ local function btn_set()
             [7] = pitch         number 
             [8] = velocity      number 
         ]]--
+        note[3] = opts[3] and copy[3] or note[3]
 
-
+        -- Just Start
+        if opts[4] and not opts[5] then
+            
+            local len = note[5] - note[4]
+            note[4] = note[4] + (copy[4] - note[4]) * strength
+            note[5] = note[4] + len
+            --note[5] = copy[4] + (note[5] - note[4])
+            --note[4] = copy[4]
+            
+        -- Just Length
+        elseif opts[5] and not opts[4] then
+        
+            --old:
+            -- len = (copy[5] - copy[4])
+            local len_note = note[5] - note[4]
+            local len_copy = copy[5] - copy[4]
+            local len = len_note + (len_copy - len_note) * strength
+            note[5] = note[4] + len
+            
+        -- Start and Length
+        elseif opts[4] and opts[5] then
+        
+            local len_note = note[5] - note[4]
+            local len_copy = copy[5] - copy[4]
+            local len = len_note + (len_copy - len_note) * strength
+         
+            note[4] = note[4] + (copy[4] - note[4]) * strength
+            note[5] = note[4] + len
+        
+        end
+        
+        note[6] = opts[6] and copy[6] or note[6]
+        note[7] = opts[7] and copy[7] or note[7]
+        note[8] = opts[8] and copy[8] or note[8]
         
         reaper.MIDI_SetNote( take, idx, note[2], note[3], note[4], note[5], note[6], note[7], note[8], true)
 
@@ -3508,7 +3961,8 @@ local function btn_set()
     end
     
     reaper.MIDI_Sort( take )    
-    
+   
+    reaper.Undo_EndBlock("Copy values from selected midi notes", 0)
 end
 
 
@@ -3516,12 +3970,10 @@ local function get_ext_vals()
     
     local vals = {}
     local str = reaper.GetExtState("Lokasenna_Copy values from selected midi notes", "Options")
-
     for k in string.gmatch(str, "%d+") do
         vals[tonumber(k)] = true
     end
-    
-    return vals
+    GUI.Val("chk_vals", vals)
     
 end
 
@@ -3554,10 +4006,50 @@ GUI.New("btn_set", "Button", 3, 120, 288, 80, 24, "Apply values", btn_set)
 
 GUI.New("lbl_got", "Label", 3, 16, 52, "", true, 4)
 
-GUI.New("chk_vals", "Checklist", 3, 80, 88, 160, 180, "Values to apply:", "Mute,Start Position,End Position,Channel,Pitch,Velocity", "v", 6)
+GUI.New("chk_vals", "Checklist", 4, 48, 88, 224, 180, "Values to apply:", "Mute,Position,Length,Channel,Pitch,Velocity", "v", 6)
+
+GUI.New("knb_strength", "Knob", 3, 208, 136, 32, "Strength", 0, 100, 100, 1, false)
 
 GUI.New("frm_nope", "Frame", 2, 0, 48, 400, 308, true, true, "wnd_bg", 0)
 GUI.New("lbl_nope", "Label", 1, 88, 96, "No notes copied", true, 2)
+
+
+local line_x1, line_x2, line_x3, line_y1, line_y2, line_y3
+function GUI.elms.chk_vals:init()
+    
+    GUI.Checklist.init(self)
+    
+    line_x1 = self.x + self.pad + self.opt_size
+    line_x2 = line_x1 + 80
+    line_x3 = GUI.elms.knb_strength.x + 8
+    line_y1 = self.y + self.cap_h + 2.5*self.pad + 1.5*self.opt_size 
+    line_y2 = line_y1 + self.opt_size + self.pad
+    line_y3 = line_y1 + (line_y2 - line_y1) / 2
+    
+    GUI.elms.knb_strength.y = line_y3 - (GUI.elms.knb_strength.h / 2)
+    
+end
+
+
+function GUI.elms.chk_vals:draw()
+       
+    GUI.color("elm_frame")
+    gfx.line(line_x1, line_y1, line_x2, line_y1)
+    gfx.line(line_x1, line_y2, line_x2, line_y2)
+    gfx.line(line_x2, line_y1, line_x2, line_y2)
+    gfx.line(line_x2, line_y3, line_x3, line_y3)
+
+    GUI.Checklist.draw(self)
+    
+end
+
+
+function GUI.elms.knb_strength:draw()
+    
+    self.caption = "Strength: " .. self.retval .. "%"
+    GUI.Knob.draw(self)
+
+end
 
 
 function GUI.elms.lbl_nope:init()
@@ -3588,9 +4080,22 @@ btn_get()
 
 startup = false
 
-GUI.Val("chk_vals", get_ext_vals())
+get_ext_vals()
 
 GUI.exit = set_ext_vals
+
+local function check_undo()
+    
+    -- Ctrl
+    if GUI.mouse.cap & 4 == 4 and GUI.char == 26 then
+        
+        reaper.Undo_DoUndo2(-1)
+        
+    end  
+    
+end
+GUI.func = check_undo
+GUI.freq = 0
 
 GUI.Init()
 GUI.Main()
