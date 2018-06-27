0e6b47ba7ac8bb3115247996766da7ca8b31ad40
cfillion <cfillion@users.noreply.github.com>
2018-06-26 22:27:56 -0400

    fix packaging of Lokasenna's GUI library v2

---
+++ Development/Lokasenna_GUI library v2 (developer tools).lua
@@ -0,0 +1,17 @@
+--[[
+Description: Lokasenna's GUI library v2 for Lua (developer tools)
+Version: 2.9.0
+Changelog: First stable release
+Author: Lokasenna
+Provides: [nomain] Lokasenna_GUI v2/Developer Tools/**/*
+Metapackage: true
+Donation: https://paypal.me/Lokasenna
+Links:
+    https://forum.cockos.com/showthread.php?t=177772
+    Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+About:
+    Provides a framework allowing Lua scripts to use a graphical interface, since Reaper
+    has no ability to do so natively.
+--]]
+
+-- Licensed under the GNU GPL v3
--- Development/Lokasenna_GUI library v2.lua
+++ Development/Lokasenna_GUI library v2.lua
@@ -1,13 +1,14 @@
 --[[
-Description: Lokasenna_GUI v2
+Description: Lokasenna's GUI library v2 for Lua
 Version: 2.9.0
+Changelog: First stable release
 Author: Lokasenna
+Provides: [nomain] Lokasenna_GUI v2/Library/**/*
+Metapackage: true
 Donation: https://paypal.me/Lokasenna
-Changelog:
-    First stable release
 Links:
     Forum Thread https://forum.cockos.com/showthread.php?t=177772
-	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+    Lokasenna's Website http://forum.cockos.com/member.php?u=10417
 About:
     Provides a framework allowing Lua scripts to use a graphical interface, since Reaper
     has no ability to do so natively.
@@ -21,11 +22,6 @@ About:
       REAPER\Scripts\ReaTeam Scripts\Development\Lokasenna_GUI v2\Library
     - Run "Set Lokasenna_GUI v2 library path.lua"
     - Done!
-Provides:
-    [nomain] Lokasenna_GUI v2/Library/*.*
-    [nomain] Lokasenna_GUI v2/Library/**/*.*
-Metapackage: true
-    
 --]]
 
 -- Licensed under the GNU GPL v3
