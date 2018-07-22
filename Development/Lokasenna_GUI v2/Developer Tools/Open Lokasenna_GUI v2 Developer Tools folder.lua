98906f0470ed18219f888b5069bb1140902ab1e3
Lokasenna <jalovatt@hotmail.com>
2018-07-21 19:55:13 -0600

    Add script to access Dev. Tools folder

---
+++ Development/Lokasenna_GUI v2/Developer Tools/Open Lokasenna_GUI v2 Developer Tools folder.lua
@@ -0,0 +1,17 @@
+-- NoIndex: true
+
+-- Opens the folder containing Lokasenna_GUI's Developer Tools package
+
+local path = ({reaper.get_action_context()})[2]:match("(.-)([^/\\]+).lua$")
+
+local function open_file(path)
+    
+    local OS = reaper.GetOS()
+    local cmd = ( string.match(OS, "OSX") and "open" or "start" ) ..
+                ' "" "' .. path .. '"'
+    
+    os.execute(cmd)
+
+end
+
+open_file(path)
