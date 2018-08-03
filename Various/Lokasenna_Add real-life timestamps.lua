912fb89496fac15580256bdaa3e11833995e203d
Lokasenna <jalovatt@hotmail.com>
2018-08-02 22:18:42 -0600

    Fix: Better crash protection

--- Various/Lokasenna_Add real-life timestamps.lua
+++ Various/Lokasenna_Add real-life timestamps.lua
@@ -1,10 +1,10 @@
 --[[
     Description: Add real-life timestamps
-    Version: 1.0.1
+    Version: 1.0.2
     Author: Lokasenna
     Donation: https://paypal.me/Lokasenna
     Changelog:
-        Fix: Typo was making the script do nothing
+        Fix: Better crash protection
     Links:
         Lokasenna's Website http://forum.cockos.com/member.php?u=10417
     About:
@@ -133,12 +133,18 @@ local function parseSettings()
     --  Parse settings
     local mults = {1, 60, 3600}
     settings.interval = GUI.Val("txt_interval")
-    if not tonumber(settings.interval) then return end
+    if not tonumber(settings.interval) then 
+        reaper.MB("Please enter a valid interval.", "Whoops!", 0)
+        return 
+    end
 
     settings.interval = settings.interval * mults[GUI.Val("mnu_interval")]
 
     settings.start = stringToTime( GUI.Val("txt_start") )
-    if not settings.start then return end
+    if not settings.start then
+        reaper.MB("Please enter a valid start time. (HH:MM:SS)", "Whoops!", 0)
+        return 
+    end
 
     settings.format = GUI.Val("mnu_format")
 
@@ -221,7 +227,7 @@ local function doMarkerLoop()
 
     end
 
-    reaper.Undo_EndBlock("Add markers with real time", -1)
+    reaper.Undo_EndBlock("Add real-life timestamps", -1)
 
 end
 
