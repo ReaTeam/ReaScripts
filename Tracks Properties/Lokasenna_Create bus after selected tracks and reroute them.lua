cc48b8d9ba6db3f854289d8e9ded7e341472e883
Lokasenna <jalovatt@hotmail.com>
2018-09-08 11:12:11 -0600

    Initial release

---
+++ Tracks Properties/Lokasenna_Create bus after selected tracks and reroute them.lua
@@ -0,0 +1,70 @@
+--[[
+Description: Create bus after selected tracks and reroute them
+Version: 1.0.0
+Author: Lokasenna
+Donation: https://paypal.me/Lokasenna
+Changelog:
+	Make sure sends are at unity gain, post-fader
+Links:
+	Lokasenna's Website http://forum.cockos.com/member.php?u=10417
+About: 
+	Inserts a new track before the selected tracks, labelled "Bus", then
+	reroutes all selected tracks to it and disables their
+  Master/Parent sends.
+  
+  This will only work properly for mono/stereo audio. Reaper makes
+  multichannel support impractical. :(
+	
+--]]
+
+-- Licensed under the GNU GPL v3
+
+local function dMsg(str)
+	if debug_mode then reaper.ShowConsoleMsg(tostring(str).."\n") end
+end
+
+
+local last_sel = reaper.GetSelectedTrack(0, reaper.CountSelectedTracks(0)-1)
+if not last_sel then return end
+
+reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh( 1 )
+
+
+local idx = reaper.GetMediaTrackInfo_Value(last_sel, "IP_TRACKNUMBER")
+reaper.InsertTrackAtIndex(idx, true)
+local bus = reaper.GetTrack(0, idx)
+reaper.GetSetMediaTrackInfo_String( bus, "P_NAME", "Bus", true )
+
+
+-- Loop through all tracks in the project
+for i = 0, reaper.CountSelectedTracks(0) - 1 do
+	
+	dMsg("looking at track "..i)
+	
+	local tr = reaper.GetSelectedTrack(0, i)
+	
+	dMsg("\tdepth = "..reaper.GetTrackDepth(tr))
+	dMsg("\tp_send = "..tostring( reaper.GetMediaTrackInfo_Value(tr, "B_MAINSEND") ) )
+	
+	dMsg("\trerouting")
+	
+	-- Disable Master Out
+	reaper.SetMediaTrackInfo_Value(tr, "B_MAINSEND", 0)
+		
+    local send = reaper.CreateTrackSend(tr, bus)
+    
+    -- Make sure send is at unity, post-fader (overriding default send values)
+    reaper.SetTrackSendInfo_Value(tr, 0, send, "D_VOL", 1)
+    reaper.SetTrackSendInfo_Value(tr, 0, send, "I_SENDMODE", 0)    
+	
+end
+
+
+reaper.PreventUIRefresh( -1 )
+
+reaper.TrackList_AdjustWindows( false )
+reaper.UpdateArrange()
+
+reaper.Undo_EndBlock("Create bus after selected tracks and reroute them", 0)
+
