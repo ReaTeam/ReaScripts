2fc194b734ad5de60c7916c2ebe926568457eb1c
amagalma <amagalma@gmail.com>
2018-12-24 02:41:31 +0200

    Update amagalma_Toggle enclose selected or focused FX in vsible chain with AB_LM Level Matching VST.lua

--- Various/amagalma_Toggle enclose selected or focused FX in vsible chain with AB_LM Level Matching VST.lua
+++ Various/amagalma_Toggle enclose selected or focused FX in vsible chain with AB_LM Level Matching VST.lua
@@ -1,19 +1,20 @@
 -- @description amagalma_Toggle enclose selected or focused FX in vsible chain with AB_LM Level Matching VST
 -- @author amagalma
--- @version 1.0
+-- @version 1.05
 -- @about
 --   # Inserts or Removes TBProAudio's AB_LM Level Matching VST enclosing the selected FX or the focused FX (if not any selected)
 --   - Smart undo point creation
 --   - Automatically checks if AB_LM VST2 or VST3 is present in your system
 -- @link http://www.tb-software.com/TBProAudio/ab_lm.html
 
+--[[
+ * Changelog:
+ * v1.05(2018-12-24)
+  + float AB_LM Receive
+--]]
+
 ------------------------------------------------------------------------------------------------
 local reaper = reaper
-local find = string.find
-local insert = table.insert
-local match = string.match
-local concat = table.concat
-local remove = table.remove
 
 ------------------------------------------------------------------------------------------------
 
@@ -52,7 +53,7 @@ end
 ------------------------------------------------------------------------------------------------
 local function GetInfo()
   local FX_win = reaper.JS_Window_Find("FX: ", false )
-  local sel_FX = {}
+  local sel_FX, firstselFX, lastselFX = {}
   if FX_win then
     local title = reaper.JS_Window_GetTitle( FX_win, "" )
     if title:match("FX: Track ") or title:match("FX: Master Track") or title:match("FX: Item ") then
@@ -65,6 +66,7 @@ local function GetInfo()
         a = a + 1
       end
       local what, trackGUID, take
+      reaper.JS_Window_SetForeground( FX_win ) -- GetFocusedFX works better
       local focus, track, item, fxid = reaper.GetFocusedFX()
       if focus == 1 then
         what = "track"
@@ -81,7 +83,11 @@ local function GetInfo()
         trackGUID = reaper.guidToString(reaper.GetTrackGUID(track), "")
         take = reaper.GetMediaItemTake(item, fxid >> 16)
       end
-      return sel_FX, fxid, track, what, trackGUID, take
+      if #sel_FX > 1 then
+        firstselFX = sel_FX[1]
+        lastselFX = sel_FX[#sel_FX]
+      end
+      return fxid, track, what, trackGUID, take, firstselFX, lastselFX
     else
       return nil
     end
@@ -108,11 +114,17 @@ local function AddTakeAB(take, pos)
 end
 
 ------------------------------------------------------------------------------------------------
-local function InsertAB(sel_FX, fxid, track, what, take)
+local function InsertAB(fxid, track, what, trackGUID, take, firstselFX, lastselFX)
+  local focusedFX = fxid
+  if lastselFX and focusedFX >= firstselFX and focusedFX <= lastselFX then
+    focusedFX = fxid + 1
+  elseif lastselFX and focusedFX > lastselFX then
+    focusedFX = fxid + 2
+  end
   if what == "track" then
-    if #sel_FX > 1 then -- enclose selected FXs
-      AddTrackAB(track, sel_FX[1])
-      AddTrackAB(track, #sel_FX+2)
+    if lastselFX then -- enclose selected FXs
+      AddTrackAB(track, firstselFX)
+      AddTrackAB(track, lastselFX+2)
     else -- enclose focused FX
       AddTrackAB(track, fxid)
       AddTrackAB(track, fxid+2)
@@ -134,12 +146,12 @@ local function InsertAB(sel_FX, fxid, track, what, take)
     end
     chunk = table.concat(t, "\n")
     reaper.SetTrackStateChunk( track, chunk, false )
-    reaper.TrackFX_Show( track, #sel_FX > 1 and #sel_FX+2 or fxid+2, 3 ) -- float AB_LM Receive
-    reaper.TrackFX_SetOpen( track, fxid+1, true ) -- keep previously focused FX focused
+    reaper.TrackFX_Show( track, lastselFX ~= nil and lastselFX+2 or fxid+2, 3 ) -- float AB_LM Receive
+    reaper.TrackFX_SetOpen( track, lastselFX ~= nil and focusedFX or fxid+1, true ) -- keep previously focused FX focused
   elseif what == "item" then
-    if #sel_FX > 1 then -- enclose selected FXs
-      AddTakeAB(take, sel_FX[1])
-      AddTakeAB(take, #sel_FX+2)
+    if lastselFX then -- enclose selected FXs
+      AddTakeAB(take, firstselFX)
+      AddTakeAB(take, lastselFX+2)
     else -- enclose focused FX
       AddTakeAB(take, fxid)
       AddTakeAB(take, fxid+2)
@@ -162,8 +174,8 @@ local function InsertAB(sel_FX, fxid, track, what, take)
     end
     chunk = table.concat(t, "\n")
     reaper.SetItemStateChunk( item, chunk, false )
-    reaper.TakeFX_Show( take, #sel_FX > 1 and #sel_FX+2 or fxid+2, 3 ) -- float AB_LM Receive
-    reaper.TakeFX_SetOpen( take, fxid+1, true ) -- keep previously focused FX focused
+    reaper.TakeFX_Show( take, lastselFX ~= nil and lastselFX+2 or fxid+2, 3 ) -- float AB_LM Receive
+    reaper.TakeFX_SetOpen( take, lastselFX ~= nil and focusedFX or fxid+1, true ) -- keep previously focused FX focused
   end
 end
 
@@ -183,7 +195,7 @@ local function RemoveAB(track, what, take)
 end
 
 -- Main function -------------------------------------------------------------------------------
-local sel_FX, fxid, track, what, trackGUID, take = GetInfo()
+local fxid, track, what, trackGUID, take, firstselFX, lastselFX = GetInfo()
 if track and trackGUID then
   local ok, value = reaper.GetProjExtState(0, "AB_LM VST Toggle", trackGUID)
   if ok and value == "1" then
@@ -193,7 +205,7 @@ if track and trackGUID then
     reaper.Undo_EndBlock("Remove AB_LM VST from focused FX Chain", -1)
   else
     reaper.Undo_BeginBlock()
-    InsertAB(sel_FX, fxid, track, what, take)
+    InsertAB(fxid, track, what, trackGUID, take, firstselFX, lastselFX)
     reaper.SetProjExtState(0, "AB_LM VST Toggle", trackGUID, "1")
     reaper.Undo_EndBlock("Enclose selected/focused FX in Chain with AB_LM VST", -1)
   end
