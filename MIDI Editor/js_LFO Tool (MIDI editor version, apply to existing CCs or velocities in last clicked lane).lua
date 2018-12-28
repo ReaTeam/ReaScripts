7c15bd5320386a5299f5496ab026d5a0bd278551
juliansader <julian.sader@gmail.com>
2018-12-28 12:11:28 +0200

    Update js_LFO Tool (MIDI editor version, apply to existing CCs or velocities in last clicked lane).lua

--- MIDI Editor/js_LFO Tool (MIDI editor version, apply to existing CCs or velocities in last clicked lane).lua
+++ MIDI Editor/js_LFO Tool (MIDI editor version, apply to existing CCs or velocities in last clicked lane).lua
@@ -1,6 +1,6 @@
 --[[
 ReaScript name: js_LFO Tool (MIDI editor version, apply to existing CCs or velocities in last clicked lane).lua
-Version: 2.70
+Version: 2.71
 Author: juliansader
 Website: http://forum.cockos.com/showthread.php?t=177437
 Screenshot: http://stash.reaper.fm/29477/LFO%20Tool%20%28MIDI%20editor%20version%2C%20apply%20to%20existing%20CCs%20or%20velocities%29.gif
@@ -96,6 +96,8 @@ About:
     + "Morph" slider when applied to existing events.
   v2.70 (2018-12-26)
     + "Swing" envelope.
+  v2.71 (2018-12-28)
+    + Small improvements.
 ]]
 -- The archive of the full changelog is at the end of the script.
 
@@ -1144,7 +1146,7 @@ function MAIN_generateNodes()
         
         ptcount=ptcount+1   
         
-    until instime >= time_end -- while time < gen_end_time
+    until instime > time_end -- while time < gen_end_time
 end -- function MAIN_generateNodes()
 
 ----------------------------------------------------------
