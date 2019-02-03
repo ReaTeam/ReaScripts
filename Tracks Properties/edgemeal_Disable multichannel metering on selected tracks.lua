994d9a13c5cc8f60902a6ee15436da4b42c3b291
Edgemeal <47255672+Edgemeal@users.noreply.github.com>
2019-02-03 15:15:56 -0600

    Release Disable multichannel metering on selected tracks v1.0

---
+++ Tracks Properties/edgemeal_Disable multichannel metering on selected tracks.lua
@@ -0,0 +1,5 @@
+-- @description Disable multichannel metering on selected tracks
+-- @author Edgemeal
+-- @version 1.0
+-- @provides [main] Disable multichannel metering on selected tracks https://github.com/Edgemeal/ReaScripts/blob/master/Disable%20multichannel%20metering%20on%20selected%20tracks.lua
+-- @link https://forum.cockos.com/showthread.php?t=208982
