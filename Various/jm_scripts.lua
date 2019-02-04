9140fd22328dc33cea04cb7c97e167507fc8c1d5
jmieszkowski <44314566+jmieszkowski@users.noreply.github.com>
2019-02-04 17:48:09 +0100

    Release jm_scripts v1.0

---
+++ Various/jm_scripts.lua
@@ -0,0 +1,14 @@
+@description jm_scripts
+@author jmieszkowski
+@version 1.0
+@provides
+  . > jm_scripts
+  [main] set track color to parent.lua https://github.com/jmieszkowski/ReaScripts/blob/master/LUA/Color/SET_TRACK_COLOR_TO_PARENT.lua
+@link https://github.com/jmieszkowski/ReaScripts/
+@about
+  # jm_scripts
+
+  Contains:
+             1. Set track`s color to parents`
+
+  more soon :)
