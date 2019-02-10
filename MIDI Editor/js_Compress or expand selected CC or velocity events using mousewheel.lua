b9f0da6a1f36ce64c42fa78c981709e68950064b
juliansader <julian.sader@gmail.com>
2019-02-10 05:28:19 +0200

    Update js_Compress or expand selected CC or velocity events using mousewheel.lua

--- MIDI Editor/js_Compress or expand selected CC or velocity events using mousewheel.lua
+++ MIDI Editor/js_Compress or expand selected CC or velocity events using mousewheel.lua
@@ -15,14 +15,13 @@ About:
 
 reaper.MB([[This script has been replaced by "js_Mouse editing - Stretch and Compress".
 
-The new "Mouse editing" scripts require the js_ReaScriptAPI extension, and have several advantages:
+The new "Mouse editing" scripts require the js_ReaScriptAPI extension, and offer several improvements:
 
 * No helper scripts required for mousewheel control: 
 The new scripts can detect mouse input themselves.
 
 * Fewer scripts and fewer shortcuts: 
-Since the new scripts can detect mouse input, each script can be multi-functional, 
-and middle-click and right-click are used to switch between different modes. 
+Since the new scripts can detect mouse input, each script can be multi-functional, and middle-click and right-click are used to switch between different modes. 
 For example, the single new script, "js_Mouse editing - Stretch and Compress", combines and replaces both the "Stretch" and "Compress" scripts.
 
 * New editing features: 
