23ea5424933479fd868d95ef7f6bef3c519dcce2
Edgemeal <47255672+Edgemeal@users.noreply.github.com>
2019-03-14 13:59:10 -0500

    Release Toggle show monitoring FX chain v1.01 (#166)
    Show monitor if docked but not the selected tab.
    Additional checking to identify target window.

--- FX/edgemeal_Toggle show monitoring FX chain.lua
+++ FX/edgemeal_Toggle show monitoring FX chain.lua
@@ -1,16 +1,35 @@
 -- @description Toggle show monitoring FX chain
 -- @author Edgemeal
--- @version 1.0
+-- @version 1.01
+-- @changelog
+--   Show monitor if docked but not the selected tab.
+--   Additional checking to identify target window.
+
+function FindWindow(window_titles, child_id, child_must_visible)
+  local arr = reaper.new_array({}, 128)
+  for i = 1, #window_titles do
+    local title = reaper.JS_Localize(window_titles[i], 'common')
+    reaper.JS_Window_ArrayFind(title, true, arr)
+    local handles = arr.table()
+    for j = 1, #handles do
+      local hwnd = reaper.JS_Window_HandleFromAddress(handles[j]) -- window handle
+      local child_hwnd =  reaper.JS_Window_FindChildByID(hwnd, child_id) -- child handle
+      if child_hwnd then -- child control found
+        if child_must_visible and not reaper.JS_Window_IsVisible(child_hwnd) then -- child must be visible
+          return nil
+        else
+          return hwnd
+        end
+      end
+    end
+  end
+end
 
 function Main()
-  local title = reaper.JS_Localize('FX: Monitoring', 'common')
-  local hwnd = reaper.JS_Window_Find(title, true)
+  local t = {'FX: Monitoring','FX: Monitoring [BYPASSED]'} -- titlebar text(s) to find
+  local hwnd = FindWindow(t, 1076, true) -- 1076 = child id to find, true = child must be visible, i.e., docked but tab not selected.
   if not hwnd then
-    title = reaper.JS_Localize('FX: Monitoring [BYPASSED]', 'common')
-    hwnd = reaper.JS_Window_Find(title, true)
-  end
-  if not hwnd then -- show fx monitoring
-    reaper.Main_OnCommand(41882, 0) -- View: Show monitoring FX chain 
+    reaper.Main_OnCommand(41882, 0) -- View: Show monitoring FX chain
   else -- close fx monitor window
     reaper.JS_WindowMessage_Post(hwnd, "WM_CLOSE", 0,0,0,0)
   end
