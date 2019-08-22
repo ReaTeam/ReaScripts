63e45df37e1d417631f55e71ca5ca2cb71bd84ff
Christian Fillion <cfillion@users.noreply.github.com>
2019-08-22 12:35:50 -0400

    add the action's name

--- Various/cfillion_Enable or disable external timecode synchronization.lua
+++ Various/cfillion_Enable or disable external timecode synchronization.lua
@@ -7,12 +7,12 @@
 --   [main] . > cfillion_Disable external timecode synchronization.lua
 -- @about This package provides two actions for enabling or disabling REAPER's external timecode synchronization.
 
-local ACTION = 40620
+local action = 40620 -- Toggle external timecode synchronization
 local modes = {Enable=1, Disable=0}
 local name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
-local state = reaper.GetToggleCommandState(ACTION)
+local state = reaper.GetToggleCommandState(action)
 local mode = assert(modes[name:match('^%w+')], 'Invalid script filename')
 
 if state ~= mode then
-  reaper.Main_OnCommand(ACTION, 0)
+  reaper.Main_OnCommand(action, 0)
 end
