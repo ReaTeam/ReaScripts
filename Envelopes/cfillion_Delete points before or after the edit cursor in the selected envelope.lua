3d5ec556d4d15a5a5218f67b302dba815fef7c46
Christian Fillion <cfillion@users.noreply.github.com>
2019-10-02 12:26:47 -0400

    Release Delete points before or after the edit cursor in the selected envelope v1.0

---
+++ Envelopes/cfillion_Delete points before or after the edit cursor in the selected envelope.lua
@@ -0,0 +1,34 @@
+-- @description Delete points before or after the edit cursor in the selected envelope
+-- @author cfillion
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] . > cfillion_Delete points before the edit cursor in the selected envelope.lua
+--   [main] . > cfillion_Delete points after the edit cursor in the selected envelope.lua
+
+local UNDO_STATE_TRACKCFG = 1
+
+local scriptName = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
+local before = scriptName:match('before')
+
+reaper.Undo_BeginBlock();
+
+(function()
+  local env = reaper.GetSelectedEnvelope(0)
+  if not env then return end
+
+  local timeStart, timeEnd
+
+  if before then
+    timeStart, timeEnd = 0, reaper.GetCursorPosition()
+  else
+    local lastPoint = reaper.CountEnvelopePoints(env) - 1
+    timeStart = reaper.GetCursorPosition()
+    timeEnd = ({reaper.GetEnvelopePoint(env, lastPoint)})[2] + 1
+  end
+
+  reaper.DeleteEnvelopePointRange(env, timeStart, timeEnd)
+  reaper.UpdateArrange()
+end)()
+
+reaper.Undo_EndBlock(scriptName, UNDO_STATE_TRACKCFG)
