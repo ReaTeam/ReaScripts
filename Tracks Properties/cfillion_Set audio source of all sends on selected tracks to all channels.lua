a1a4ded067dda13a5e6175ea8ca84bdb1315a006
Christian Fillion <cfillion@users.noreply.github.com>
2019-10-20 19:00:30 -0400

    better constant naming for future self

--- Tracks Properties/cfillion_Set audio source of all sends on selected tracks to all channels.lua
+++ Tracks Properties/cfillion_Set audio source of all sends on selected tracks to all channels.lua
@@ -4,9 +4,10 @@
 -- @donation https://www.paypal.com/cgi-bin/webscr?business=T3DEWBQJAV7WL&cmd=_donations&currency_code=CAD
 
 local UNDO_STATE_TRACKCFG = 1
+local SENDS = 0
+local DESTINATION = 1
 
 local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
-local category = 0
 
 reaper.Undo_BeginBlock()
 
@@ -19,15 +20,15 @@ for ti = 0, reaper.CountSelectedTracks(0) - 1 do
     src_chan = nchan << 9
   end
 
-  for si = 0, reaper.GetTrackNumSends(track, category) - 1 do
-    local dest = reaper.BR_GetMediaTrackSendInfo_Track(track, category, si, 1)
+  for si = 0, reaper.GetTrackNumSends(track, SENDS) - 1 do
+    local dest = reaper.BR_GetMediaTrackSendInfo_Track(track, SENDS, si, DESTINATION)
     local dest_nchan = reaper.GetMediaTrackInfo_Value(dest, 'I_NCHAN')
 
     if dest_nchan < nchan then
       reaper.SetMediaTrackInfo_Value(dest, 'I_NCHAN', nchan)
     end
 
-    reaper.SetTrackSendInfo_Value(track, category, si, 'I_SRCCHAN', src_chan)
+    reaper.SetTrackSendInfo_Value(track, SENDS, si, 'I_SRCCHAN', src_chan)
   end
 end
 
