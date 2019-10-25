3ee37e37483c1fef0ba2e52ff872be72b9bc1d9d
Christian Fillion <cfillion@users.noreply.github.com>
2019-10-25 19:09:54 -0400

    fix changelog tag

--- Various/pandabot_ChordGun.lua
+++ Various/pandabot_ChordGun.lua
@@ -3,25 +3,22 @@ Description: ChordGun
 Author: pandabot
 License: MIT
 Version: 1.4
+Changelog: + fixed UI flaw with dropdown icon
 Screenshot: https://github.com/benjohnson2001/ChordGun/raw/release/ChordGun/src/images/chordGunInterface.png
 Donate: https://paypal.me/benjohnson2001
 Links:
   pandabot (Cockos forum) https://forum.cockos.com/member.php?u=127396
   Source repository (GitHub) https://github.com/benjohnson2001/ChordGun/tree/release
 Metapackage: true
-Provides:
-    [main=main,midi_editor] pandabot_ChordGun/*.lua
+Provides: [main=main,midi_editor] pandabot_ChordGun/*.lua
 About:
-
     ### Chord Gun
     
     #### What is it?
     Reaper tool that fires scale chords and notes into a composition, either by sequencing through the MIDI Editor or recording directly into Media Items
-    
 
     #### How do I use it?
     There is an instructional video [here](www.youtube.com/watch?v=1CqVYcN3VAw)
-		
 
     #### keyboard shortcuts (when GUI has focus)
     0 - stop all notes from playing
@@ -106,23 +103,3 @@ About:
 
     command+shift+. - increment chord inversion
 --]]
-
---[[
-Changelog:
-
-v1.4 (2019-10-25)
-  + fixed UI flaw with dropdown icon
-
-v1.3 (2019-10-14)
-  + added shift-click insertion for keyboard shortcuts
-
-v1.2 (2019-10-13)
-  + added click for preview, shift-click to insert (https://forum.cockos.com/showthread.php?t=225717)
-
-v1.1 (2019-10-13)
-  + added guard clause for scale numbers greater than 5 on pentatonic scales
-  + added fix for tempo change markers
-
-v1.0 (2018-11-07)
-  + initial version
-]]--
