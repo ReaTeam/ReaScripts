4fac248bd4299bfcf69799ce61cf016867ef8bf0
Adam Lovatt <jalovatt@hotmail.com>
2020-01-19 12:39:50 -0700

    Fixes package syntax

--- Development/Lokasenna_Scythe library v3 (developer tools).lua
+++ Development/Lokasenna_Scythe library v3 (developer tools).lua
@@ -14,24 +14,24 @@ Changelog:
   None
 Metapackage: true
 Provides:
-  [main] Lokasenna_Scythe library v3/development/examples/Scythe_Example - Default parameters.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Default parameters.lua
-  [main] Lokasenna_Scythe library v3/development/examples/Scythe_Example - Menubar, Listbox, and TextEditor.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Menubar, Listbox, and TextEditor.lua
-  [main] Lokasenna_Scythe library v3/development/examples/Scythe_Example - General demonstration.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - General demonstration.lua
-  [main] Lokasenna_Scythe library v3/development/examples/Scythe_Example - Working with Images.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Working with Images.lua
-  [main] Lokasenna_Scythe library v3/development/examples/Scythe_Example - Typical script options.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Typical script options.lua
-  [nomain] Lokasenna_Scythe library v3/development/gui-testing/user-event-hooks.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/user-event-hooks.lua
-  [nomain] Lokasenna_Scythe library v3/development/gui-testing/textbox-validation.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/textbox-validation.lua
-  [nomain] Lokasenna_Scythe library v3/development/gui-testing/preventDefault1.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/preventDefault1.lua
-  [nomain] Lokasenna_Scythe library v3/development/gui-testing/preventDefault2.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/preventDefault2.lua
-  [main] Lokasenna_Scythe library v3/development/testing/Scythe_Test Runner.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/Scythe_Test Runner.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/getContext.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/getContext.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/example.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/example.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/color.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/color.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/string.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/string.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/message.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/message.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/menu.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/menu.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/math.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/math.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/table.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/table.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/tests/public/file.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/file.lua
-  [nomain] Lokasenna_Scythe library v3/development/testing/lib/core.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/lib/core.lua
+  [main] /Lokasenna_Scythe library v3/development/examples/Scythe_Example - Default parameters.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Default parameters.lua
+  [main] /Lokasenna_Scythe library v3/development/examples/Scythe_Example - Menubar, Listbox, and TextEditor.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Menubar, Listbox, and TextEditor.lua
+  [main] /Lokasenna_Scythe library v3/development/examples/Scythe_Example - General demonstration.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - General demonstration.lua
+  [main] /Lokasenna_Scythe library v3/development/examples/Scythe_Example - Working with Images.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Working with Images.lua
+  [main] /Lokasenna_Scythe library v3/development/examples/Scythe_Example - Typical script options.lua raw.githubusercontent.com/jalovatt/scythe/master/development/examples/Scythe_Example - Typical script options.lua
+  [nomain] /Lokasenna_Scythe library v3/development/gui-testing/user-event-hooks.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/user-event-hooks.lua
+  [nomain] /Lokasenna_Scythe library v3/development/gui-testing/textbox-validation.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/textbox-validation.lua
+  [nomain] /Lokasenna_Scythe library v3/development/gui-testing/preventDefault1.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/preventDefault1.lua
+  [nomain] /Lokasenna_Scythe library v3/development/gui-testing/preventDefault2.lua raw.githubusercontent.com/jalovatt/scythe/master/development/gui-testing/preventDefault2.lua
+  [main] /Lokasenna_Scythe library v3/development/testing/Scythe_Test Runner.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/Scythe_Test Runner.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/getContext.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/getContext.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/example.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/example.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/color.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/color.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/string.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/string.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/message.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/message.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/menu.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/menu.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/math.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/math.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/table.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/table.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/tests/public/file.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/tests/public/file.lua
+  [nomain] /Lokasenna_Scythe library v3/development/testing/lib/core.lua raw.githubusercontent.com/jalovatt/scythe/master/development/testing/lib/core.lua
 ]]--
