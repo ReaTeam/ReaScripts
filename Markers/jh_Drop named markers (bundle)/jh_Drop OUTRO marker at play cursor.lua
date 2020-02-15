99744f395a1aa7dd0277f695e2f3e3b4e966f2c8
John Harkins <john.harkinz@gmail.com>
2020-02-15 23:26:30 +0000

    Release jh_Drop named marker v1.0

---
+++ Markers/jh_Drop named markers (bundle)/jh_Drop OUTRO marker at play cursor.lua
@@ -0,0 +1,12 @@
+-- @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'jh_Drop named marker function'
+
+--
+-- CUSTOM MARKER NAME HERE
+--
+local name = "Outro"
+--
+
+Insert_Marker_Custom_Name(name)
