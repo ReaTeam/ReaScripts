df891380612ff180a8f70d8054cf4a4689a88251
amagalma <amagalma@gmail.com>
2020-05-05 15:27:15 +0300

    Update amagalma_Move edit cursor to previous visible grid line.lua

--- Cursor/amagalma_Move edit cursor to previous visible grid line.lua
+++ Cursor/amagalma_Move edit cursor to previous visible grid line.lua
@@ -1,27 +1,36 @@
 -- @description amagalma_Move edit cursor to previous visible grid line
 -- @author amagalma
--- @version 1.03
+-- @version 1.05
 -- @changelog
---   - Support grid lines of extra small duration
+--   - Optimized code
 -- @about
 --   # Moves the edit cursor to the previous grid line that is visible
 
 
-function NoUndoPoint() end 
 reaper.Main_OnCommand(40755, 0) -- Snapping: Save snap state
 reaper.Main_OnCommand(40754, 0) -- Snapping: Enable snap
 local cursorpos = reaper.GetCursorPosition()
+local _, division = reaper.GetSetProjectGrid( 0, 0, 0, 0, 0 )
+local tmsgn_cnt = reaper.CountTempoTimeSigMarkers( 0 )
+local _, tempo
+if tmsgn_cnt == 0 then
+  tempo = reaper.Master_GetTempo()
+else
+  local active_tmsgn = reaper.FindTempoTimeSigMarker( 0, cursorpos )
+  _, _, _, _, tempo = reaper.GetTempoTimeSigMarker( 0, active_tmsgn )
+end
+local grid_duration = 60/tempo * division
 if cursorpos > 0 then
   local grid = cursorpos
   while (grid >= cursorpos) do
-      cursorpos = cursorpos - 0.02
-      if cursorpos >= 0.02 then
+      cursorpos = cursorpos - grid_duration
+      if cursorpos >= grid_duration then
         grid = reaper.SnapToGrid(0, cursorpos)
       else
         grid = 0
       end
   end
   reaper.SetEditCurPos(grid,1,1)
-end  
+end
 reaper.Main_OnCommand(40756, 0) -- Snapping: Restore snap state
-reaper.defer(NoUndoPoint)
+reaper.defer(function() end)
