9011c4f9d2dc3fe86e02815d5c86599921858349
J-Reverb <50082934+J-Reverb@users.noreply.github.com>
2020-05-08 18:53:31 +0100

    Release ENV_Snap selected tcpfx envelope points to semitones v1.0

---
+++ Envelopes/jreverb_ENV_Snap selected tcpfx envelope points to semitones.lua
@@ -0,0 +1,14 @@
+-- @description ENV_Snap selected tcpfx envelope points to semitones
+-- @author J Reverb
+-- @version 1.0
+-- @provides
+--   [main] jreverb_ENV_Snap selected tcpfx envelope points to semitones/jreverb_ENV_Snap selected tcpfx envelope points to semitones positive 24 steps
+--   [main] jreverb_ENV_Snap selected tcpfx envelope points to semitones/jreverb_ENV_Snap selected tcpfx envelope points to semitones negative 24 steps
+--   [main] jreverb_ENV_Snap selected tcpfx envelope points to semitones/jreverb_ENV_Snap selected tcpfx AI envelope points to semitones positive 24 steps
+--   [main] jreverb_ENV_Snap selected tcpfx envelope points to semitones/jreverb_ENV_Snap selected tcpfx AI envelope points to semitones negative 24 steps
+-- @about
+--   A convenient way to snap envelope point values to semitones for Pitch Wheel automation.
+--   Plus/minus 12 semitones.
+--   Currently 2 versions for Main tcpfx envelope lane and tcpfx Automation items (AI).
+
+x
