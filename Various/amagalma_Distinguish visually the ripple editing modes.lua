334f1215c172583f1ffd6e0e33722b42d60be4c2
amagalma <amagalma@gmail.com>
2020-05-12 01:39:57 +0300

    Update amagalma_Distinguish visually the ripple editing modes.lua (#406)

--- Various/amagalma_Distinguish visually the ripple editing modes.lua
+++ Various/amagalma_Distinguish visually the ripple editing modes.lua
@@ -1,8 +1,10 @@
 -- @description Distinguish visually the ripple editing modes
 -- @author amagalma
--- @version 1.00
+-- @version 1.10
+-- @changelog
+--   - Changed behavior: In both ripple modes affected items are colored. No change of timeline color
 -- @about
---   # Colors the timeline in ripple all-tracks or the affected items in ripple per-track
+--   # Colors the the affected items in ripple editing modes
 --   - Color can be set inside the script
 --   - Script reports on/off state so that it can be set to a toolbar button
 --   - Requires JS_ReaScriptAPI extension >v1.002
@@ -77,29 +79,13 @@ local _, width_trackview, height_trackview = reaper.JS_Window_GetClientSize( tra
 width_trackview = width_trackview + 17
 local _, left_trackview, top_trackview, right_trackview, bottom_trackview = reaper.JS_Window_GetRect( trackview )
 
-local timeline = reaper.JS_Window_FindChildByID( MainHwnd, 0x3ED )
-local _, left_timeline, top_timeline, right_timeline, bottom_timeline = reaper.JS_Window_GetRect( timeline )
-local width_timeline = right_timeline - left_timeline
-local height_timeline = bottom_timeline - top_timeline
-local timeline_mask = reaper.JS_Window_Create( "timeline_mask", "timeline_mask", left_timeline, top_timeline, width_timeline, height_timeline, "POPUP", MainHwnd )
-local bmp_timeline = reaper.JS_LICE_CreateBitmap( true, width_timeline, 300 )
-reaper.JS_LICE_Clear( bmp_timeline, color )
-reaper.JS_Window_SetOpacity( timeline_mask, "ALPHA", ripple == 2 and alpha or 0 )
-reaper.JS_Window_SetZOrder( timeline_mask, "TOP" )
-
---Edgemeal code to click through window & don't show in taskbar
-local style = reaper.JS_Window_GetLong( timeline_mask, "EXSTYLE" )
-if style > 0 then 
-  reaper.JS_Window_Show( timeline_mask, "HIDE" )
-  reaper.JS_Window_SetLong( timeline_mask, "EXSTYLE", style | 0x20| 0x80 )
-  reaper.JS_Window_Show( timeline_mask, "SHOWNA" )
-end
-
 ---------------------------------------------------------------
 
 local start = reaper.time_precise()
 local checksizetime = start
 local checkrippletime = start
+local p_item, p_item_cnt = 0, -1
+local first_pos
 
 function main()
   local give_space = false
@@ -116,14 +102,11 @@ function main()
       "Ripple per track\n\n" or "Ripple all tracks\n\n" )
       Msg(msg)
       ripple = r_state
-      reaper.JS_Window_SetOpacity( timeline_mask, "ALPHA", r_state == 2 and alpha or 0 )
-      if r_state ~= 1 then
-        for k in pairs(bmps) do
-          reaper.JS_LICE_DestroyBitmap( bmps[k])
-        end
-        for k in pairs(tracks) do
-          tracks[k] = nil
-        end
+      for k in pairs(bmps) do
+        reaper.JS_LICE_DestroyBitmap( bmps[k])
+      end
+      for k in pairs(tracks) do
+        tracks[k] = nil
       end
     end
   end
@@ -184,24 +167,76 @@ function main()
         
         end
       else    
-        break   
+        break
       end
     end  
     
   elseif r_state == 2 then
- 
-    if now - checksizetime >= 0.75 then
-      checksizetime = now
-      local _, l, t, r, b = reaper.JS_Window_GetRect( timeline )
-      if b ~= bottom_timeline or l ~= left_timeline or t ~= top_timeline or r ~= right_timeline then
-        left_timeline, right_timeline, top_timeline, bottom_timeline = l, r, t, b
-        width_timeline, height_timeline = r-l, b-t
-        reaper.JS_Window_SetPosition( timeline_mask, left_timeline, top_timeline, width_timeline, height_timeline )
+     
+    local st, en = reaper.GetSet_ArrangeView2( 0, false, 0, 0 )
+    local arr_duration = en - st
+    local item_cnt = reaper.CountSelectedMediaItems( 0 )
+    local first_item = reaper.GetSelectedMediaItem( 0, 0 )
+    if item_cnt ~= p_item_cnt or first_item ~= p_item then
+      first_pos = math.huge
+    end
+    for i = 0, item_cnt-1 do
+      local item = reaper.GetSelectedMediaItem( 0, i )
+      local item_pos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
+      if item_pos < first_pos then first_pos = item_pos end
+    end
+
+    local track_cnt = reaper.CountTracks( 0 )
+    for i = 0, track_cnt-1 do
+      local track = reaper.GetTrack( 0, i )
+      local y_pos = reaper.GetMediaTrackInfo_Value( track, "I_TCPY" )
+                                                                     
+      if reaper.IsTrackVisible( track, false ) and y_pos < height_trackview then
+        local item_cnt = reaper.CountTrackMediaItems( track )
+        if item_cnt > 0 then
+          local id = reaper.GetMediaTrackInfo_Value( track, "IP_TRACKNUMBER" )
+          local paint = false
+          local x_pos
+          for j = 0, item_cnt-1 do
+            local item = reaper.GetTrackMediaItem( track, j )
+            local item_pos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
+            if item_pos >= first_pos then
+              x_pos = floor( ( ( reaper.GetMediaItemInfo_Value( item, "D_POSITION" ) - st ) / arr_duration ) * width_trackview )
+              x_pos = x_pos > 0 and ( x_pos < width_trackview and x_pos or width_trackview ) or 0
+              paint = true
+              break
+            end
+          end
+          local track_h = reaper.GetMediaTrackInfo_Value( track, "I_WNDH" )
+          
+          if paint then
+            if ( not tracks[id] ) then
+              bmps[id] = reaper.JS_LICE_CreateBitmap( true, 1, 1 )
+              reaper.JS_LICE_Clear( bmps[id], color_trackview) -- 0x012F2F2F )
+              reaper.JS_Composite( trackview, x_pos, y_pos, width_trackview-x_pos, track_h, bmps[id], 0, 0, 1, 1, true )
+              tracks[id] = x_pos
+            elseif x_pos ~= tracks[id] then
+              reaper.JS_Window_InvalidateRect( trackview, tracks[id], y_pos, width_trackview, y_pos + track_h, false )
+              reaper.JS_Composite( trackview, x_pos, y_pos, width_trackview-x_pos, track_h, bmps[id], 0, 0, 1, 1, true )
+              tracks[id] = x_pos
+            end
+          else
+            if tracks[id] then
+              if bmps[id] then
+                reaper.JS_LICE_DestroyBitmap( bmps[id] ) 
+                bmps[id] = nil
+              end
+              reaper.JS_Window_InvalidateRect( trackview, tracks[id], y_pos, width_trackview, y_pos + track_h, false )
+              tracks[id] = nil
+            end
+          end
+        
+        end
+      else    
+        break   
       end
     end
-    
-    reaper.JS_Composite( timeline_mask, 0, 0, width_timeline, height_timeline, bmp_timeline, 0, 0, width_timeline, height_timeline, true )
-  
+
   end
 
   reaper.defer(main)
@@ -212,18 +247,16 @@ function main()
   
 end
 
-function destroyAll()
-  if bmp_timeline then reaper.JS_LICE_DestroyBitmap( bmp_timeline ) end
-  if timeline_mask then reaper.JS_Window_Destroy( timeline_mask ) end
+function Exit()
   for k in pairs(bmps) do
     reaper.JS_LICE_DestroyBitmap( bmps[k])
   end
-  reaper.UpdateTimeline()
+  reaper.UpdateArrange()
   reaper.SetToggleCommandState( section, cmdID, 0 )
   reaper.RefreshToolbar2( section, cmdID )
   Msg("\n== script end ==\n\n")
   return reaper.defer(function() end)
 end
 
-reaper.atexit(destroyAll)
+reaper.atexit(Exit)
 main()
