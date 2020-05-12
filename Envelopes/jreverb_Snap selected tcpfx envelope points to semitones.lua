6cfe20cc88bf92523191ccf98db088e30729cf65
cfillion <cfillion@users.noreply.github.com>
2020-05-12 15:34:25 -0400

    remove ENV prefix, don't install the empty package file, add .lua extension

---
+++ Envelopes/jreverb_Snap selected tcpfx envelope points to semitones.lua
@@ -0,0 +1,14 @@
+-- @description Snap selected tcpfx envelope points to semitones
+-- @author J Reverb
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] jreverb_Snap selected tcpfx envelope points to semitones/jreverb_Snap selected tcpfx envelope points to semitones positive 24 steps.lua
+--   [main] jreverb_Snap selected tcpfx envelope points to semitones/jreverb_Snap selected tcpfx envelope points to semitones negative 24 steps.lua
+--   [main] jreverb_Snap selected tcpfx envelope points to semitones/jreverb_Snap selected tcpfx AI envelope points to semitones positive 24 steps.lua
+--   [main] jreverb_Snap selected tcpfx envelope points to semitones/jreverb_Snap selected tcpfx AI envelope points to semitones negative 24 steps.lua
+-- @about
+--   A convenient way to snap envelope point values to semitones for Pitch Wheel automation.
+--   Plus/minus 12 semitones.
+--
+--   Currently 2 versions for Main tcpfx envelope lane and tcpfx Automation items (AI).
