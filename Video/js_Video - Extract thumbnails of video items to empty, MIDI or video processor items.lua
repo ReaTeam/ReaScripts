9b7cac955383a54fa6d71ceae6b0be6d9c4d7a60
juliansader <julian.sader@gmail.com>
2020-05-30 01:05:28 +0200

    Add files via upload

--- Video/js_Video - Extract thumbnails of video items to empty, MIDI or video processor items.lua
+++ Video/js_Video - Extract thumbnails of video items to empty, MIDI or video processor items.lua
@@ -1,6 +1,6 @@
 --[[
 ReaScript name: js_Video - Extract thumbnails of video items to empty, MIDI or video processor items
-Version: 0.91
+Version: 0.92
 Author: juliansader
 Website: https://forum.cockos.com/showthread.php?t=237293
 Donation: https://www.paypal.me/juliansader
@@ -11,13 +11,14 @@ About:
   
   If multiple selected video items overlap a thumbnail item, a video item on the same track takes precedence, followed by video items in the closest parent folder, and lastly the video item in the highest (i.e. lowest-numbered) track.
   
-  The empty/MIDI/processor items can be added manually at hitpoints, or at any desired regular spacing, for example by creating a long empty item across the entire length of the video item and then splitting at the grid.
+  Thumbnail items can be added manually at hitpoints, or at any desired regular spacing, for example by creating a long empty item across the entire length of the video item and then splitting at the grid.
   
-  Alternatively, empty items can be added by the script "mrlimbic_ffprobe scene detect.lua", which creates items at scene (or camera/line) cuts.
+  Alternatively, empty items can be added by the script "mrlimbic_ffprobe scene detect.lua", which creates empty items at scene (or camera/line) cuts.
   
   On Linux, ffmpeg needs to be installed, and on WindowsOS and macOS, an ffmpeg executable is required.
   
-  The user can set the default JPEG quality in the script file's USER AREA.  If no default value is set, the script will ask the user to enter a custom value each time that the script runs.
+  High-resolution thumbnails require huge amounts of RAM, so the script can scale thumbnails down to any custom size.  The default size can be specified in the script file's User Area.  
+  If no default value is set, the script will ask the user to enter a custom value each time that the script runs.
 ]] 
 
 --[[
@@ -26,10 +27,13 @@ About:
     + Initial beta release.
   * v0.91 (2020-05-29)
     + Immediately terminate if image extraction fails once, to avoid hanging with incompatible video sources.
+  * v0.92 (2020-05-30)
+    + Thumbnails use lower resolution, for vastly lower RAM usage.
 ]]
 
--- USER ARERA
-JPEG_Quality = 20 -- 1=best, 31=worst
+-- USER AREA: 
+
+--thumbnailHeight = 100 -- Default thumbnail height in pixels. Larger size implies much higher RAM usage.
 
 
 reaper.Undo_BeginBlock2(0)
@@ -38,6 +42,7 @@ reaper.Undo_BeginBlock2(0)
 -- FIND SELECTED EMPTY ITEMS AND VIDEO ITEMS
 tEmpty = {}
 tVideo = {}
+tTracks = {}
 for i = 0, reaper.CountSelectedMediaItems(0)-1 do
     local item = reaper.GetSelectedMediaItem(0, i)
     local itemStart = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
@@ -51,7 +56,8 @@ for i = 0, reaper.CountSelectedMediaItems(0)-1 do
     if not take or sourceType == "MIDI" or sourceType == "VIDEOEFFECT" then 
         chunkOK, chunk = reaper.GetItemStateChunk(item, "", false) -- To insert image, must edit chunk
         if chunkOK then
-            tEmpty[#tEmpty+1] = { item = item, chunk = chunk, itemStart = itemStart, trak = track, trackNum = trackNum, insideFolder = (reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH") < 0) }
+            tEmpty[#tEmpty+1] = { item = item, chunk = chunk, itemStart = itemStart, track = track, trackNum = trackNum, insideFolder = (reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH") < 0) }
+            tTracks[track] = true
         end
         
     -- Video items
@@ -98,7 +104,8 @@ else
             ffmpegFolder = (reaper.GetResourcePath():gsub("\\", "/").."/"):gsub("//", "/")
             ffmpegPath   = ffmpegFolder .. (OS:match("Win") and "UserPlugins/ffmpeg.exe" or "UserPlugins/ffmpeg")
             while not reaper.file_exists(ffmpegPath) do
-                fOK, ffmpegFolder = reaper.JS_Dialog_BrowseForFolder("Please select the folder that contains the ffmpeg executable", ffmpegFolder)
+                if OS:match("OSX") and not shownFfmpegDialog then reaper.MB("In the following dialog window, please select the folder that contains the ffmpeg executable.", "Extract thumbnails", 0) shownFfmpegDialog = true end
+                fOK, ffmpegFolder = reaper.JS_Dialog_BrowseForFolder("Please select the folder that contains the ffmpeg executable:", ffmpegFolder)
                 if not (fOK == 1) then return false end
                 ffmpegPath = (ffmpegFolder.."/"):gsub("\\", "/"):gsub("//", "/") .. (OS:match("Win") and "ffmpeg.exe" or "ffmpeg")
                 --reaper.ShowConsoleMsg(ffmpegPath)
@@ -113,19 +120,27 @@ end
 -- SETUP PATH TO SAVE THUMBNAILS
 saveFolder = ( { reaper.GetProjExtState(0, "js_Thumbnails", "Save folder") } )[2]
 if not saveFolder or saveFolder == "" then saveFolder = reaper.GetProjectPathEx(0, "") end
-fOK, saveFolder = reaper.JS_Dialog_BrowseForFolder("Please select the folder in which to save the thumbnails", saveFolder)
+if OS:match("OSX") and not shownSaveDialog then reaper.MB("In the following dialog window, please select the folder in which to save the thumbnails.", "Extract thumbnails", 0) shownSaveDialog = true end
+fOK, saveFolder = reaper.JS_Dialog_BrowseForFolder("Please select the folder in which to save the thumbnails:", saveFolder)
     if not (fOK == 1) then return false end
 saveFolder = (saveFolder .."/"):gsub("\\", "/"):gsub("//", "/")
 reaper.SetProjExtState(0, "js_Thumbnails", "Save folder", saveFolder)
 
 
--- GET JPEG QUALITY
-while not (type(JPEG_Quality) == "number") do
-    qOK, JPEG_Quality = reaper.GetUserInputs("JPEG quality", 1, "JPEG quality (1=best,31=worst)", "15")
+-- GET THUMBNAIL SIZE
+while not (type(thumbnailHeight) == "number") do
+    qOK, thumbnailHeight = reaper.GetUserInputs("Thumbnail size", 1, "Height in pixels", "100")
     if not qOK then return false end
-    JPEG_Quality = JPEG_Quality and tonumber(JPEG_Quality)
+    thumbnailHeight = thumbnailHeight and tonumber(thumbnailHeight)
 end
-JPEG_Quality = string.format("%i", math.min(31, math.max(1, JPEG_Quality//1)))
+thumbnailHeight = math.min(1080, math.max(10, thumbnailHeight//1))
+thumbnailHeightStr = string.format("%i", thumbnailHeight)
+
+
+-- SET TRACK HEIGHTS
+for track in pairs(tTracks) do
+    reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", tonumber(thumbnailHeight))
+end
 
 
 -- LOOP THROUGH ALL EMPTY ITEMS, AND EXTRACT THUMBNAILS
@@ -179,10 +194,10 @@ for cnt, e in ipairs(tEmpty) do
             offset = offset%1 -- - seconds
         millis = (0.5 + offset*1000)//1
         offsetStringForCommand = string.format("%i:%i:%i.%i", hours, minutes, seconds, millis) 
-        imagePath = saveFolder .. vid.file .. " - " .. JPEG_Quality .. "q " .. offsetStringForFile .. "ms.jpg"
+        imagePath = saveFolder .. vid.file .. " - " .. thumbnailHeightStr .. "p " .. offsetStringForFile .. "ms.jpg"
         -- If thumbnail has already been extracted, don't waste time doing again
         if not reaper.file_exists(imagePath) then
-            command = ffmpegPath .. [[ -ss ]] .. offsetStringForCommand .. [[ -i "]] .. vid.path .. [[" -q:v ]] .. JPEG_Quality .. [[ -frames:v 1 "]] .. imagePath .. [["]] -- q:v range from 2 (best) to 31 (worst)
+            command = ffmpegPath .. [[ -ss ]] .. offsetStringForCommand .. [[ -i "]] .. vid.path .. [[" -q:v 1 -frames:v 1 -vf scale=-1:]] .. thumbnailHeightStr .. [[ -sws_flags lanczos "]] ..imagePath .. [["]] -- q:v range from 2 (best) to 31 (worst)
             commandOK = reaper.ExecProcess(command, 10000) -- Unlike os.execute, this function doesn't open a terminal
             if not reaper.file_exists(imagePath) then
                 failed = true
@@ -209,7 +224,7 @@ reaper.UpdateTimeline()
 msg = (failed and (commandOK .. "\n\nScript terminated by failure to extract thumbnail at position: "..offsetStringForCommand.."\nThe edit cursor has been moved to this position, and the ffmpeg error message is above.") or "\n\nDone!")
       .."\n\nSuccessfully added: "..string.format("%i", countSuccess)
       .."\nNon-overlapping with video items: "..string.format("%i", countNonOverlapping)
-
 reaper.ShowConsoleMsg(msg)
+
 
 reaper.Undo_EndBlock2(0, "Extract thumbnails", -1)
