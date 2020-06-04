8e326eafd4974ab817f2459798221152dfeab57f
Aaron Cendan <65690085+acendan@users.noreply.github.com>
2020-06-04 19:55:26 -0400

    Release Set Color of Subprojects in Selected Items v1.0

---
+++ Items Properties/acendan_Set Subprojects in Selected Items to Custom Color Slot/acendan_Set Subprojects in Selected Items to Random Custom Colors.lua
@@ -0,0 +1,93 @@
+-- @noindex
+
+-- @description Set Subprojects in Selected Items to Random Custom Colors
+-- @author Aaron Cendan
+-- @version 1.0
+-- @link https://aaroncendan.me
+-- @about
+--   # Set Subprojects in Selected Items Color
+--   By Aaron Cendan - June 2020
+--
+--   ### General Info
+--   * Sets color of all subprojects in selected items based on criteria in script name
+--   * Can easily be combined with "Item: Select all items" to affect all subprojects in current project
+--
+--   ### Requirements
+--   * SWS Extension: https://www.sws-extension.org/
+
+-- Set subproject item color
+local function setSubProjItemsColor(filename, track, item)
+  reaper.Main_OnCommand(40289, 0) -- Item: Unselect all items
+  reaper.SetMediaItemSelected( item, true )
+  local set_color_action = reaper.NamedCommandLookup( "_SWS_ITEMRANDCOL" ) --Set item to one random custom color
+  reaper.Main_OnCommand(set_color_action, 0)
+
+  reaper.UpdateArrange()
+end
+
+-- Save item selection
+function saveSelectedItems (table)
+  for i = 1, reaper.CountSelectedMediaItems(0) do
+    table[i] = reaper.GetSelectedMediaItem(0, i-1)
+  end
+end
+
+-- Restore item selection
+function restoreSelectedItems(table)
+  for i = 1, tableLength(table) do
+    reaper.SetMediaItemSelected( table[i], true )
+  end
+end
+
+-- Get table length
+function tableLength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+-- Get file name of active take
+local function getFilenameTrackActiveTake(item)
+  if item ~= nil then
+    local tk = reaper.GetActiveTake(item)
+    if tk ~= nil then
+      local pcm_source = reaper.GetMediaItemTake_Source(tk)
+      local filenamebuf = ""
+      filenamebuf = reaper.GetMediaSourceFileName(pcm_source, filenamebuf)
+      local track = reaper.GetMediaItemTrack(item)
+      return filenamebuf, track
+    end
+  end
+  return nil, nil
+end
+
+-- Get three character extension on filename
+local function getFileExtension(filename)
+  return filename:sub(-3):upper()
+end
+
+-- Main - filter out subprojects in selected items
+local function main()
+  local at_least_one_subproj = false
+  for i, item in ipairs( init_sel_items ) do
+    local filename, track = getFilenameTrackActiveTake(item)
+    if filename ~= nil then
+      if getFileExtension(filename) == "RPP" then
+        setSubProjItemsColor(filename, track, item)
+        at_least_one_subproj = true
+      end
+    end
+  end
+  if not at_least_one_subproj then reaper.MB("No subprojects in selected items!","Set Subproject Color",0) end
+end
+
+reaper.PreventUIRefresh(1)
+reaper.Undo_BeginBlock()
+reaper.ClearConsole()
+init_sel_items = {}
+saveSelectedItems( init_sel_items )
+main()
+restoreSelectedItems( init_sel_items )
+reaper.Undo_EndBlock("Set selected subproject colors",-1)
+reaper.UpdateArrange()
+reaper.PreventUIRefresh(-1)
