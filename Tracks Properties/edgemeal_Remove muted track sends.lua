f59f720941ad03cd1d7bf6362e02f0b3ff641a4a
Edgemeal <47255672+Edgemeal@users.noreply.github.com>
2020-07-30 09:26:33 -0500

    Release Remove muted track sends v1.0

---
+++ Tracks Properties/edgemeal_Remove muted track sends.lua
@@ -0,0 +1,21 @@
+-- @description Remove muted track sends
+-- @author Edgemeal
+-- @version 1.0
+-- @link Forum Thread https://forum.cockos.com/showthread.php?t=239747
+-- @donation Donate https://www.paypal.me/Edgemeal
+
+reaper.PreventUIRefresh(1)
+reaper.Undo_BeginBlock()
+local track_count = reaper.CountTracks(0)
+for i = 0, track_count-1 do
+  track = reaper.GetTrack(0,i)
+  local send_cnt = reaper.GetTrackNumSends(track, 0)
+  for send_index = send_cnt-1,0,-1 do
+    local muted = reaper.GetTrackSendInfo_Value(track, 0, send_index, "B_MUTE")
+    if muted == 1 then reaper.RemoveTrackSend(track, 0, send_index) end
+  end
+end
+reaper.Undo_EndBlock('Remove muted track sends', -1)
+reaper.TrackList_AdjustWindows(false)
+reaper.UpdateArrange()
+reaper.PreventUIRefresh(-1)
