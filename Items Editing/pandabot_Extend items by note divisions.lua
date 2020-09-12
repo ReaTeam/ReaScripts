ff535c7a9cd507048d5936a0470c2540d9ed0df0
benjohnson2001 <ben@letskedaddle.com>
2020-09-12 15:06:41 -0400

    added Extend items by note divisions scripts (#490)

---
+++ Items Editing/pandabot_Extend items by note divisions.lua
@@ -0,0 +1,21 @@
+--[[
+Description: Extend items by note divisions
+Author: pandabot
+License: MIT
+Version: 1.0
+Donate: https://paypal.me/benjohnson2001
+Links:
+  pandabot (Cockos forum) https://forum.cockos.com/member.php?u=127396
+  Source repository (GitHub) https://github.com/benjohnson2001/ExtendItemsByNoteDivisions
+Metapackage: true
+Provides:
+    [main] pandabot_Extend items by note divisions/*.lua
+About:
+    ### Extend items by note divisions
+    
+    #### What is this?
+    Extend items by note division lengths with fade-in/fade-outs and snap offset, useful for comping
+    
+    #### How do I use it?
+    There is an instructional gif [here](https://github.com/benjohnson2001/ExtendItemsByNoteDivisions/raw/master/extendItemsByNoteDivision.gif)
+--]]
