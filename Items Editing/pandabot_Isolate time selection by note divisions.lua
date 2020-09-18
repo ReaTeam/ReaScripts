c54fc18f11b1a47a8b80aa321b98b6daf04f2288
Ben <ben@letskedaddle.com>
2020-09-17 20:33:01 -0400

    added scripts pandabot_Isolate items by note divisions, pandabot_Isolate time selection by note divisions

---
+++ Items Editing/pandabot_Isolate time selection by note divisions.lua
@@ -0,0 +1,21 @@
+--[[
+Description: Isolate time selection by note divisions
+Author: pandabot
+License: MIT
+Version: 1.0
+Donate: https://paypal.me/benjohnson2001
+Links:
+  pandabot (Cockos forum) https://forum.cockos.com/member.php?u=127396
+  Source repository (GitHub) https://github.com/benjohnson2001/IsolateTimeSelectionByNoteDivisions
+Metapackage: true
+Provides:
+    [main] pandabot_Isolate time selection by note divisions/*.lua
+About:
+    ### Isolate time selection by note divisions
+    
+    #### What is this?
+    Isolate time selection of selected tracks by note division lengths with fade-in and fade-outs, useful for comping vst output
+    
+    #### How do I use it?
+    There is an instructional gif [here](https://raw.githubusercontent.com/benjohnson2001/IsolateTimeSelectionByNoteDivisions/master/isolateTimeSelectionByNoteDivisions.gif)
+--]]
