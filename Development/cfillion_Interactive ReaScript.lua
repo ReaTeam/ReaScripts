429d32fe9cb016fe1255a6713000d0c7d6df20fb
Christian Fillion <cfillion@users.noreply.github.com>
2020-10-02 12:44:50 -0400

    Release Interactive ReaScript (iReaScript) v0.8.4
    Fix autocompletion after an opening parenthesis

--- Development/cfillion_Interactive ReaScript.lua
+++ Development/cfillion_Interactive ReaScript.lua
@@ -1,10 +1,7 @@
 -- @description Interactive ReaScript (iReaScript)
 -- @author cfillion
--- @version 0.8.3
--- @changelog
---   Fix blinking caret
---   Fix initial blank window on macOS and REAPER 5
---   Print the command name when running actions (SWS v2.10+ only)
+-- @version 0.8.4
+-- @changelog Fix autocompletion after an opening parenthesis
 -- @link
 --   cfillion.ca https://cfillion.ca
 --   Forum Thread https://forum.cockos.com/showthread.php?t=177324
@@ -751,11 +748,11 @@ function ireascript.loop()
   if now - ireascript.lastRedraw >= 1 then
     -- let the cursor caret blink
     ireascript.redraw = true
-    ireascript.lastRedraw = now
   end
 
   if ireascript.redraw then
     ireascript.redraw = false
+    ireascript.lastRedraw = now
     ireascript.draw()
   end
 
@@ -812,6 +809,10 @@ function ireascript.nl()
   ireascript.buffer[#ireascript.buffer + 1] = ireascript.SG_NEWLINE
 end
 
+function ireascript.stripTabs(contents)
+  return contents:gsub("\t", string.rep("\x20", ireascript.INDENT))
+end
+
 function ireascript.push(contents)
   if contents == nil then
     error('content is nil')
@@ -827,7 +828,7 @@ function ireascript.push(contents)
       ireascript.buffer[#ireascript.buffer + 1] = {
         font=ireascript.font,
         fg=ireascript.foreground, bg=ireascript.background,
-        text=line:gsub("\t", string.rep("\x20", ireascript.INDENT)),
+        text=ireascript.stripTabs(line),
       }
     end
   end
@@ -1330,6 +1331,7 @@ function ireascript.paste(selection)
     clipboard = ireascript.selectedText()
   else
     clipboard = reaper.CF_GetClipboard('')
+    clipboard = ireascript.stripTabs(clipboard)
   end
 
   for line in ireascript.lines(clipboard) do
@@ -1365,7 +1367,7 @@ function ireascript.complete()
 
   local code = ireascript.prepend .. "\x20" .. before
   local matches, source = {}, _G
-  local prefix, word = code:match("([%a%d_%s%.]*[%a%d_]+)%s*%.%s*([^%s]*)$")
+  local prefix, word = code:match("([%a%d_%s%.]*[%a%d_]+)%s*%.%s*([^(%s]*)$")
 
   if word then
     for key in prefix:gmatch('[^%.%s]+') do
