b119c562795d41e7d238b294ee5c5de712600f3f
Christian Fillion <cfillion@users.noreply.github.com>
2020-10-17 15:29:28 -0400

    Release Move left/right edge of selected automation items to start/end of time selection v1.0
    Add an action for moving the left edge to the start of the time selection [p=2353888]

--- Envelopes/Automation Items/cfillion_Move right edge of selected automation items to end of time selection.lua
+++ Envelopes/Automation Items/cfillion_Move right edge of selected automation items to end of time selection.lua
@@ -1,19 +1,24 @@
--- @description Move right edge of selected automation items to end of time selection
--- @version 1.0
+-- @description Move left/right edge of selected automation items to start/end of time selection
 -- @author cfillion
+-- @version 1.0
+-- @changelog Add an action for moving the left edge to the start of the time selection [p=2353888]
+-- @provides
+--   .
+--   [main] . > cfillion_Move left edge of selected automation items to start of time selection.lua
 -- @link cfillion.ca https://cfillion.ca
--- @donate https://www.paypal.com/cgi-bin/webscr?business=T3DEWBQJAV7WL&cmd=_donations&currency_code=CAD&item_name=ReaScript%3A+Move+right+edge+of+selected+automation+items+to+end+of+time+selection
+-- @donation https://www.paypal.com/cgi-bin/webscr?business=T3DEWBQJAV7WL&cmd=_donations&currency_code=CAD&item_name=ReaScript%3A+Move+right+edge+of+selected+automation+items+to+end+of+time+selection
 
 local UNDO_STATE_TRACKCFG = 1
 
 local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+).lua$")
+local right_edge = script_name:match('right edge')
 
 reaper.defer(function() end)
 
 local tstart, tend = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
 if tstart == tend then return end
 
-local env = reaper.GetSelectedEnvelope(0)
+local env = reaper.GetSelectedTrackEnvelope(0)
 if not env then return end
 
 local bucket = {}
@@ -24,7 +29,11 @@ for i=0,reaper.CountAutomationItems(env)-1 do
   local length = reaper.GetSetAutomationItemInfo(env, i, 'D_LENGTH', 0, false)
 
   if selected and startTime < tend then
-    table.insert(bucket, {id=i, len=tend - startTime})
+    if right_edge then
+      table.insert(bucket, {id=i, len=tend - startTime})
+    else
+      table.insert(bucket, {id=i, pos=tstart, len=length + (startTime - tstart)})
+    end
   end
 end
 
@@ -33,6 +42,10 @@ if #bucket < 1 then return end
 reaper.Undo_BeginBlock()
 
 for _,ai in ipairs(bucket) do
+  if ai.pos then
+    reaper.GetSetAutomationItemInfo(env, ai.id, 'D_POSITION', ai.pos, true)
+  end
+
   reaper.GetSetAutomationItemInfo(env, ai.id, 'D_LENGTH', ai.len, true)
 end
 
