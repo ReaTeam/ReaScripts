535777791086f22012dae70f56ed4c147b6466c0
Benjamin Futász <34510704+bfut@users.noreply.github.com>
2020-11-17 04:44:52 +0100

    Release, Update bfut Scripts

--- Items Editing/bfut_Trim to source media lengths (limit items lengths).lua
+++ Items Editing/bfut_Trim to source media lengths (limit items lengths).lua
@@ -1,56 +1,57 @@
 --[[
   @author bfut
-  @version 1.0
+  @version 1.1
   @description Trim to source media lengths (limit items lengths)
-  @website https://github.com/bfut
   @about
     HOW TO USE:
       1) Select media item(s).
       2) Run the script.
-    REQUIRES: Reaper v5.70 or later
+    REQUIRES: Reaper v5.99 or later
+  @changelog
+    + ignore locked items
+  @website https://github.com/bfut
   LICENSE:
-    Copyright (c) 2017 and later Benjamin Futasz <bendfu@gmail.com><https://github.com/bfut>
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+    Copyright (C) 2017 and later Benjamin Futasz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
-local reaper = reaper
-reaper.Undo_BeginBlock2(0)
-local gl_CountSelectedMediaItems = reaper.CountSelectedMediaItems(0)
-if gl_CountSelectedMediaItems < 1 then
+local COUNT_SEL_ITEMS = reaper.CountSelectedMediaItems(0)
+if COUNT_SEL_ITEMS < 1 then
   return
 end
+local IS_ITEM_LOCKED = {
+  [1.0] = true,
+  [3.0] = true
+}
+reaper.Undo_BeginBlock2(0)
 reaper.PreventUIRefresh(1)
-for i=0,gl_CountSelectedMediaItems-1 do
-  local item = reaper.GetSelectedMediaItem(0,i)
-  local item_activetake = reaper.GetActiveTake(item,0)
-  if item_activetake ~= nil then
-    local item_length = reaper.GetMediaItemInfo_Value(item,"D_LENGTH")
-    local item_activetake_mediasourcelength, lengthIsQN = reaper.GetMediaSourceLength(reaper.GetMediaItemTake_Source(item_activetake))
-    if lengthIsQN then
-      item_activetake_mediasourcelength = reaper.TimeMap_QNToTime(item_activetake_mediasourcelength)
-    end
-    item_activetake_mediasourcelength = item_activetake_mediasourcelength/reaper.GetMediaItemTakeInfo_Value(item_activetake,"D_PLAYRATE")
-    if item_length > item_activetake_mediasourcelength then
-      reaper.SetMediaItemLength(item,item_activetake_mediasourcelength,false)
+for i = 0, COUNT_SEL_ITEMS - 1 do
+  item = reaper.GetSelectedMediaItem(0, i)
+  if not IS_ITEM_LOCKED[reaper.GetMediaItemInfo_Value(item, "C_LOCK")] then
+    local item_activetake = reaper.GetActiveTake(item, 0)
+    if item_activetake then
+      local item_activetake_mediasourcelength, lengthIsQN = reaper.GetMediaSourceLength(reaper.GetMediaItemTake_Source(item_activetake))
+      if lengthIsQN then
+        item_activetake_mediasourcelength = reaper.TimeMap_QNToTime(item_activetake_mediasourcelength)
+      end
+      item_activetake_mediasourcelength = item_activetake_mediasourcelength / reaper.GetMediaItemTakeInfo_Value(item_activetake, "D_PLAYRATE")
+      if reaper.GetMediaItemInfo_Value(item, "D_LENGTH") > item_activetake_mediasourcelength then
+        reaper.SetMediaItemLength(item, item_activetake_mediasourcelength, false)
+      end
     end
   end
 end
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
-reaper.Undo_EndBlock2(0,"Trim to source media lengths (limit items lengths)",-1)
+reaper.Undo_EndBlock2(0, "bfut_Trim to source media lengths (limit items lengths)", -1)
