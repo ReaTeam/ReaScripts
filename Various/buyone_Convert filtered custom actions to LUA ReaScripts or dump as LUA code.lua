9dda86b9257fa4c6bc569be4912b9da507108fc6
Buy-One <61803004+Buy-One@users.noreply.github.com>
2020-12-27 16:38:12 +0200

    Update buyone_Convert filtered custom actions to LUA ReaScripts or dump as LUA code.lua

--- Various/buyone_Convert filtered custom actions to LUA ReaScripts or dump as LUA code.lua
+++ Various/buyone_Convert filtered custom actions to LUA ReaScripts or dump as LUA code.lua
@@ -1,7 +1,3 @@
--- @description Convert filtered custom actions to LUA ReaScripts or dump as LUA code
--- @author BuyOne
--- @version 1.0
-
 --[[
 
 * ReaScript Name: BuyOne_Convert filtered custom actions to LUA ReaScripts or dump as LUA code.lua
@@ -14,6 +10,15 @@
 * Version: 1.0
 * REAPER: at least v5.962
 * Extensions: SWS/S&M
+* Changelog:
+	+ v1.1 	Correction of typos in comments
+			Removal of redundant info from Help
+			Added support for ancillary actions often used within custom action:
+			Action: Wait X seconds before next action
+			Action: Prompt to continue (only valid within custom actions)
+			Action: Set action loop start (only valid within custom actions)
+			Action: Prompt to go to action loop start (only valid within custom actions)
+	+ v1.0 	Initial release
 
 ]]
 
@@ -43,13 +48,9 @@ The search is geared towards words in alphanumeric characters, is case insensiti
 
 Semi-fuzzy search operator is only supported as the last character in the search term so phrases can only be extended by the length of their last word/number. On the flipside exact search for strings ending with + isn't possible.
 
-Boolean 'and' search is realized with = operator. Use it between search terms (save for phrases) without spaces when you need several search terms to feature in a custom action name.
+The default search syntax works as boolean OR. Boolean AND search is realized with = operator. Use it between search terms (save for phrases) without spaces when you need several search terms to feature in a custom action name.
 
 	E.g. one=2=three
-
-Boolean 'or' search can be fashioned by combining regular syntax with boolean 'and'
-
-	E.g. one;2;three;one=2=three
 
 If search terms are provided the search for matches in custom action names is performed in sections of the Action list selected in the next field with flags. If the search field only contains the default text or is empty ALL custom actions will be exported according to the settings in the next field.
 
@@ -82,7 +83,7 @@ All available combinations:
 1245 - Both Main/Media Explorer and MIDI Editor/MIDI Event List Editor sections custom actions as plain txt dump file, MIDI Editor/MIDI Event List Editor custom actions for use from Arrange
 
 
-III. SPECIFICS
+III. SPECS
 
 Main flag includes Media Explorer custom actions. MIDI flag includes both MIDI Editor and MIDI Event list custom actions.
 
@@ -233,7 +234,7 @@ local lines_t = {}
 
 	if #lines_t == 0 then r.MB('     N o  m a t c h e s.\n\n          ¯\\_(ツ)_/¯','INFO',0) return end
 
-	-- Wade out duplicates stemming from the same custom action name satisfying several search terms, much simpler than preventing duplicates in the table construction loop above
+	-- Weed out duplicates stemming from the same custom action name satisfying several search terms, much simpler than preventing duplicates in the table construction loop above
 	for k1,v1 in next, lines_t do
 		for k2,v2 in next, lines_t do
 			if v2 == v1 and k1 ~= k2 then table.remove(lines_t,k2) end
@@ -263,7 +264,6 @@ local lines_t = {}
 local captures_t = {'0','32063','32060','32061'}
 local tmp_t = {}
 	for _, sect in ipairs(captures_t) do
-		--reorder(lines_t, tmp_t, v)
 		-- Bring custom actions from the same section together, moving their table values in a predefined order of section codes to a temporary table
 		for _,str in next, lines_t do
 		if str:match('%s('..sect..')%s') then tmp_t[#tmp_t+1] = str end
@@ -368,6 +368,21 @@ local pref, f_name, ext = '', inset..tostring(#lines_t)..' custom actions LUA du
 			else actions_t[#actions_t+1] = w end
 		end
 
+		
+	-- Initialize code for ancillary actions
+	local cont_action = '\nr.PreventUIRefresh(-1)\n\tlocal resp = r.MB(\'Continue running the script?\',\'Script paused\',1)\n\tif resp == 2 then return end\n\nr.PreventUIRefresh(1)\n' -- 2000 Action: Prompt to continue (only valid within custom actions)
+	local set_loop_start = '\nr.PreventUIRefresh(-1)\n\n\trepeat\n\nr.PreventUIRefresh(1)\n' -- 2001 Action: Set action loop start (only valid within custom actions)
+	local go_loop_start = '\nr.PreventUIRefresh(-1)\n\n\tlocal resp = r.MB(\'Loop the script from step 1?\',\'Script paused\',1)\n\tuntil resp == 2\n\nr.PreventUIRefresh(1)\n' -- 2002 Action: Prompt to go to action loop start (only valid within custom actions)
+	local function wait(v) -- 2008 - 2012 Action: Wait X seconds before next action
+		if v == '2008' then sec = '0.1'
+		elseif v == '2009' then sec = '0.5'
+		elseif v == '2010' then sec = '1'
+		elseif v == '2011' then sec = '5'
+		elseif v == '2012' then sec = '10' end
+		local wait = '\nr.PreventUIRefresh(-1)\n\nlocal time_stamp = r.time_precise()\n\n\trepeat\n\tlocal cur_time = r.time_precise()\n\tuntil cur_time - time_stamp == '..sec..'\n\nr.PreventUIRefresh(1)'
+		return wait
+	end
+	
 
 	-- Concatenate actions code
 	local code_t = {}
@@ -376,9 +391,23 @@ local pref, f_name, ext = '', inset..tostring(#lines_t)..' custom actions LUA du
 		local id = r.NamedCommandLookup(v) -- works for native actions as well
 		local sect_midi = line:match('%s(3206[01])%s')
 		local sect_media_main = line:match('%s(32063)%s') or '0'
-		--reaper.MB(v:match('%s(32063)%s'),"",0)
-			if sect_midi then str = 'r.MIDIEditor_OnCommand(hwnd,'..func..') -- '..r.CF_GetCommandText(tonumber(sect_midi), id) -- thanks to cfillion https://forum.cockos.com/showthread.php?t=186732
-			else str = 'r.Main_OnCommand('..func..',0) -- '..r.CF_GetCommandText(tonumber(sect_media_main), id) end
+			if sect_midi then 
+				if v == "2000" then str = cont_action
+				elseif v == "2001" then str = set_loop_start; repeat_loop = v -- any value so it's not nil
+				elseif v == "2002" then str = go_loop_start;
+					-- Insert repeat at the very start of action sequence if loop start wasn't set explicitly by 2001
+					if not repeat_loop then table.insert(code_t, 1, set_loop_start) end
+				else str = 'r.MIDIEditor_OnCommand(hwnd,'..func..') -- '..r.CF_GetCommandText(tonumber(sect_midi), id) -- thanks to cfillion https://forum.cockos.com/showthread.php?t=186732
+				end
+			else 
+				if v == "2000" then str = cont_action
+				elseif v == "2001" then str = set_loop_start; repeat_loop = v -- any value so it's not nil
+				elseif v == "2002" then str = go_loop_start;
+					-- Insert repeat at the very start of action sequence if loop start wasn't set explicitly by 2001
+					if not repeat_loop then table.insert(code_t, 1, set_loop_start) end
+				elseif tonumber(v) > 2007 and tonumber(v) < 2013 and sect_media_main == '0' then str = wait(v)
+				else str = 'r.Main_OnCommand('..func..',0) -- '..r.CF_GetCommandText(tonumber(sect_media_main), id) end
+			end
 		code_t[#code_t+1] = str
 		end
 
