f231210263750db2e4301382de994ad0cf1102e9
Buy-One <61803004+Buy-One@users.noreply.github.com>
2021-02-08 18:00:09 +0200

    Create BuyOne_Move pitch cursor.lua

---
+++ MIDI Editor/BuyOne_Move pitch cursor.lua
@@ -0,0 +1,32 @@
+-- @description Move pitch cursor in various permutations
+-- @author BuyOne
+-- @website https://forum.cockos.com/member.php?u=134058
+-- @version 1.0
+-- @about
+--   23 scripts to complement native MIDI Editor pitch cursor actions for use in custom actions
+-- @metapackage
+-- @provides
+--   [main] BuyOne_Move pitch cursor/BuyOne_Add notes at pitch cursor to selection.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Select only notes at pitch cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Deselect only notes at pitch cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move edit and pitch cursors to the next note relative to edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move edit and pitch cursors to the previous note relative to edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to selected note.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the first note.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the last note.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the first note left of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the first note right of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the first of selected notes.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the last of selected notes.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the highest note.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the lowest note.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the highest note left of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the highest note right of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the lowest note left of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the lowest note right of edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the highest of selected notes.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the lowest of selected notes.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the note ending at edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the note starting at edit cursor.lua
+--   [main] BuyOne_Move pitch cursor/BuyOne_Move pitch cursor to the note nearest to or under edit cursor.lua
+
