845070286477df797403fffd38244b4ed586e21c
Triode123 <stefhambrook@gmail.com>
2021-02-17 11:19:12 +0000

    Release Track-based item range selection v0.66

---
+++ Items Editing/triode_Track-based item range selection.lua
@@ -0,0 +1,26 @@
+-- @description Track-based item range selection
+-- @author Triode
+-- @version 0.66
+-- @metapackage
+-- @provides
+--   [main] triode_Track-based item range selection/Track-based item range selection.lua
+--   [main] . > New file 5.lua
+-- @link http://www.outoftheboxsounds.com/
+-- @donation If you like this script please support https://www.paypal.me/outoftheboxsounds
+-- @about
+--   Map this to media item left shift-click in mouse modifiers to allow selection of item ranges based on the folder or track group they are contained in.  Also includes individual item selection and selection of items across all tracks.
+--
+--   Requires SWS exclusive toggles B1 to B4 on the toolbar which correspond to the following item selection modes:
+--
+--   B1: Folder-based item selection
+--   B2: Item selection across all tracks
+--   B3: Individual item selection
+--   B4: Track group based item selection
+--
+--   This script is for the second (shift) click to select a range of items.  Download the other track based scripts to get item first click selection, toggle selection, item split (select right or left) using the same modes.
+--
+--   There are also scripts for unselect all items and making newly recorded items create selections compatible with the above actions.
+--
+--   None of these scripts run anything in the background.
+--   No undo point is created by the item selection scripts (unless you specifically set item select undo in reaper's preferences)
+
