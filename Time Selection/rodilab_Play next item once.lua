af66dcf51cd02642c17caa2ba21601d3ec356b6b
Rodilab <78826646+Rodilab@users.noreply.github.com>
2021-03-04 11:54:51 +0100

    Release Play next item once v1.1 (#660)
    Update : Prevent UI Refresh

--- Time Selection/rodilab_Play next item once.lua
+++ Time Selection/rodilab_Play next item once.lua
@@ -1,12 +1,14 @@
 -- @description Play next item once
 -- @author Rodilab
--- @version 1.0
+-- @version 1.1
+-- @changelog Update : Prevent UI Refresh
 -- @about
 --   Select, set time selection and play the next item of the track once.
 --
 --   by Rodrigo Diaz (aka Rodilab)
 
 reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh(1)
 
 -- Disable "repeat"
 if reaper.GetToggleCommandState(1068) == 1 then
@@ -28,3 +30,5 @@ reaper.Main_OnCommand(41039,0)
 reaper.Main_OnCommand(1007,0)
 
 reaper.Undo_EndBlock("Play next item once",0)
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
