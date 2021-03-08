06e25a47ea58a5b5a2b905821244ca2dcec31d61
Rodilab <78826646+Rodilab@users.noreply.github.com>
2021-03-08 16:13:30 +0100

    Release Move cursor to nearest item edge in selected track v1.0

---
+++ Cursor/rodilab_Move cursor to nearest item edge in selected track.lua
@@ -0,0 +1,15 @@
+-- @description Move cursor to nearest item edge in selected track
+-- @author Rodilab
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] rodilab_Move cursor to nearest item edge in selected track/rodilab_Move cursor left to nearest item edge in selected tracks.lua
+--   [main] rodilab_Move cursor to nearest item edge in selected track/rodilab_Move cursor right to nearest item edge in selected tracks.lua
+--   [main] rodilab_Move cursor to nearest item edge in selected track/rodilab_Move cursor left to nearest item edge in selected tracks ans set time selection.lua
+--   [main] rodilab_Move cursor to nearest item edge in selected track/rodilab_Move cursor right to nearest item edge in selected tracks ans set time selection.lua
+-- @about
+--   Move cursor left/right to nearest item edge in selected tracks (and set or not time selection)
+--   If no track is selected, is a normal "Move cursor to nearest item edge".
+--
+--   by Rodrigo Diaz (aka Rodilab)
+
