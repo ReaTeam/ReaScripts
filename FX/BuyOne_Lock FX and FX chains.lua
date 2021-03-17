52bbc9aa91b5ec021880b697d3c0cdb0ce1a06fd
Buy-One <61803004+Buy-One@users.noreply.github.com>
2021-03-17 23:18:11 +0200

    Create BuyOne_Lock FX and FX chains.lua

---
+++ FX/BuyOne_Lock FX and FX chains.lua
@@ -0,0 +1,12 @@
+-- @description Lock FX and FX chains + Append and remove lock tag
+-- @author BuyOne
+-- @website https://forum.cockos.com/member.php?u=134058
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] BuyOne_Lock FX and FX chains/BuyOne_Lock FX and FX chains (guide inside).lua
+--   [main] BuyOne_Lock FX and FX chains/BuyOne_Lock FX and FX chains - append or remove lock tag (guide inside).lua
+-- @about
+--  Locks individual tagged FX in the focused FX chain or the entire FX chain to prevent inadvertent change of FX order.
+--  Another script automates the task of appending and removing lock tags.
+--  Istructions are provided inside the script files.
