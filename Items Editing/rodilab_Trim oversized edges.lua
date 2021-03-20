641dce873437ac6a10f7e340d7d49a5d57f45764
Rodilab <78826646+Rodilab@users.noreply.github.com>
2021-03-20 13:32:01 +0100

    Release Trim oversized edges v1.2 (#686)

--- Items Editing/rodilab_Trim oversized edges.lua
+++ Items Editing/rodilab_Trim oversized edges.lua
@@ -1,7 +1,6 @@
 -- @description Trim oversized edges
 -- @author Rodilab
--- @version 1.1
--- @changelog Update : Prevent UI Refresh
+-- @version 1.2
 -- @about
 --   Trim item edges that exceed the length of the source media file.
 --
@@ -23,33 +22,37 @@ if count > 0 then
 
   for i, item in ipairs(item_list) do
     local take =  reaper.GetActiveTake(item)
-    local source = reaper.GetMediaItemTake_Source(take)
-    local position = reaper.GetMediaItemInfo_Value(item,"D_POSITION")
-    local length = reaper.GetMediaItemInfo_Value(item,"D_LENGTH")
-    local playrate = reaper.GetMediaItemTakeInfo_Value(take,"D_PLAYRATE")
-    local startoffs = reaper.GetMediaItemTakeInfo_Value(take,"D_STARTOFFS")/playrate
-    local source_length, lengthIsQN = reaper.GetMediaSourceLength(source)
-    if lengthIsQN == true then
-      source_length = reaper.TimeMap_QNToTime(source_length)
-    end
-    source_length = source_length/playrate
-    local samplerate = reaper.GetMediaSourceSampleRate(source)
+    if take then
+      local source = reaper.GetMediaItemTake_Source(take)
+      local samplerate = reaper.GetMediaSourceSampleRate(source)
+      if samplerate > 0 then
+        local position = reaper.GetMediaItemInfo_Value(item,"D_POSITION")
+        local length = reaper.GetMediaItemInfo_Value(item,"D_LENGTH")
+        local playrate = reaper.GetMediaItemTakeInfo_Value(take,"D_PLAYRATE")
+        local startoffs = reaper.GetMediaItemTakeInfo_Value(take,"D_STARTOFFS")/playrate
+        local source_length, lengthIsQN = reaper.GetMediaSourceLength(source)
+        if lengthIsQN == true then
+          source_length = reaper.TimeMap_QNToTime(source_length)
+        end
+        source_length = source_length/playrate
 
-    reaper.SelectAllMediaItems(0,false)
-    reaper.SetMediaItemSelected(item,true)
+        reaper.SelectAllMediaItems(0,false)
+        reaper.SetMediaItemSelected(item,true)
 
-    if startoffs < 0 and samplerate > 0 then
-      -- Trim left edge to cursor
-      cursor = reaper.GetCursorPosition()
-      reaper.MoveEditCursor(position-startoffs-cursor,false)
-      reaper.Main_OnCommand(41305,0)
-    end
+        if startoffs < 0 then
+          -- Trim left edge to cursor
+          cursor = reaper.GetCursorPosition()
+          reaper.MoveEditCursor(position-startoffs-cursor,false)
+          reaper.Main_OnCommand(41305,0)
+        end
 
-    if length+startoffs > source_length and samplerate > 0 then
-      -- Trim right edge to cursor
-      cursor = reaper.GetCursorPosition()
-      reaper.MoveEditCursor(position - startoffs + source_length - cursor,false)
-      reaper.Main_OnCommand(41311,0)
+        if length+startoffs > source_length then
+          -- Trim right edge to cursor
+          cursor = reaper.GetCursorPosition()
+          reaper.MoveEditCursor(position - startoffs + source_length - cursor,false)
+          reaper.Main_OnCommand(41311,0)
+        end
+      end
     end
   end
 
