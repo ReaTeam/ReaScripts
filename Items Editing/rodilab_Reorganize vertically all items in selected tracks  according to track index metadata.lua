fd5c5b38ef102882eb82c3896fbf92b37e1b1ac8
Rodilab <78826646+Rodilab@users.noreply.github.com>
2021-03-31 09:36:44 +0200

    Release Reorganize vertically all items in selected tracks according to track index metadata v1.1
    Update : Read iXML Track List metadata

--- Items Editing/rodilab_Reorganize vertically all items in selected tracks  according to track index metadata.lua
+++ Items Editing/rodilab_Reorganize vertically all items in selected tracks  according to track index metadata.lua
@@ -1,8 +1,9 @@
--- @description Reorganize vertically all items in selected tracks  according to track index metadata
+-- @description Reorganize vertically all items in selected tracks according to track index metadata
 -- @author Rodilab
--- @version 1.0
+-- @version 1.1
+-- @changelog Update : Read iXML Track List metadata
 -- @about
---   Reorganize vertically all items in selected tracks  according to track index metadata.
+--   Reorganize vertically all items in selected tracks according to track index metadata.
 --   To do this, items are moved up or down in the selected tracks.
 --   If additional tracks are needed, then they are created (with the parameters of the first selected track).
 --   Note: selected tracks must follow each other.
@@ -10,7 +11,7 @@
 --   by Rodrigo Diaz (aka Rodilab)
 
 -- List of track parameter to copy/past for new tracks
-script_name = "Reorganize vertically all items in selected tracks  according to track index metadata"
+script_name = "Reorganize vertically all items in selected tracks according to track index metadata"
 
 local track_parnames = {
   B_MUTE = "",
@@ -87,17 +88,35 @@ if count_sel_tracks > 0 then
           local source = reaper.GetMediaItemTake_Source(take)
           if reaper.GetMediaSourceType(source,"") == "WAVE" then
 
-            -- Get tracks index in BWF metadata
+            -- Set all track index in a list
             local list_track_index = {}
-            local retval, description = reaper.GetMediaFileMetadata(source,"BWF:Description")
-            if retval then
-              for w in string.gmatch(description, "([^\n\r]+)") do
-                for k, v in string.gmatch(w, "(.+)=(.+)") do
-                  local find = string.find(k,"TRK")
-                  if find then
-                    local id = tonumber(string.sub(k,find+3,string.len(k)))
-                    if id then
-                      table.insert(list_track_index,id)
+            -- First search in iXML
+            local retval, tracklist_count = reaper.GetMediaFileMetadata(source,"IXML:TRACK_LIST:TRACK_COUNT")
+            if retval == 1 then
+              for i=1, tracklist_count do
+                local tracklist_interleave = ""
+                if i > 1 then
+                  tracklist_interleave = tracklist_interleave..":"..i
+                end
+                local retval, tracklist_index = reaper.GetMediaFileMetadata(source,"IXML:TRACK_LIST:TRACK:CHANNEL_INDEX"..tracklist_interleave)
+                if retval == 1 then
+                  table.insert(list_track_index,tonumber(tracklist_index))
+                end
+              end
+            else
+              -- No IXML Track List, search in BWF Description
+              for i=1, 64 do
+                local retval, description = reaper.GetMediaFileMetadata(source,"BWF:Description")
+                if retval then
+                  for w in string.gmatch(description, "([^\n\r]+)") do
+                    for k, v in string.gmatch(w, "(.+)=(.+)") do
+                      local find = string.find(k,"TRK")
+                      if find then
+                        local id = tonumber(string.sub(k,find+3,string.len(k)))
+                        if id then
+                          table.insert(list_track_index,id)
+                        end
+                      end
                     end
                   end
                 end
