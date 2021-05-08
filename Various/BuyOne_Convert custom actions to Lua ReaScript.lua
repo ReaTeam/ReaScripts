8e54f385a3ad991462c788ea4fe157072cf822fe
Buy-One <61803004+Buy-One@users.noreply.github.com>
2021-05-08 17:10:03 +0300

    Update BuyOne_Convert custom actions to Lua ReaScript.lua

--- Various/BuyOne_Convert custom actions to Lua ReaScript.lua
+++ Various/BuyOne_Convert custom actions to Lua ReaScript.lua
@@ -9,7 +9,10 @@
 --   [main] buyone_Convert custom actions to Lua ReaScript/BuyOne_Convert filtered custom actions to Lua ReaScripts or dump as Lua code.lua
 -- @about
 --   3 scripts to convert custom actions to Lua scripts or to dump their Lua code as txt files:
+--
 --   BuyOne_Convert custom action to Lua ReaScript.lua
+--
 --   BuyOne_Convert custom actions in batch to Lua ReaScripts or dump as Lua code.lua
+--
 --   BuyOne_Convert filtered custom actions to Lua ReaScripts or dump as Lua code.lua
 
