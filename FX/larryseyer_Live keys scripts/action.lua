6943572e6c79b6692bc20f0a9b7832b817ec1c0b
Christian Fillion <cfillion@users.noreply.github.com>
2021-06-15 08:20:54 -0700

    Release Live keys scripts v1.0.0-1
    Fix broken include path due to packaging changes

--- FX/larryseyer_Live keys scripts/action.lua
+++ FX/larryseyer_Live keys scripts/action.lua
@@ -7,16 +7,7 @@
 
 local script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
 local this_live_track = tonumber(script_name:match("Track (%d+)")) or -1
+local path = debug.getinfo(1, 'S').source:match('^@(.+)[\\//]')
 
-function get_script_path()
-  if reaper.GetOS() == "Win32" or reaper.GetOS() == "Win64" then
-    return debug.getinfo(1,'S').source:match("(.*".."\\"..")"):sub(2) .. "\\" -- remove "@"
-  end
-    return debug.getinfo(1,'S').source:match("(.*".."/"..")"):sub(2) .. "/"
-end
-
-local path = string.format('%s/../larryseyer_Live keys scripts.lua', get_script_path())
-
-dofile(path)
-
+dofile(string.format('%s/larryseyer_Live keys scripts.lua', path))
 Live_Inst_Main_Logic(this_live_track)
