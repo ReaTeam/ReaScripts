6e3305a3da98b5fc4350b12d2c32f955c19d814e
amagalma <amagalma@gmail.com>
2021-07-31 20:05:42 +0300

    Update amagalma_Show path from list menu (Resource, Selected Item, Project File, Record, Secondary Record, Render).lua

--- Various/amagalma_Show path from list menu (Resource, Selected Item, Project File, Record, Secondary Record, Render).lua
+++ Various/amagalma_Show path from list menu (Resource, Selected Item, Project File, Record, Secondary Record, Render).lua
@@ -1,6 +1,7 @@
 -- @description Show path from list menu (Resource, Selected Item, Project File, Record, Secondary Record, Render)
 -- @author amagalma
--- @version 1.00
+-- @version 1.1
+-- @changelog - Added "First selected script's path in the Actions List"
 -- @link https://forum.cockos.com/showthread.php?t=239556
 -- @screenshot https://i.ibb.co/vhMDkZn/Show-path-from-list-menu.gif
 -- @donation https://www.paypal.me/amagalma
@@ -12,6 +13,7 @@
 --   - Record path
 --   - Secondary Record path
 --   - Render path
+--   - First selected script's path in the Actions List
 --
 --   - Requires JS_ReaScriptAPI and SWS extensions
 
@@ -33,6 +35,7 @@ local t = {
       {"Record path", 40024},
       {"Secondary Record path", 40028},
       {"Render path", '_AUTORENDER_OPEN_RENDER_PATH'},
+      {"First selected script in Actions List", false}
 }
 
 local menu = ""
@@ -56,10 +59,71 @@ local selection = gfx.showmenu(menu)
 gfx.quit()
 
 if selection > 0 then
-  if selection == 4 and proj_file ~= "" then
+  if selection == 4 and proj_file ~= "" then --------------
+  
     reaper.CF_LocateInExplorer( proj_file )
-  else
+    
+  elseif selection == 8 then --------------------
+  
+    if reaper.GetToggleCommandState( 40605 ) ~= 1 then
+      return reaper.defer(function() end)
+    end
+    
+    local hWnd_action = reaper.JS_Window_Find("Actions", true)
+    
+    local function SendMsg( number )
+      reaper.JS_WindowMessage_Send(hWnd_action, "WM_COMMAND", number, 0, 0, 0)
+    end
+    
+    local hWnd_LV = reaper.JS_Window_FindChildByID(hWnd_action, 1323)
+    -- get selected count & selected indexes
+    local sel_count, sel_indexes = reaper.JS_ListView_ListAllSelItems(hWnd_LV)
+    if sel_count == 0 then
+      return reaper.defer(function() end)
+    end
+    
+    local path
+    local fisrt_sel_index = tonumber(sel_indexes:match("[^,]+"))
+    local lv_header = reaper.JS_Window_HandleFromAddress(reaper.JS_WindowMessage_Send(hWnd_LV, "0x101F", 0,0,0,0)) -- 0x101F = LVM_GETHEADER
+    local lv_column_count = reaper.JS_WindowMessage_Send(lv_header, "0x1200" ,0,0,0,0) -- 0x1200 = HDM_GETITEMCOUNT
+    
+    if lv_column_count == 3 then
+      SendMsg(41170) -- show Command ID column
+      SendMsg(41387) -- show Command Paths column
+      path = reaper.JS_ListView_GetItemText(hWnd_LV, fisrt_sel_index, 4)
+      SendMsg(41170)
+      SendMsg(41387)
+    elseif lv_column_count == 4 then
+      last_item = reaper.JS_ListView_GetItemText(hWnd_LV, 0, 3)
+      if last_item:match("^_") or last_item:match("%d+") then
+        -- show Command Paths column
+        SendMsg(41387)
+        path = reaper.JS_ListView_GetItemText(hWnd_LV, fisrt_sel_index, 4)
+        SendMsg(41387)
+      end
+    else
+      path = reaper.JS_ListView_GetItemText(hWnd_LV, fisrt_sel_index, 4)
+    end
+    
+    if path and path ~= "" then
+      local absolute
+      local name = reaper.JS_ListView_GetItemText(hWnd_LV, fisrt_sel_index, 1):gsub(".+: ", "", 1)
+      local sep = package.config:sub(1,1)
+      if string.match(reaper.GetOS(), "Win") then
+        if path:match("^%a:\\") or path:match("^\\\\") then
+          absolute = true
+        end
+      else -- unix
+        absolute = path:match("^/")
+      end
+      reaper.CF_LocateInExplorer( (absolute and "" or reaper.GetResourcePath() .. sep .. "Scripts" .. sep) ..
+      path .. sep .. name )
+    end
+    
+  else ---------------------
+  
     reaper.Main_OnCommand(reaper.NamedCommandLookup(t[selection][2]), 0)
+    
   end
 end
 reaper.defer(function() end)
