22dfa4613cd80e4a23d09700960e943ad3434a37
JR Taylor <81386894+JRTaylorMusic@users.noreply.github.com>
2021-09-28 23:35:42 -0500

    Release Scroll View OR Move Notes Up or Down v1.0

---
+++ MIDI Editor/JRT_Scroll View OR Move Notes Up or Down.lua
@@ -0,0 +1,9 @@
+-- @description Scroll View OR Move Notes Up or Down
+-- @author JRTaylorMusic
+-- @version 1.0
+-- @metapackage
+-- @provides
+--   [main] JRT_Scroll View OR Move Notes Up or Down/JRT_Navigate-Scroll View OR Move Notes Up.lua
+--   [main] JRT_Scroll View OR Move Notes Up or Down/JRT_Navigate-Scroll View OR Move Notes Down.lua
+-- @about Midi Editor Navigation; If note(s) selected, move up/down; If no notes selected, scroll view up/down
+
