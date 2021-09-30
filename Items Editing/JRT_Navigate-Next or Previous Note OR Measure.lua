db1665236fa17ce042436262e0789547744db64a
cfillion <cfillion@users.noreply.github.com>
2021-09-30 19:39:19 -0400

    move to the 'MIDI Editor' category, fix 'New file 6.lua' filename, rename to sentence case

--- Items Editing/JRT_Navigate-Next or Previous Note OR Measure.lua
+++
@@ -1,9 +0,0 @@
--- @description Navigate-Next or Previous Note OR Measure
--- @author JRTaylorMusic
--- @version 1.0
--- @metapackage
--- @provides
---   [main=midi_editor] JRT_Navigate-Next or Previous Note OR Measure/JRT_Navigate-Next Note OR Measure.lua
---   [main=midi_editor] JRT_Navigate-Next or Previous Note OR Measure/New file 6.lua > JRT_Navigate-Next or Previous Note OR Measure/JRT_Navigate-Previous Note OR Measure.lua
--- @about MIDI Editor Navigation; If note(s) selected, move cursor to (and select only) next or previous note; If no note selected, move cursor to next/previous measure.
-
