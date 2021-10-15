5536c9b3bc7b9ed62333f8650f773ecc069fb005
AZpercussion <70285853+AZpercussion@users.noreply.github.com>
2021-10-15 05:06:04 +0300

    Release Move cursor leaving items selected (for mouse click in track area) v1.0

---
+++ Cursor/az_Move cursor leaving items selected (for mouse click in track area).lua
@@ -0,0 +1,14 @@
+-- @description Move cursor leaving items selected (for mouse click in track area)
+-- @author AZ
+-- @version 1.0
+
+playstate = reaper.GetPlayStateEx(0)
+
+if playstate == 1 then
+  reaper.Main_OnCommandEx(40513, 0, 0) -- move edit cursor to mouse
+  reaper.Main_OnCommandEx(1007, 0, 0) -- play
+else
+  reaper.Main_OnCommandEx(40513, 0, 0) -- move edit cursor to mouse
+end
+
+reaper.defer(function()end)
