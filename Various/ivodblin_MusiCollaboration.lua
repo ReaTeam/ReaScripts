5d0a78039e850a331c564a74488de600197b1c0b
Ivo <42642554+IvoDueblin@users.noreply.github.com>
2021-11-02 12:49:59 +0100

    Release MusiCollaboration v1.2.1

---
+++ Various/ivodblin_MusiCollaboration.lua
@@ -0,0 +1,16 @@
+-- @description MusiCollaboration
+-- @author Ivo Düblin
+-- @version 1.2.1
+-- @provides
+--   ivodblin_MusiCollaboration/MC_CollabControl.lua
+--   ivodblin_MusiCollaboration/MC_ProcessProject.lua
+--   ivodblin_MusiCollaboration/MC_ToggleIndicator.lua
+-- @about
+--   MusiCollaboration (MuCol) enables efficient collaboration for composers, musicians and engineers. 
+--   MuCol manages the distribution of the recording results and ensures secure, conflict free data exchange.
+
+function main()
+	reaper.ShowMessageBox("Please refer to www.mucol.ch for datails on how to setup and use MuCol.", "MusiCollaboration", 0)
+end
+
+main()
