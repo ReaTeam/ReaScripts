52c5c648ce6b895292682d9765abf50c93f03e34
Nicola Lunghi <25422924+nicola-lunghi@users.noreply.github.com>
2022-04-09 23:18:14 +0100

    Release import export track group names to text file v1.0.0
    First Release

---
+++ Project Properties/nickstomp_import export track group names to text file.lua
@@ -0,0 +1,13 @@
+-- @description import export track group names to text file
+-- @author Nickstomp
+-- @version 1.0.0
+-- @changelog First Release
+-- @provides
+--   [main] nickstomp_import export track group names to text file/nickstomp_export_track_group_names.lua
+--   [main] nickstomp_import export track group names to text file/nickstomp_import_track_group_names.lua
+-- @about
+--   # Export and import the track group names to a text file
+--
+--   This allows to export the group names from a project and import it into another one.
+
+
