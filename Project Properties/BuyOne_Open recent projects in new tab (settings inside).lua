68c4f78469ac3da5c8bb04258ef238c539710613
Buy-One <61803004+Buy-One@users.noreply.github.com>
2022-04-11 17:57:35 +0300

    Update BuyOne_Open recent projects in new tab (settings inside).lua

--- Project Properties/BuyOne_Open recent projects in new tab (settings inside).lua
+++ Project Properties/BuyOne_Open recent projects in new tab (settings inside).lua
@@ -4,7 +4,8 @@ Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058
 Version: 1.1
 Changelog: #Fixed bug preventing loading in a new tab an already open project
-	   #Improved undo point handling when error
+		   #Improved undo point handling when error
+		   #Added a setting to only list valid projects
 Licence: WTFPL
 REAPER: at least v5.962
 About: 
@@ -19,17 +20,28 @@ About:
 	Also allows opening in another tab an instance of an already open
 	project which the native option doesn't allow.  
 	https://forum.cockos.com/showthread.php?t=265300
+	
+	However unlike in the native menu, if project file or path are 
+	no longer valid, its entry cannot be cleared from the script menu 
+	and will be listed as long as it's present in reaper.ini file.   
+	To compensate for this a setting VALID_PROJECTS_ONLY has been added
+	to the USER SETTINGS
 
 ]]
 -----------------------------------------------------------------------------
 ------------------------------ USER SETTINGS --------------------------------
 -----------------------------------------------------------------------------
--- To have the menu display only project names without the path 
--- and the extension, insert any alphanumeric characted between 
+-- To enable a setting insert any QWERTY alphanumeric character between
 -- the quotation marks.
--- The max number of projects in the recent project list still depends on 
+
+-- Enable to have the menu only list valid (loadable) projects
+VALID_PROJECTS_ONLY = ""
+
+-- Enable to have the menu only display project names without the path
+-- and the extension;
+-- the max number of projects in the recent project list still depends on
 -- the setting at Preferences -> General -> Maximum projects in recent project list:
--- The script doesn't allow removing invalid entries from the list.
+-- the script doesn't allow removing invalid entries from the list.
 
 PROJECT_NAMES_ONLY = ""
 
@@ -45,20 +57,34 @@ end
 
 local r = reaper
 
+function validate_sett(sett) -- can be either a non-empty string or a number
+return type(sett) == 'string' and #sett:gsub(' ','') > 0 or type(sett) == 'number'
+end
+
+VALID_PROJECTS_ONLY = validate_sett(VALID_PROJECTS_ONLY)
+
 -- collect recent project paths
 local t = {}
 	for line in io.lines(r.get_ini_file()) do
-		if line == '[Recent]' then found = true 
-		elseif -- next section
-		found and line:match('%[.-%]') and line ~= '[Recent]' then break end 
+		if line == '[Recent]' then found = true
+		elseif found and line:match('%[.-%]') and line ~= '[Recent]' then -- next section
+		break end
 		if found and line ~= '[Recent]' then -- collect paths excluding the section name
-		t[#t+1] = line:gsub('recent%d+=','') end -- or line:match('=(.+)') // strip away the key		
+		local projpath = line:gsub('recent%d+=','') -- or line:match('=(.+)') // strip away the key
+			if VALID_PROJECTS_ONLY and r.file_exists(projpath) then
+			t[#t+1] = projpath
+			elseif not VALID_PROJECTS_ONLY then
+			t[#t+1] = projpath
+			end
+		end
 	end
-	
+
+
 -- in reaper.ini recent projects are listed in descending order, thus table should be reordered
 
+PROJECT_NAMES_ONLY = validate_sett(PROJECT_NAMES_ONLY)
+
 local _, projfn = r.EnumProjects(-1) -- OR r.GetProjectPath('')..r.GetProjectPath(''):match('[\\/]')..r.GetProjectName(0,'')
-local PROJECT_NAMES_ONLY = type(PROJECT_NAMES_ONLY) == 'string' and #PROJECT_NAMES_ONLY:gsub(' ','') > 0 or type(PROJECT_NAMES_ONLY) == 'number' -- can be either a non-empty string or a number
 
 local recent_proj_t = {}
 local menu_t = {}
@@ -73,6 +99,8 @@ local menu_t = {}
 local menu_t = #menu_t == 0 and recent_proj_t or menu_t -- if PROJECT_NAMES_ONLY is not ON, display full paths in the menu
 
 
+Msg(#recent_proj_t)
+
 gfx.init('', 1, 1)
 
 gfx.x = gfx.mouse_x
@@ -82,15 +110,14 @@ local output = gfx.showmenu(table.concat(menu_t, '|'))
 
 local projfn = output > 0 and recent_proj_t[output]:match('!?(.+)') -- remove ! signifying checkmark of an open project in case such project is selected for loading
 
-	if projfn and r.file_exists(projfn) then 
+	if projfn and r.file_exists(projfn) then
 	r.Main_OnCommand(40859,0) -- New project tab
 	r.Main_openProject(projfn)
 	elseif projfn then r.MB('The file is not available at the stored path\n\n'..recent_proj_t[output],'ERROR',0)
 	return r.defer(function() do return end end) end -- prevent generic undo point in case the above error message is displayed
-	
+
 gfx.quit()
-	
-	
-	
-	
-	
+
+
+
+
