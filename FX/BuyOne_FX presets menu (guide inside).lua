2e3e461fe14868f6589813f68816197300f08a92
Buy-One <61803004+Buy-One@users.noreply.github.com>
2022-07-05 11:36:06 +0300

    Update BuyOne_FX presets menu (guide inside).lua

--- FX/BuyOne_FX presets menu (guide inside).lua
+++ FX/BuyOne_FX presets menu (guide inside).lua
@@ -4,9 +4,9 @@ Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058
 Version: 1.3
 Changelog:  # Fixed losing item focus and unnecessary horizontal scroll for REAPER builds prior to 6.37
-			# Added MCP support for REAPER builds 6.37 onward
-			# Minor code optimizations
-			# Updated Guide
+	    # Added MCP support for REAPER builds 6.37 onward
+	    # Minor code optimizations
+	    # Updated Guide
 Provides: [main] .
 Licence: WTFPL
 REAPER: at least v5.962
