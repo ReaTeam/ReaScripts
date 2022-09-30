284d297edfba46f9e272929ccffbd6f125d5aacb
Buy-One <61803004+Buy-One@users.noreply.github.com>
2022-09-30 18:13:18 +0300

    Update BuyOne_Cycle through focused FX presets forward (guide inside).lua

--- FX/BuyOne_FX presets navigation/BuyOne_Cycle through focused FX presets forward (guide inside).lua
+++ FX/BuyOne_FX presets navigation/BuyOne_Cycle through focused FX presets forward (guide inside).lua
@@ -57,21 +57,41 @@ and won't allow cycling through all presets in the list
 -----------------------------------------------------------------------------
 ------------------------------ USER SETTINGS --------------------------------
 -----------------------------------------------------------------------------
--- To enable the setting place any alphanumeric character between the quotation
--- marks next to it.
+-- To enable the settings SWITCH_BY_OBJ_SEL and/or INDEX_BASED_PRESET_SWITCH
+-- place any alphanumeric character between the quotation marks next to it.
 -- Conversely, to disable it remove the character.
 -- Try to not leave empty spaces.
+
+-- See explanation in the GUIDE above
 SWITCH_BY_OBJ_SEL = ""
+
+--------------------------------------------------------
+-- Only relevant if the setting CUSTOM_PRESET_LIST 
+-- is enabled below;
+-- if the current setting is enabled, preset names 
+-- are immaterial, only their numbers matter,
+-- this allows changing names of the presets from the custom list
+-- and using the custom preset list to cycle through 
+-- presets of other plugins provided their preset lists 
+-- are long enough to include at least some of the preset 
+-- numbers featured in the custom list,
+-- BUT creating new presets for the original plugin may throw 
+-- off the configured preset sequence if the plugin preset list is re-ordered;
+-- conversely, if not enabled the custom preset list will 
+-- be pretty much tied to a single plugin whose preset names 
+-- will match it and creating new presets won't affect the functionality
+INDEX_BASED_PRESET_SWITCH = ""
 
 -- Replace the text between the double square brackets
 -- with the list of presets obtained using
--- 'Extract focused FX preset count, list, active preset number and name' script
+-- 'BuyOne_Extract focused FX preset count, list, active preset number and name.lua' script
 -- and select presets for cycling by prefixing their number in the list with +, e.g:
 -- +34 MY_PRESET_1
 -- 35 MY_PRESET_2
 -- +36 MY_PRESET_3
 -- adding the entire extracted list isn't necessary
 -- as long as the entries which are added are marked with +;
+-- the order doesn't matter as long as the numbers are there;
 -- the script can be used with any plugin whose preset NUMBERS
 -- match those in the custom list regardless of their names,
 -- if you want it to work exclusively with the plugin the list
@@ -91,8 +111,9 @@ R E P L A C E  T H I S  T E X T
 -----------------------------------------------------------------------------
 
 
-function Msg(param)
-reaper.ShowConsoleMsg(tostring(param)..'\n')
+function Msg(param, cap) -- caption second or none
+local cap = cap and type(cap) == 'string' and #cap > 0 and cap..' = ' or ''
+reaper.ShowConsoleMsg(cap..tostring(param)..'\n')
 end
 
 local r = reaper
@@ -115,53 +136,72 @@ local function GetMonFXProps() -- get mon fx accounting for floating window, rea
 end
 
 
-function Construct_Custom_Preset_Array(CUSTOM_PRESET_LIST)
--- since this array is index based, the custom list can be used with presets
+function Construct_Custom_Preset_Array(CUSTOM_PRESET_LIST, INDEX_BASED_PRESET_SWITCH)
+-- if the array is index based, the custom list can be used with presets
 -- of any plugin as long as their indices match those in the array;
 -- in order to limit the custom list to specific plugin
 -- its name can be included as the 2nd field, e.g.
 -- t[#t+1] = {idx-1, line:match('%d (.+)')}
 -- to be then evaluated against the currently or next selected preset inside Switch_FX_Preset() function
+-- eventually was emplemented differently
 local t = {}
 	for line in CUSTOM_PRESET_LIST:gmatch('[^\n]+') do
-		if 	line:sub(1,1) == '+' then
-		local idx = tonumber(line:match('^%+%s*(%d+)'))
-			if idx then t[#t+1] = idx-1 end -- -1 since the listed indices count is 1-based
+		if line and line:match('^%s*%+') then
+		local value = INDEX_BASED_PRESET_SWITCH and tonumber(line:match('^%s*%+%s*(%d+)'))
+		or not INDEX_BASED_PRESET_SWITCH and line:match('^%s*%+%s*(%d+ .+)') -- preset name
+			if value then t[#t+1] = value end
 		end
 	end
 return t
 end
 
 
-function Switch_FX_Preset(obj, fx_idx, cust_pres_list_t, forward)
+function Switch_FX_Preset(obj, fx_idx, cust_pres_list_t, INDEX_BASED_PRESET_SWITCH, forward)
 
 local tr, take = r.ValidatePtr(obj, 'MediaTrack*'), r.ValidatePtr(obj, 'MediaItem_Take*')
-local get_pres_idx, set_preset, navigate_presets = table.unpack(tr and {r.TrackFX_GetPresetIndex, r.TrackFX_SetPresetByIndex, r.TrackFX_NavigatePresets} or take and {r.TakeFX_GetPresetIndex, r.TakeFX_SetPresetByIndex, r.TakeFX_NavigatePresets})
+local get_pres_idx, get_pres_name, set_preset, navigate_presets = table.unpack(tr and {r.TrackFX_GetPresetIndex, r.TrackFX_GetPreset, INDEX_BASED_PRESET_SWITCH and r.TrackFX_SetPresetByIndex or r.TrackFX_SetPreset, r.TrackFX_NavigatePresets} or take and {r.TakeFX_GetPresetIndex, r.TakeFX_GetPreset, INDEX_BASED_PRESET_SWITCH and r.TakeFX_SetPresetByIndex or r.TakeFX_SetPreset, r.TakeFX_NavigatePresets})
 local cur_pres_idx, pres_cnt = get_pres_idx(obj, fx_idx) -- only needed if set_preset() function is used below
+local retval, cur_pres_name = get_pres_name(obj, fx_idx, '')
 
-	if (not cust_pres_list_t or cust_pres_list_t and #cust_pres_list_t == 0) and cur_pres_idx > -1 and pres_cnt > 0	then
+	local function select_value(value, INDEX_BASED_PRESET_SWITCH)
+	return INDEX_BASED_PRESET_SWITCH and value-1 or value:match('^%d+ (.+)') -- -1 since the listed indices count is 1-based
+	end
+	
+	if (not cust_pres_list_t or cust_pres_list_t and #cust_pres_list_t == 0) and pres_cnt > 0 then
 		if cur_pres_idx == pres_cnt then -- 'No preset' is selected, in which case these values are equal (the index is out of the 0-based range), start from the beginning or the end of the list, navigate_presets() for some reason starts from the 2nd preset
 		set_preset(obj, fx_idx, forward and 0 or pres_cnt-1)
 		else -- navigate as normal
 		navigate_presets(obj, fx_idx, forward and 1 or -1)
 		end
 	elseif cust_pres_list_t and #cust_pres_list_t > 1 then
-		for k, pres_idx in ipairs(cust_pres_list_t) do -- search for the current preset idx in the table the select one from the next table slot
-			if pres_idx == cur_pres_idx then
-			local next_pres_idx = forward and cust_pres_list_t[k+1] or not forward and cust_pres_list_t[k-1]
-			local next_pres_idx = forward and not next_pres_idx and cust_pres_list_t[1] or not forward and not next_pres_idx and cust_pres_list_t[#cust_pres_list_t] or next_pres_idx -- next_pres_idx being nil means that the value is out of the table's range hence the count needs to be wrapped around
-			set_preset(obj, fx_idx, next_pres_idx) -- -1 since the indices are saved from 1-based count
+		for k, pres_idx_name in ipairs(cust_pres_list_t) do -- search for the current preset idx in the table the select one from the next table slot
+			if INDEX_BASED_PRESET_SWITCH and pres_idx_name-1 == cur_pres_idx -- -1 since the listed indices count is 1-based
+			or not INDEX_BASED_PRESET_SWITCH and cur_pres_name == pres_idx_name:match('^%d+ (.+)') then
+			local next_pres_idx_name = forward and cust_pres_list_t[k+1] or not forward and cust_pres_list_t[k-1]
+			local next_pres_idx_name = forward and not next_pres_idx_name and cust_pres_list_t[1] or not forward and not next_pres_idx_name and cust_pres_list_t[#cust_pres_list_t] or next_pres_idx_name -- next_pres_idx being nil means that the value is out of the table's range hence the count needs to be wrapped around
+			set_preset(obj, fx_idx, select_value(next_pres_idx_name, INDEX_BASED_PRESET_SWITCH))
 			break
-			elseif cur_pres_idx == pres_cnt then -- if the plugin started out with 'No preset' selected in which case these values are equal (the index is out of the 0-based range) and custom list routines won't work
-			local new_pres_idx = forward and cust_pres_list_t[1] or not forward and cust_pres_list_t[#cust_pres_list_t] -- start from the 1st if forward and from the last if backwards
-			set_preset(obj, fx_idx, new_pres_idx)
+			elseif cur_pres_idx == pres_cnt then -- OR cur_pres_name == '' // if the plugin started out with 'No preset' selected in which case these values are equal (the index is out of the 0-based range), custom list routines won't work
+			local new_pres_idx_name = forward and cust_pres_list_t[1] or not forward and cust_pres_list_t[#cust_pres_list_t] -- start from the 1st if forward and from the last if backwards
+			set_preset(obj, fx_idx, select_value(new_pres_idx_name, INDEX_BASED_PRESET_SWITCH))
 			break
-			else -- if a preset not from the custom list is currently active, select first preset from the list which is greater or smaller than the index of the current preset depending on the cycling direction
-				for _, pres_idx in ipairs(cust_pres_list_t) do
-					if forward and pres_idx > cur_pres_idx or not forward and pres_idx < cur_pres_idx then
-					set_preset(obj, fx_idx, pres_idx)
+			else -- if on the first main loop cycle custom list preset is diff from the active custom list preset or a preset not from the custom list is currently active, select first preset from the list which is greater or smaller than the index of the current preset depending on the cycling direction
+				local function return_idx(value, INDEX_BASED_PRESET_SWITCH)
+				return INDEX_BASED_PRESET_SWITCH and value or value:match('%d+')
+				end
+			local exit
+			local start, fin, dir = table.unpack(forward and {1, #cust_pres_list_t, 1} or {#cust_pres_list_t, 1, -1})
+				for idx = start, fin, dir do
+				local pres_idx_name = cust_pres_list_t[idx]
+				local pres_idx = tonumber(return_idx(pres_idx_name, INDEX_BASED_PRESET_SWITCH))-1 -- either name based or index based list // -1 since the listed indices count is 1-based
+				local pres_idx_name = forward and (cur_pres_idx > tonumber(return_idx(cust_pres_list_t[#cust_pres_list_t], INDEX_BASED_PRESET_SWITCH))-1 and cust_pres_list_t[1] or pres_idx > cur_pres_idx and pres_idx_name)
+				or not forward and (cur_pres_idx < tonumber(return_idx(cust_pres_list_t[1], INDEX_BASED_PRESET_SWITCH))-1 and cust_pres_list_t[#cust_pres_list_t] or pres_idx < cur_pres_idx and pres_idx_name) -- accounting for when active preset index is greater or smaller than the index of the first and the last presets in the custom list, which covers the very 1st and the very last presets // -1 since the listed indices count is 1-based
+					if pres_idx_name then
+					set_preset(obj, fx_idx, select_value(pres_idx_name, INDEX_BASED_PRESET_SWITCH))
+					exit = 1
 					break end
 				end
+				if exit then break end -- exit the main loop
 			end
 		end
 	end
@@ -170,6 +210,7 @@ end
 
 
 local SWITCH_BY_OBJ_SEL = SWITCH_BY_OBJ_SEL:gsub('[%s]','') ~= ''
+local INDEX_BASED_PRESET_SWITCH = INDEX_BASED_PRESET_SWITCH:gsub('[%s]','') ~= ''
 
 local retval, track_num, item_num, fx_num = r.GetFocusedFX()
 local src_mon_fx_idx = GetMonFXProps() -- get Monitor FX
@@ -217,15 +258,15 @@ local ret, pres_cnt = table.unpack(t)
 
 	if pres_cnt > 0 then
 
-	local cust_pres_list_t = Construct_Custom_Preset_Array(CUSTOM_PRESET_LIST)
+	local cust_pres_list_t = Construct_Custom_Preset_Array(CUSTOM_PRESET_LIST, INDEX_BASED_PRESET_SWITCH)
 
 	r.Undo_BeginBlock()
 		if retval == 1 or mon_fx then
-		Switch_FX_Preset(tr, fx_num, cust_pres_list_t, true) -- forward true
+		Switch_FX_Preset(tr, fx_num, cust_pres_list_t, INDEX_BASED_PRESET_SWITCH, true) -- forward true
 		 _, fx_name = r.TrackFX_GetFXName(tr, fx_num, '') -- for undo caption
 		_, pres_name = r.TrackFX_GetPreset(tr, fx_num, '') -- for undo caption
 		elseif retval == 2 then
-		Switch_FX_Preset(take, fx_num, cust_pres_list_t, true) -- forward true
+		Switch_FX_Preset(take, fx_num, cust_pres_list_t, INDEX_BASED_PRESET_SWITCH, true) -- forward true
 		_, take_name = r.GetSetMediaItemTakeInfo_String(take, 'P_NAME', '', false)
 		_, fx_name = r.TakeFX_GetFXName(take, fx_num, '') -- for undo caption
 		_, pres_name = r.TakeFX_GetPreset(take, fx_num, '') -- for undo caption
