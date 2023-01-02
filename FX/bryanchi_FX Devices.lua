9e587425223ff193488e7dceae5ad72d2dd926d0
BryanChi <93679021+BryanChi@users.noreply.github.com>
2023-01-02 23:59:38 +0900

    Release FX Devices v1.0beta
    --------------------------==  declare Initial Variables & Functions  ------------------------
        VersionNumber = 'Dec 23 2022 - bug fix 3 '
        FX_Add_Del

---
+++ FX/bryanchi_FX Devices.lua
@@ -0,0 +1,24 @@
+-- @description FX Devices
+-- @author Bryan Chi
+-- @version 1.0beta
+-- @changelog
+--   --------------------------==  declare Initial Variables & Functions  ------------------------
+--       VersionNumber = 'Dec 23 2022 - bug fix 3 '
+--       FX_Add_Del
+-- @provides
+--   . > BryanChi_FX_Devices.lua
+--   bryanchi_FX Devices/FXD MainScript.lua
+--   [data] bryanchi_FX Devices/IconFont1.ttf
+--   [effect] bryanchi_FX Devices/FXD Macros.jsfx
+--   [effect] bryanchi_FX Devices/FXD Split to 32 Channels.jsfx
+--   [effect] bryanchi_FX Devices/FXD Gain Reduction Scope.jsfx
+--   bryanchi_FX Devices/FXD ReSpectrum.jsfx
+--   bryanchi_FX Devices/FXD Split To 4 Channels.jsfx
+--   [effect] bryanchi_FX Devices/firhalfband.jsfx-inc
+--   bryanchi_FX Devices/spectrum.jsfx-inc
+--   bryanchi_FX Devices/svf_filter.jsfx-inc
+-- @about Check out the forum release thread for info
+
+
+
+
