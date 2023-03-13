5f6be9659c28009d832846756635aaae5331e60e
tompad2 <relativt63@gmail.com>
2023-03-13 17:18:53 +0100

    Update tompad_FX_chain_AB.lua

--- Various/tompad_FX_chain_AB.lua
+++ Various/tompad_FX_chain_AB.lua
@@ -1,3 +1,28 @@
+-- @description tompad_FXchain_A-B
+-- @author Thomas Dahl
+-- @version 1.0
+-- @changelog Release 13 mars 2023
+-- @about
+--   # tompad_FXchain_A-B
+--   Reascript for A/B-ing FX chains in Reaper DAW 
+--
+--   Use tompad_FXchain_A-B to compare different fx chains you create on a track.
+--   You can also use it to copy a fx chain to another track.
+--
+--   How to use:
+--   1. Create a fx chain on a track.
+--   2. Run tompad_FXchain_A-B.
+--   3. Make sure the track with the fx chain is selected and press Inject A-button.
+--   4. Make a new fx chain on track OR make some adjustments to the fx chain and press Inject B-button.
+--   5. Its now possible to compare the two fx chains by pressing the two big buttons A and B.
+--
+--   Requirements (from ReaPack):
+--
+--   Reaper Tool Kit (rtk) from https://reapertoolkit.dev/index.xml 
+--
+--   Ultraschall API package from https://github.com/Ultraschall/ultraschall-lua-api-for-reaper/raw/master/ultraschall_api_index.xml
+
+
 -- Setup package path locations to find rtk via ReaPack
 local entrypath = ({reaper.get_action_context()})[2]:match('^.+[\\//]')
 package.path = string.format('%s/Scripts/rtk/1/?.lua;%s?.lua;', reaper.GetResourcePath(), entrypath)
