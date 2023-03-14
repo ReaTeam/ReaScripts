9aa9b06e80465bc82293e00819215e3c36ad9d4f
Raymond Radet <X-Raym@users.noreply.github.com>
2023-03-14 21:51:10 +0100

    Update and rename gus-lan_Select foldertracks based on track indentation (trackdepth) (settings inside).lua to gus-lan_Select folder tracks of depth 0.lua

--- Various/guslan_Select foldertracks based on track indentation/gus-lan_Select folder tracks of depth 0.lua
+++ Various/guslan_Select foldertracks based on track indentation/gus-lan_Select folder tracks of depth 0.lua
@@ -1,4 +1,7 @@
 -- @noindex
+-- @description Select folder tracks of depth 0
+-- @author gus-lan
+-- @version 1.0
 
 --────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 --        ::::::::  :::    :::  ::::::::                :::            :::     ::::    :::          ::::::::   ::::::::  :::::::::  ::::::::::: ::::::::: ::::::::::: ::::::::  --
@@ -9,34 +12,6 @@
 --  #+#    #+# #+#    #+# #+#    #+#               #+#        #+#     #+# #+#   #+#+#         #+#    #+# #+#    #+# #+#    #+#     #+#     #+#           #+#    #+#    #+#      --
 --  ########   ########   ########                ########## ###     ### ###    ####          ########   ########  ###    ### ########### ###           ###     ########        --
 --────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-
---[[
-  * ReaScript Name: gus-lan_Select foldertracks at trackdepth(n) (settings inside).lua
-  
-  * About:
-      A script that selects tracks that are folders and at a specific track depth throughout the current project.
-      Useful for large templates that utlize folder systems for grouping.
-      
-  * Instructions:
-      Set which track depth the script should use in the user input section. 
-      Execute script.
-
-  * Author: Gustav Landerholm
-  * Repository: GitHub > gus-lan > reaper_scripts
-  * Repository URL:https://github.com/gus-lan/reaper_scripts.git 
-  * Licence: GPL v3
-  * Version: 1.0
-  * Version Date: 2022-07-11
-  * REAPER: v6.63
-  * Extensions:
---]]
-
---[[
-* Changelog:
-* v1.0 (2022-07-11)
-+ Initial Release
---]]
-
 
 --──── USER INPUT ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────  
 
