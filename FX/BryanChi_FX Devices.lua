040565b8e1ad5f20ea88eb9975a94ab0753f49cb
BryanChi <93679021+BryanChi@users.noreply.github.com>
2023-09-10 09:08:03 +0800

    Update BryanChi_FX Devices.lua
    Update copy file function

--- FX/BryanChi_FX Devices.lua
+++ FX/BryanChi_FX Devices.lua
@@ -1576,10 +1576,26 @@ function GeneralFunctions()
     end
 end
 
+
 function CopyFile(old_path, new_path)
-    local CopyCmd = r.GetOS():find("Win") and 'copy "' or 'cp "'
-    local ok = os.execute(CopyCmd .. old_path .. '" "' .. new_path .. '"')
-    return ok
+    local old_file = io.open(old_path, "rb")
+    local new_file = io.open(new_path, "wb")
+    local old_file_sz, new_file_sz = 0, 0
+    if not old_file or not new_file then
+        return false
+    end
+    while true do
+        local block = old_file:read(2 ^ 13)
+        if not block then
+            old_file_sz = old_file:seek("end")
+            break
+        end
+        new_file:write(block)
+    end
+    old_file:close()
+    new_file_sz = new_file:seek("end")
+    new_file:close()
+    return new_file_sz == old_file_sz
 end
 
 function LayoutEditorFunctions()
