8703345e352d285fec0b41b7282e79a65da530f0
TiagoLr <prog4mr@gmail.com>
2023-09-12 22:28:31 +0100

    Release Snapshooter v1.0

---
+++ Envelopes/tilr_Snapshooter/tilr_Snapshooter apply snap 4.lua
@@ -0,0 +1,12 @@
+-- @noindex
+
+skip_init = true
+local sep = package.config:sub(1, 1)
+local script_folder = debug.getinfo(1).source:match("@?(.*[\\|/]).*[\\|/]")
+local snapshooter = dofile(script_folder .. sep .. 'tilr_Snapshooter.lua')
+
+local snap = 4
+local hassnap = reaper.GetProjExtState(0, 'snapshooter', 'snap'..snap) ~= 0
+if hassnap then
+  snapshooter.applysnap(snap, false)
+end
