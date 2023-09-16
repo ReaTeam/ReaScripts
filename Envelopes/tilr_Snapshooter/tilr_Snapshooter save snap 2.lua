c53f044bda69aa1820640789b4fb894a9151a22b
TiagoLr <prog4mr@gmail.com>
2023-09-16 08:27:04 +0100

    Release Snapshooter v1.0 (#1248)

---
+++ Envelopes/tilr_Snapshooter/tilr_Snapshooter save snap 2.lua
@@ -0,0 +1,8 @@
+-- @noindex
+
+skip_init = true
+local sep = package.config:sub(1, 1)
+local script_folder = debug.getinfo(1).source:match("@?(.*[\\|/]).*[\\|/]")
+local snapshooter = dofile(script_folder .. sep .. 'tilr_Snapshooter.lua')
+
+snapshooter.savesnap(2)
