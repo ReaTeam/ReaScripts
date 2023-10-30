f744aad1b0500c926a65acdcf8518168a15dc006
BryanChi <93679021+BryanChi@users.noreply.github.com>
2023-10-30 18:19:12 +0900

    Update bryanchi_Background Save.lua
    update

--- Project Properties/bryanchi_Background Save.lua
+++ Project Properties/bryanchi_Background Save.lua
@@ -3,27 +3,42 @@
 -- @version 0.9
 -- @changelog first release
 -- @about Uses Ultraschall background copy so Reaper doesn't freeze when saving a big project. Runs in a defer loop to always save the project's latest changes.
-
 r=reaper
-dofile(r.GetResourcePath() .. "/UserPlugins/ultraschall_api.lua")
-u = ultraschall
+ultraschall_path = reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua"
+if reaper.file_exists( ultraschall_path ) then
+  dofile( ultraschall_path )
+end
+
+if not ultraschall or not ultraschall.GetApiVersion then -- If ultraschall loading failed of if it doesn't have the functions you want to use
+  reaper.MB("Please install Ultraschall API, available via Reapack. Check online doc of the script for more infos.\nhttps://github.com/Ultraschall/ultraschall-lua-api-for-reaper", "Error", 0)
+  return
+end
 
 path = r.GetProjectPath()
 ProjNm = r.GetProjectName( 0)
 
+TimeGap = 30
+
 filename = path..ProjNm
-time = os.date("*t")
+time1 = reaper.time_precise()
+
 
 function msg (s)
   r.ShowConsoleMsg(s)
+end
+
+function SetButtonState( set )
+  local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  reaper.SetToggleCommandState( sec, cmd, set or 0 )
+  reaper.RefreshToolbar2( sec, cmd )
 end
 
 
 
 
 function loop()
- i = (i or 0 )+1
-  if i > 1000 then 
+ time2 = r.time_precise() 
+  if time2 - time1 > TimeGap then 
     time = os.date("*t")
     --if time.min % 5 then -- every 5 mins 
 
@@ -34,20 +49,21 @@ function loop()
         
         filename = path..'/'..ProjNm
         
-        
-        
-        current_copyqueue_position = ultraschall.CopyFile_AddFileToQueue(filename, path..'/'..ProjNm..'-bak' , false )
+        current_copyqueue_position = ultraschall.CopyFile_AddFileToQueue(filename, path..'/'..ProjNm..'-bak' , true )
         instance_number = ultraschall.CopyFile_StartCopying()
         SLEM() 
       end
+      time1= r.time_precise() 
     --end
   end
 
   reaper.defer(loop)
 end
 
+SetButtonState( 1 )
 reaper.defer(loop) 
 
+reaper.atexit( SetButtonState )
 
 
 
