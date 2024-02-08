33fb6877e12599a0403e962690235c6857943038
AZpercussion <70285853+AZpercussion@users.noreply.github.com>
2024-02-08 14:49:53 +0300

    Release Insert project marker and edit it under mouse or play cursor v1.0

---
+++ Markers/az_Insert project marker and edit it under mouse or play cursor.lua
@@ -0,0 +1,62 @@
+-- @description Insert project marker and edit it under mouse or play cursor
+-- @author AZ
+-- @version 1.0
+-- @provides [main=main,midi_editor] .
+-- @link Forum thread https://forum.cockos.com/showthread.php?t=288069
+-- @donation Donate via PayPal https://www.paypal.me/AZsound
+-- @about
+--   #Insert project marker and edit it under mouse or play cursor
+--
+--   SWS is required
+--
+--   This script brings usability of project markers closer to the take markers.
+--
+--   If mouse placed in arrange -  use mouse position for marker, else use edit or play cursor position.
+
+function ins_marker_mouse()
+reaper.Main_OnCommandEx( 40513, 0, 0 )  --View: Move edit cursor to mouse cursor
+reaper.Main_OnCommandEx( 40171, 0, 0 )  --Insert and/or edit marker at current position
+reaper.SetEditCurPos2( 0, edit_cur_pos, false, false )
+end
+
+----------
+----------
+
+function ins_marker_play()
+reaper.Main_OnCommandEx( 40434, 0, 0 )  --View: Move edit cursor to play cursor
+reaper.Main_OnCommandEx( 40171, 0, 0 )  --Insert and/or edit marker at current position
+reaper.SetEditCurPos2( 0, edit_cur_pos, false, false )
+end
+
+-------------------------------
+-------------------------------
+
+function ins_proj_marker()
+edit_cur_pos = reaper.GetCursorPositionEx( 0 )
+window, segment, details = reaper.BR_GetMouseCursorContext()
+
+if window == "arrange" or window == "midi_editor" then
+  pos =  reaper.BR_GetMouseCursorContext_Position()
+  
+  if window == "midi_editor" then
+    if segment == "unknown"or segment == "notes"or segment == "cc_lane" then
+    ins_marker_mouse()
+    else
+    ins_marker_play()
+    end
+  else
+    ins_marker_mouse()
+  end
+else
+  ins_marker_play()
+end
+
+end
+
+
+
+reaper.Undo_BeginBlock2( 0 )
+reaper.PreventUIRefresh( 1 )
+ins_proj_marker()
+reaper.PreventUIRefresh( -1 )
+reaper.Undo_EndBlock2( 0, "Insert project marker", -1 )
