3b20c64196dceb813ef6ce5742ba578606b902ea
kmarilleau <kevin.marilleau@gmail.com>
2024-04-20 19:35:04 +0200

    Release MIDI Channel Toggle JSFX v0.0.1

---
+++ FX/erin_MIDI_Channel_Toggle.lua
@@ -0,0 +1,10 @@
+-- @description MIDI Channel Toggle JSFX
+-- @author erin
+-- @version 0.0.1
+-- @metapackage
+-- @provides [effect] erin_MIDI_Channel_Toggle/erin_MIDI_Channel_Toggle.jsfx
+-- @about
+--   # MIDI Channel Toggle JSFX
+--
+--   FX to toggle any MIDI Channel
+
