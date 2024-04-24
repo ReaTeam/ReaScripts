a390ebfa3f7fa728550b9b6b34adadb7d67454fe
amagalma <amagalma@gmail.com>
2024-04-24 19:32:50 +0300

    Update amagalma_Split item at mouse cursor position ( Define crossfade and selection settings ).lua

--- Items Editing/amagalma_Split item at mouse cursor position ( use defined crossfade and selection settings )/amagalma_Split item at mouse cursor position ( Define crossfade and selection settings ).lua
+++ Items Editing/amagalma_Split item at mouse cursor position ( use defined crossfade and selection settings )/amagalma_Split item at mouse cursor position ( Define crossfade and selection settings ).lua
@@ -14,6 +14,7 @@ end
 local chosen_selection = tonumber(reaper.GetExtState("amagalma_Split at mouse cursor position", "selection")) or 3
 local xfadeposition = tonumber(reaper.GetExtState("amagalma_Split at mouse cursor position", "xfadeposition")) or 1
 local snaptogrid = tonumber(reaper.GetExtState("amagalma_Split at mouse cursor position", "snaptogrid")) or 0
+local ignoregrouping = tonumber(reaper.GetExtState("amagalma_Split at mouse cursor position", "ignoregrouping")) or 0
 
 local t = {
       {"#Selection after split:"},
@@ -25,8 +26,9 @@ local t = {
       {"on the left side", 1},
       {"centered at mouse cursor position", 0.5},
       {"on the right side", 0},
-      {"|#Respect snap setting:"},
-      {"snap to grid respected", snaptogrid}
+      {"|#Respect snap and grouping settings:"},
+      {"snap to grid is respected", snaptogrid},
+      {"item grouping is respected", ignoregrouping},
 }
 
 
@@ -39,6 +41,8 @@ for i = 1, #t do
     if xfadeposition == t[i][2] then check = "!" end
   elseif i == 11 then
     if snaptogrid == 1 then check = "!" end
+  elseif i == 12 then
+    if ignoregrouping == 0 then check = "!" end
   end
   menu = menu .. check .. t[i][1] .. "|"
 end
@@ -61,5 +65,7 @@ elseif selection > 6 and selection < 10 then
   reaper.SetExtState("amagalma_Split at mouse cursor position", "xfadeposition", t[selection][2], true)
 elseif selection == 11 then
   reaper.SetExtState("amagalma_Split at mouse cursor position", "snaptogrid", 1 - snaptogrid, true)
+elseif selection == 12 then
+  reaper.SetExtState("amagalma_Split at mouse cursor position", "ignoregrouping", 1 - ignoregrouping, true)
 end
 reaper.defer(function() end)
