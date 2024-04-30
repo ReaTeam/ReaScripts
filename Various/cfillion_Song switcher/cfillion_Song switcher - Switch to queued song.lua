412e24f9885ae6e289b36cc98a64de447b882da0
Christian Fillion <cfillion@users.noreply.github.com>
2024-04-30 10:37:32 -0400

    Release Song switcher (for live use) v1.6
    add action for queuing songs using a MIDI controller [p=2777472]
    add actions for queuing the next or previous song
    rewrite the user interface using ReaImGui

--- Various/cfillion_Song switcher/cfillion_Song switcher - Switch to queued song.lua
+++
@@ -1,7 +0,0 @@
--- @noindex
-
--- @noindex
-
--- This script is part of cfillion_Song switcher.lua
-reaper.SetExtState('cfillion_song_switcher', 'activate_queued', 'true', false)
-reaper.defer(function() end) -- no undo point
