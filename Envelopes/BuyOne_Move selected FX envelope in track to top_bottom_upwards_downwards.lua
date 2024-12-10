cab13cfbfcf3d748f0e886debc1b986be38dda59
Christian Fillion <cfillion@users.noreply.github.com>
2024-12-10 11:11:29 -0500

    release  Move selected FX envelope in track to top/bottom/upwards/downwards (6 scripts) v1.2-1

--- Envelopes/BuyOne_Move selected FX envelope in track to top_bottom_upwards_downwards.lua
+++ Envelopes/BuyOne_Move selected FX envelope in track to top_bottom_upwards_downwards.lua
@@ -1,8 +1,8 @@
 --[[
 ReaScript Name: Move selected FX envelope in track to top/bottom/upwards/downwards (6 scripts)
 Author: BuyOne
-Version: 1.2
-Changelog: #Fixed script crach when applying to the only envelope of a particular FX
+Version: 1.2-1
+Changelog: Fix packaging of the 6 actions
 Author URL: https://forum.cockos.com/member.php?u=134058
 Licence: WTFPL
 REAPER: at least v5.962
@@ -26,7 +26,6 @@ About:  Moves selected FX envelope of a track to the top/bottom lane,
 	as the selected envelope.  
 
 	Screenshot: https://raw.githubusercontent.com/Buy-One/screenshots/main/Move%20selected%20FX%20envelope%20in%20track%20to%20top_bottom_upwards_downwards.gif
-
 Metapackage: true
 Provides: 	[main] . > BuyOne_Move selected FX envelope/BuyOne_Move selected FX envelope in track to top lane.lua
 		[main] . > BuyOne_Move selected FX envelope/BuyOne_Move selected FX envelope in track to bottom lane.lua
