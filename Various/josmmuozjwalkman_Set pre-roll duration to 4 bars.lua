31955a796d0eb18a5eb7281a367b1ac9bc64502f
JM_Walkman <josemanuel.tmn@gmail.com>
2025-04-11 14:04:07 -0500

    Release Set pre-roll duration to 4 bars v1.0

---
+++ Various/josmmuozjwalkman_Set pre-roll duration to 4 bars.lua
@@ -0,0 +1,6 @@
+-- @description Set pre-roll duration to 4 bars
+-- @author José M Muñoz (J-WalkMan)
+-- @version 1.0
+-- @provides . > JWM_Set pre-roll duration to 4 bars.lua
+
+reaper.SNM_SetDoubleConfigVar("prerollmeas", 4)
