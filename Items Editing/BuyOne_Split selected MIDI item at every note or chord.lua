e6deba803761e7c5d8651639aca065ee7f548132
Buy-One <61803004+Buy-One@users.noreply.github.com>
2025-05-03 15:36:12 +0300

    Update BuyOne_Split selected MIDI item at every note or chord.lua

--- Items Editing/BuyOne_Split selected MIDI item at every note or chord.lua
+++ Items Editing/BuyOne_Split selected MIDI item at every note or chord.lua
@@ -2,17 +2,10 @@
 ReaScript name: Split selected MIDI item at every note or chord
 Author: BuyOne
 Website: https://forum.cockos.com/member.php?u=134058
-Version: 1.2
-Changelog:	#Made the script compatible with preference 
-		Preferences -> Media -> MIDI -> Allow trim of MIDI items when splitting
-		added in REAPER build 6.74
-		#Prevented unnecessary zooming if MIDI Editor is open
-		at the moment of the script execution
-		#Prevented closure of the MIDI Editor if it was open initially
-		#Included option to split even if notes at different pitches overlap
-		#Re-organized the code
-		#Updated GLUE_SLICES setting description
-		#Updated 'About' text to reflect the new functionality
+Version: 1.3
+Changelog:  	#Created error and prompt messages for cases in which the script 
+		cannot apply or which require user consent
+		#Removed unnecessary actions
 Licence: WTFPL
 REAPER: at least v5.962
 Provides: [main=main,midi_editor] .
@@ -54,6 +47,62 @@ reaper.ShowConsoleMsg(cap..tostring(param)..'\n')
 end
 
 local r = reaper
+
+
+function no_undo()
+do return end
+end
+
+
+function Error_Tooltip(text, caps, spaced, x2, y2, want_color, want_blink)
+-- the tooltip sticks under the mouse within Arrange 
+-- but quickly disappears over the TCP, to make it stick 
+-- just a tad longer there it must be directly under the mouse
+-- not directly under the mouse the tooltip sticks if mouse is over Arrange
+-- but soon disappears if mouse is in the TCP area but not over the TCP
+-- and immediately disappears if the mouse is over the TCP
+-- caps and spaced are booleans, caps doesn't apply to non-ANSI characters
+-- x2, y2 are integers to adjust tooltip position by
+-- want_color is boolean to enable temporary ruler coloring to emphasize the error
+-- want_blink is boolean to enable ruler color blinking
+local x, y = r.GetMousePosition()
+local text = caps and text:upper() or text
+local utf8 = '[\0-\127\194-\244][\128-\191]*'
+local text = spaced and text:gsub(utf8,'%0 ') or text -- supporting UTF-8 char
+local x2, y2 = x2 and math.floor(x2) or 0, y2 and math.floor(y2) or 0
+r.TrackCtl_SetToolTip(text, x+x2, y+y2, true) -- topmost true
+-- r.TrackCtl_SetToolTip(text:upper(), x, y, true) -- topmost true
+-- r.TrackCtl_SetToolTip(text:upper():gsub('.','%0 '), x, y, true) -- spaced out // topmost true
+	if want_color then	
+	local color_init = r.GetThemeColor('col_tl_bg', 0)
+	local color = color_init ~= 255 and 255 or 65535 -- use red or yellow of red is taken
+		if want_blink then
+		    for i = 1, 100 do    
+				if i == 1 or i == 40 or i == 80 then
+				r.SetThemeColor('col_tl_bg', color, 0)
+				elseif i == 20 or i == 60 or i == 100 then
+				r.SetThemeColor('col_tl_bg', color_init, 0)
+				end
+			r.UpdateTimeline()
+			end		
+		else
+		r.SetThemeColor('col_tl_bg', color, 0) -- Timeline background
+			for i = 1, 200 do -- ensures that the warning color sticks for some time
+			-- without the function inside the loop the end (200) value must be much greater
+			r.UpdateTimeline()
+			end
+		r.SetThemeColor('col_tl_bg', color_init, 0) -- Timeline background // restore the orig color
+		r.UpdateTimeline() -- without this function the color will only be restored when user clicks within the Arrange
+		end
+	end
+--[[
+-- a time loop can be added to run until certain condition obtains, e.g.
+local time_init = r.time_precise()
+repeat
+until condition and r.time_precise()-time_init >= 0.7 or not condition
+]]
+r.UpdateTimeline() -- might be needed because tooltip can sometimes affect graphics
+end
 
 
 function ACT(comm_ID, midi) -- midi is boolean
@@ -116,10 +165,30 @@ end
 r.PreventUIRefresh(1)
 
 local item = r.GetSelectedMediaItem(0,0)
-	if not item then r.MB('No selected items.','ERROR',0) return r.defer(function() do return end end) end
+local take = item and r.GetActiveTake(item)
+local err = not item and 'no selected items' or
+(not take or not r.TakeIsMIDI(take) and r.CountTakes(item) == 1) and 'not a midi item' -- empty or audio
+or not r.TakeIsMIDI(take) and 'not a midi take'
 
-local take = r.GetActiveTake(item)
+	if err then
+	Error_Tooltip('\n\n '..err..' \n\n', 1, 1) -- caps, spaced true
+	return r.defer(no_undo)
+	end
+
 local retval, notecnt, _, _ = r.MIDI_CountEvts(take)
+local obj = r.CountTakes(item) == 1 and 'item' or 'take'
+
+	if notecnt == 0 then
+	Error_Tooltip('\n\n the '..obj..' doesn\'t contain notes \n\n', 1, 1) -- caps, spaced true
+	return r.defer(no_undo)
+	end
+		
+local s = function(num) return (' '):rep(num) end
+	if r.CountTakes(item) > 1
+	and r.MB(s(11)..'The item is multi-take.\n\n'..s(7)..'Do you still want to split it?', 'PROMPT', 4) == 7 -- user declined
+	then
+	return r.defer(no_undo)
+	end
 
 -- Find if there're any overlapping notes
 local i = 0
@@ -137,7 +206,7 @@ r.Undo_BeginBlock()
 	if i < notecnt then
 	local resp = r.MB('\t   There\'re overlapping notes.\n\n\t     Should they all be fixed?\n\n\tStart positions will be preserved.'
 	..'\n\n\t If not, some slices may end up\n\n\t      containing several notes.','PROMPT',3)
-		if resp == 2 then return r.defer(function() do return end end) -- cacelled by the user
+		if resp == 2 then return r.defer(no_undo) -- cacelled by the user
 		elseif resp == 6 then -- user assented
 		-- Correct overlapping notes preserving start positions
 		local i = 0
@@ -165,12 +234,10 @@ r.Undo_BeginBlock()
 	end
 
 
-local ME_vis = MIDIEditor_GetActiveAndVisible() -- when the script is executed while the MIDI Editor is already open, triggering the following action may result in zooming MIDI Editor to content and changing the original zoom amount, so use this var to condition the following action as well as preventing its closuse at the end of the routine
+local ME_vis = MIDIEditor_GetActiveAndVisible() -- when the script is executed while the selected take is already open in the MIDI Editor, triggering the following action may result in zooming MIDI Editor to content and changing the original zoom amount, so use this var to condition the following action as well as preventing its closuse at the end of the routine
 	if not ME_vis then
 	ACT(40153) -- Item: Open in built-in MIDI editor (set default behavior in preferences)
 	end
-ACT(40036, true) -- View: Go to start of file
-ACT(40214, true) -- Edit: Unselect all
 
 local trimmidionsplit = Extract_reaper_ini_val('trimmidionsplit')
 GLUE_SLICES = #GLUE_SLICES:gsub(' ','') > 0 and (#trimmidionsplit == 0 or trimmidionsplit == '0') -- validating against the preference 'Preferences -> Media -> MIDI -> Allow trim of MIDI items when splitting' added circa 6.74 which if enabled (and it's enabled by default) makes all splits unique by default with no need of gluing; the evaluation be true if it doesn't exist or enabled (bits 1 or 5), in which case gluing will make sense
@@ -198,7 +265,7 @@ local i = 0
 	i = i + 1
 	end
 
-	if not ME_vis then -- do not close ME if it was open initially
+	if not ME_vis then -- only close ME if it wasn't open initially
 	ACT(2, true) -- File: Close window
 	end
 
