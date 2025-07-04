82f5475c96c6bbb84cd471eac19d729abd5d56a6
Christian Fillion <cfillion@users.noreply.github.com>
2025-07-04 04:35:21 -0400

    remove the duplicate metadata header and rename to sentence case

--- FX/graysonsolis_Add JS Humanizer to top of track chain.lua
+++ FX/graysonsolis_Add JS Humanizer to top of track chain.lua
@@ -1,15 +1,9 @@
--- @description Add JS Humanizer to Top of Track Chain
+-- @description Add JS Humanizer to top of track chain
 -- @author Grayson Solis
 -- @version 1.0
 -- @about Inserts the built-in JS humanizer at slot 1 on the selected track’s FX chain.
-
---[[
-@version 1.0
-@author Grayson Solis
-@description Inserts the built-in JS humanizer at slot 1 on the selected track’s FX chain.
-@website https://graysonsolis.com
-@donations https://paypal.me/GrayTunes?country.x=US&locale.x=en_US
-]]
+-- @website https://graysonsolis.com
+-- @donations https://paypal.me/GrayTunes?country.x=US&locale.x=en_US
 
 local tr = reaper.GetSelectedTrack(0, 0)
 if not tr then return end
@@ -27,4 +21,3 @@ reaper.Undo_BeginBlock()
     reaper.TrackFX_CopyToTrack(tr, fx, tr, 0, true)
   end
 reaper.Undo_EndBlock("Add JS MIDI Velocity and Timing Humanizer to top", -1)
-
