6e77a61cbf5e951f143e85401ecb655d635532c2
JhonBrook <62082877+JhonBrook@users.noreply.github.com>
2025-07-14 15:06:56 -0300

    Release Controlador Multitracks Elite v1.0

---
+++ Various/jonhbrook_Controlador Multitracks Elite.lua
@@ -0,0 +1,13 @@
+-- @description Controlador Multitracks Elite
+-- @author Jonh.brook
+-- @version 1.0
+
+; @description Controlador Multitracks Elite
+; @version 1.0
+; @author Jonh Brook
+; @about
+    Script que carrega a interface de controle para multitracks no REAPER
+    e ajuda o usuário a adicionar um botão à barra de ferramentas.
+; @provides
+    [main] Controlador Multitracks Elite.lua
+; @image ControladorMultitracksElite.png
