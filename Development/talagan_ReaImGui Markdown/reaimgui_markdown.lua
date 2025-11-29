2a74949e0bd7a20b82b6a5b3690183343c48de1f
BenTalagan <ben_talagan@github.nospam.com>
2025-11-29 21:31:57 +0100

    Update ReaImGui Markdown v0.1.12 > v0.1.13

--- Development/talagan_ReaImGui Markdown/reaimgui_markdown.lua
+++ Development/talagan_ReaImGui Markdown/reaimgui_markdown.lua
@@ -24,8 +24,19 @@ local function deepMerge(t1, t2)
     return t1
 end
 
-local function deepCopy(t2)
-    return deepMerge({}, t2)
+local function deepCopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepCopy(orig_key)] = deepCopy(orig_value)
+        end
+        setmetatable(copy, deepCopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
 end
 
 local ReaImGuiMd = {}
