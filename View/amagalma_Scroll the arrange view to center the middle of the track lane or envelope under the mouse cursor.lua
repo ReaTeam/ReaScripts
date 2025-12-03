5b31e87b5944510344243cebd0306bfe03ebd497
amagalma <amagalma@gmail.com>
2025-12-03 12:08:28 +0200

    Release Scroll the arrange view to center the middle of the track lane or envelope under the mouse cursor v1.00 (#1685)

---
+++ View/amagalma_Scroll the arrange view to center the middle of the track lane or envelope under the mouse cursor.lua
@@ -0,0 +1,48 @@
+-- @description Scroll the arrange view to center the middle of the track lane or envelope under the mouse cursor
+-- @author amagalma
+-- @version 1.00
+-- @donation https://www.paypal.me/amagalma
+
+if not reaper.CF_SetTcpScroll then
+  return reaper.MB( "Please, install / update SWS.", "Missing CF_SetTcpScroll API", 0 )
+end
+
+if not reaper.JS_Window_FindChildByID then
+  return reaper.MB( "Please, install JS_ReaScriptAPI", "Missing API", 0 )
+end
+
+local x, y = reaper.GetMousePosition()
+local track, info = reaper.GetTrackFromPoint( x, y )
+
+if not track then return end
+
+local arrange = reaper.JS_Window_FindChildByID( reaper.GetMainHwnd(), 0x3E8 )
+local arrange_height = select( 3, reaper.JS_Window_GetClientSize( arrange ) )
+
+local function Info( str )
+  return reaper.GetMediaTrackInfo_Value( track, str )
+end
+
+local position, half = 0, 0
+
+if info == 1 then
+  reaper.BR_GetMouseCursorContext()
+  local env, isTakeEnvelope = reaper.BR_GetMouseCursorContext_Envelope()
+  if not isTakeEnvelope then
+    position = reaper.GetEnvelopeInfo_Value( env, "I_TCPY_USED" )
+    half = reaper.GetEnvelopeInfo_Value( env, "I_TCPH_USED" ) // 2
+  end
+else
+  local lane = info >> 8
+  if Info("C_LANESCOLLAPSED") == 0 then
+    local lane_height = ( Info("I_TCPH") - 18  ) / Info("I_NUMFIXEDLANES")
+    position = lane_height * lane - 1
+    half = lane_height // 2
+  else
+    half = Info("I_TCPH") // 2
+  end
+end
+
+local mid = position + half - (arrange_height // 2)
+
+reaper.CF_SetTcpScroll( track, mid )
