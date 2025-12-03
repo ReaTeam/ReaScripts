dfec30e19f00cbce332554496771ff8205bc486e
amagalma <amagalma@gmail.com>
2025-12-03 14:25:04 +0200

    Update amagalma_Smooth scroll view Settings.lua

--- View/amagalma_Smooth scroll view Settings.lua
+++ View/amagalma_Smooth scroll view Settings.lua
@@ -1,6 +1,7 @@
 -- @description Smooth scroll arrange view settings
 -- @author amagalma
--- @version 1.00
+-- @version 1.01
+-- @changelog Do not accept a maximum duration that is less or equal to 31ms (one defer cycle)
 -- @donation https://www.paypal.me/amagalma
 
 local ret = reaper.GetExtState("amagalma_Smooth Scroll", "scroll")
@@ -14,7 +15,7 @@ if ok then
   local new_scroll, new_max_duration = retvals:match("(.+),(.+)")
   local new_scroll_num = tonumber(new_scroll)
   local new_max_duration_num = tonumber(new_max_duration)
-  if new_scroll_num and new_scroll_num > 0 and new_max_duration_num and new_max_duration_num > 0 then
+  if new_scroll_num and new_scroll_num > 0 and new_max_duration_num and new_max_duration_num > 31 then
     reaper.SetExtState( "amagalma_Smooth Scroll", "scroll", new_scroll, true )
     reaper.SetExtState( "amagalma_Smooth Scroll", "max_duration", new_max_duration, true )
   else
