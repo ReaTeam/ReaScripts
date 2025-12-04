6d3defc583f2d83d9df8a5c81bc106730e45a92d
amagalma <amagalma@gmail.com>
2025-12-04 09:47:29 +0200

    Update amagalma_Smooth scroll arrange view left.lua

--- View/amagalma_Smooth scroll arrange view left.lua
+++ View/amagalma_Smooth scroll arrange view left.lua
@@ -1,21 +1,21 @@
 -- @description Smooth scroll arrange view left
 -- @author amagalma
--- @version 1.01
--- @changelog Auto-terminate and re-run script if shortcut key is held down
+-- @version 1.02
+-- @changelog No undo point creation when the view is already at the start of the project
 -- @donation https://www.paypal.me/amagalma
 
 reaper.set_action_options( 1|2 )
+
+local Set = reaper.GetSet_ArrangeView2
+local a0, b0 = Set( 0, 0, 0, 0, 0, 0 )
+if a0 == 0 then return reaper.defer(function() end) end
 
 local ret = tonumber(reaper.GetExtState("amagalma_Smooth Scroll", "scroll"))
 local scroll = ret and ret or 49
 ret = tonumber(reaper.GetExtState("amagalma_Smooth Scroll", "max_duration"))
 local max_duration = ret and ret or 400
 
-
-local Set = reaper.GetSet_ArrangeView2
 local total_frames = math.floor(max_duration / 31 + 0.5)
-local a0, b0 = Set( 0, 0, 0, 0, 0, 0 )
-if a0 == 0 then return end
 local total_shift = (b0 - a0) * scroll / 100
 local delta = total_shift / total_frames
 
