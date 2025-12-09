ad55dd75b352e40545f68f1950c7c557e9ac03b4
amagalma <amagalma@gmail.com>
2025-12-09 13:36:45 +0200

    Update amagalma_Set project length limit to edit cursor position.lua

--- Project Properties/amagalma_Set project length limit to edit cursor position.lua
+++ Project Properties/amagalma_Set project length limit to edit cursor position.lua
@@ -1,12 +1,16 @@
 -- @description Set project length limit to edit cursor position
 -- @author amagalma
--- @version 1.01
--- @changelog - enable project limit, if not already enabled
+-- @version 1.02
+-- @changelog - if mouse cursor hovers above timeline/ruler then set that position (snapped to grid)
 -- @about - requires SWS extensions
 
 
 if reaper.APIExists("SNM_SetDoubleConfigVar") then
-  reaper.SNM_SetDoubleConfigVar("projmaxlen", reaper.GetCursorPosition())
+  local wnd, seg, det = reaper.BR_GetMouseCursorContext()
+  local position = (wnd == "ruler" and seg == "timeline" ) and 
+                    reaper.SnapToGrid( 0, reaper.BR_GetMouseCursorContext_Position()) or
+                    reaper.GetCursorPosition()
+  reaper.SNM_SetDoubleConfigVar("projmaxlen", position)
   if reaper.SNM_GetIntConfigVar("projmaxlenuse", "-1234") ~= 1 then
     reaper.SNM_SetIntConfigVar("projmaxlenuse", 1)
   end
