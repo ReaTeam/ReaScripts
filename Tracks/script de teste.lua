8f705dd61a50719f57235aa796923b38f5de17a3
canalreaperbrasil <canalreaperbrasil@gmail.com>
2026-05-27 12:01:17 -0300

    Release Script de teste v1.0

---
+++ Tracks/script de teste.lua
@@ -0,0 +1,8 @@
+-- @description Script de teste
+-- @author Reaper Brasil
+-- @version 1.0
+-- @about Teste do script
+
+reaper.Main_OnCommand(40044, 0)
+reaper.Main_OnCommand(40029, 0)
+reaper.Main_OnCommand(1013, 0)
