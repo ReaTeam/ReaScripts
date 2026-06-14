1363297f9d5f879b1f5d3ffd23d027bc2b7bb5eb
amagalma <amagalma@gmail.com>
2026-06-14 16:03:11 +0300

    Update amagalma_Normalize all takes in selected items to match the volume of the active take.lua

--- Takes Properties/amagalma_Normalize all takes in selected items to match the volume of the active take.lua
+++ Takes Properties/amagalma_Normalize all takes in selected items to match the volume of the active take.lua
@@ -1,6 +1,8 @@
 -- @description Normalize all takes in selected items to match the volume of the active take
 -- @author amagalma
--- @version 1.00
+-- @version 1.02
+-- @changelog - Improved working with many items
+--   - Track volume and TrackFX are not taken into account
 -- @donation https://www.paypal.me/amagalma
 -- @about Useful for bringing the rest of the takes to the same volume as the current take, when comping
 
@@ -9,6 +11,7 @@ local initial_item_cnt = reaper.CountSelectedMediaItems( 0 )
 if initial_item_cnt == 0 then return end
 local initially_selected_items, init_cnt = {}, 0
 local items, cnt = {}, 0
+local tracks = {}
 local total_take_cnt = 0
 local ini = reaper.get_ini_file()
 local ok1, itemnormtgt2_old = reaper.BR_Win32_GetPrivateProfileString( "REAPER", "itemnormtgt2", "", ini )
@@ -33,58 +36,87 @@ for i = 0, initial_item_cnt-1 do
   local take_cnt = reaper.CountTakes( item )
   local take = reaper.GetActiveTake( item )
   if take_cnt > 1 and take and not reaper.TakeIsMIDI( take ) then
-    cnt = cnt + 1
-    items[cnt] = {ptr = item, val = false, acttake = take, take_cnt = take_cnt }
-    total_take_cnt = total_take_cnt + take_cnt - 1
+    local ok, guid = reaper.GetSetMediaItemInfo_String( item, "GUID", "", false )
+    if ok then
+      cnt = cnt + 1
+      local name = reaper.GetTakeName( take )
+      tracks[reaper.GetMediaItemTrack(item)] = true
+      items[guid] = {ptr = item, val = false, acttake = take, take_cnt = take_cnt, take_name = name }
+      total_take_cnt = total_take_cnt + take_cnt - 1
+    end
   end
 end
 
 if cnt > 0 then
 
   reaper.SelectAllMediaItems( 0, false )
-  for i = 1, cnt do
-    reaper.SetMediaItemSelected( items[i].ptr, true )
+  for guid, info in pairs(items) do
+    reaper.SetMediaItemSelected( info.ptr, true )
+    reaper.GetSetMediaItemTakeInfo_String( info.acttake, "P_NAME", guid, true )
+  end
+  
+  for track in pairs(tracks) do
+    local fxen = reaper.GetMediaTrackInfo_Value( track, "I_FXEN" ) == 1
+    local vol = reaper.GetMediaTrackInfo_Value( track, "D_VOL" )
+    tracks[track] = { fxen = fxen, vol = vol }
+    reaper.SetMediaTrackInfo_Value( track, "D_VOL", 1 )
+    if fxen then
+      reaper.SetMediaTrackInfo_Value( track, "I_FXEN", 0 )
+    end
   end
 
   -- Calculate loudness of selected items, including take and track FX and settings, via dry run render
   local ok, stats = reaper.GetSetProjectInfo_String(0, "RENDER_STATS_SUMMARY", "42437", false)
+  if stats == "" then
+    ok = false
+      reaper.MB("RENDER_STATS_SUMMARY is empty!", "Aborting", 0)
+  end
 
   if ok then
     local i = 0
     for entry in stats:gmatch("[^\n\r]+") do
       i = i + 1
-      items[i].val = entry:match("LUFS%-I: (.+)")
+      local guid, lufsi = entry:match("(%b{}).-LUFS%-I: (.+)")
+      items[guid].val = lufsi
     end
     if i ~= cnt then
       reaper.MB("There was a mismatch between the number of items and the calculated LUFSI values!", "Aborting", 0)
     else
       reaper.BR_Win32_WritePrivateProfileString( "REAPER", "itemnormmode", "16", ini )
       reaper.SelectAllMediaItems( 0, false )
-      for i = 1, cnt do
-        local item = items[i].ptr
-        local acttake = items[i].acttake
-        local wanted_lufsi = items[i].val
-        local take_cnt = items[i].take_cnt
-        reaper.SetMediaItemSelected( item , true )
-        for tk = 0, take_cnt-1 do
-          local take = reaper.GetTake( item, tk )
-          if take ~= acttake and wanted_lufsi then
+      for guid, info in pairs(items) do
+        reaper.SetMediaItemSelected( info.ptr , true )
+        for tk = 0, info.take_cnt-1 do
+          local take = reaper.GetTake( info.ptr, tk )
+          if take ~= info.acttake and info.val then
             reaper.SetActiveTake( take )
-            SetLUFSI( wanted_lufsi )
+            SetLUFSI( info.val )
             reaper.Main_OnCommand(42461, 0) -- Normalize items using most recent settings
           end
         end
-        reaper.SetActiveTake( acttake )
-        reaper.SetMediaItemSelected( item, false )
+        reaper.SetActiveTake( info.acttake )
+        reaper.SetMediaItemSelected( info.ptr, false )
       end
       reaper.BR_Win32_WritePrivateProfileString( "REAPER", "itemnormmode", itemnormmode_old, ini )
       SetLUFSI( itemnormtgt2_old )
+      job_done = true
     end
+  end
+  
+  for _, info in pairs(items) do
+    reaper.GetSetMediaItemTakeInfo_String( info.acttake, "P_NAME", info.take_name, true )
   end
 
   reaper.SelectAllMediaItems( 0, false )
   for i = 1, init_cnt do
     reaper.SetMediaItemSelected( initially_selected_items[i], true )
+  end
+
+  for track, info in pairs(tracks) do
+    reaper.SetMediaTrackInfo_Value( track, "D_VOL", info.vol )
+    if fxen then
+      reaper.SetMediaTrackInfo_Value( track, "I_FXEN", 1 )
+    end
   end
 
 end
@@ -95,6 +127,6 @@ reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
 reaper.Undo_EndBlock2( 0, "Normalize all takes to match active take volume", 4 )
 
-if process_time > 5 then
+if job_done and process_time > 5 then
   reaper.MB( string.format("Normalized %i takes in %.1f seconds.",total_take_cnt,process_time), "Info", 0 )
 end
