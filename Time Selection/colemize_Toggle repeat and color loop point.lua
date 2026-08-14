d35d62a4990b2216a21c2c971004ac1a81d89b0a
ColeMizeStudios <colemize@colemizestudios.com>
2026-08-14 11:29:42 +0700

    Release Toggle repeat and color loop point v1.0

---
+++ Time Selection/colemize_Toggle repeat and color loop point.lua
@@ -0,0 +1,10 @@
+-- @description Toggle repeat and color loop point
+-- @author Cole Mize
+-- @version 1.0
+-- @metapackage
+-- @provides [main] colemize_Toggle repeat and color loop point/Change loop color yellow and toggle repeat.lua
+-- @about
+--   #Cole Mize
+--
+--   This script will toggle Repeat and color your Loop Point so that it's more obvious when Repeat is engaged.
+
