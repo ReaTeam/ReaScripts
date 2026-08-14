ced78cf4bd3cd9ef7e8d8b8d46736f591bceab13
amagalma <amagalma@gmail.com>
2026-08-14 18:51:12 +0300

    Update amagalma_Calculate Peak to Loudness Ratio (PLR - new Crest Factor) of selected tracks (within time selection , if it exists).lua

--- Various/amagalma_Calculate Peak to Loudness Ratio (PLR - new Crest Factor) of selected tracks (within time selection , if it exists).lua
+++ Various/amagalma_Calculate Peak to Loudness Ratio (PLR - new Crest Factor) of selected tracks (within time selection , if it exists).lua
@@ -1,11 +1,27 @@
 -- @description Calculate Peak to Loudness Ratio (PLR - new Crest Factor) of selected tracks (within time selection , if it exists)
 -- @author amagalma
--- @version 1.00
+-- @version 1.03
+-- changelog
+--      - If any track receives in higher channels then the calculation is done on 1&2 only
+--      - Better handling of unnamed or silent tracks
 -- @donation https://www.paypal.me/amagalma
 
 
 local track_cnt = reaper.CountSelectedTracks( 0 )
 if track_cnt == 0 then return end
+
+local track_names, tn_cnt = {}, 0
+
+for i = 0, track_cnt - 1 do
+  local track = reaper.GetSelectedTrack( 0, i )
+  local _, name = reaper.GetSetMediaTrackInfo_String( track, "P_NAME", "", false )
+  if name == "" then
+    tn_cnt = tn_cnt + 1
+    track_names[tn_cnt] = track
+    local id = reaper.GetMediaTrackInfo_Value( track, "IP_TRACKNUMBER" )
+    reaper.GetSetMediaTrackInfo_String( track, "P_NAME", string.format("Track %i",id), true )
+  end
+end
 
 local ts_start, ts_end = reaper.GetSet_LoopTimeRange( 0, 0, 0, 0, 0 )
 local InTS = false
@@ -38,25 +54,58 @@ local function rnd(num)
   end
 end
 
+local function ReceivesOnHigherChannelsExist()
+  for i = 0, track_cnt - 1 do
+    local track = reaper.GetSelectedTrack(0, i)
+    if reaper.GetMediaTrackInfo_Value(track, "I_NCHAN") > 2 then
+      for s = 0, reaper.GetTrackNumSends( track, -1 ) - 1 do
+        if reaper.GetTrackSendInfo_Value( track, -1, s, "I_DSTCHAN" ) > 0 then
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
+reaper.atexit(function()
+  if restore then
+    reaper.SNM_SetIntConfigVar( "renderclosewhendone", original )
+  end
+  if tn_cnt ~= 0 then
+    for i = 1, tn_cnt do
+      reaper.GetSetMediaTrackInfo_String( track_names[i], "P_NAME", "", true )
+    end
+  end
+end)
+
+local stereo = not ReceivesOnHigherChannelsExist()
+
 local plr, plr_cnt = {}, 0
 
-local ok, info = reaper.GetSetProjectInfo_String( 0, "RENDER_STATS", InTS and "42439" or "42438", false )
+local ok, info = reaper.GetSetProjectInfo_String( 0, "RENDER_STATS", 
+      InTS and (stereo and "43811" or "42439") or (stereo and "43810" or "42438"), false )
 if not ok then return end
 
-for name, truepeak, lufsi in info:gmatch("FILE:([^%;]+).-TRUEPEAK:([^%;]+).-LUFSI:([^%;]+)") do
-  if name then
+for entry in info:gmatch("[^;]+") do
+  local e_name, value = entry:match("(.+):(.+)")
+  if e_name == "FILE" then
     plr_cnt = plr_cnt + 1
-    plr[plr_cnt] = {
-      trpk = rnd(truepeak),
-      lufsi = rnd(lufsi),
-      plr = rnd(tonumber(truepeak) - tonumber(lufsi)),
-      name = name,
-    }
+    plr[plr_cnt] = {name = value}
+  elseif e_name == "TRUEPEAK" then
+    plr[plr_cnt].trpk = rnd(value)
+  elseif e_name == "LUFSI" then
+    plr[plr_cnt].lufsi = rnd(value)
   end
 end
 
-if restore then
-  reaper.SNM_SetIntConfigVar( "renderclosewhendone", original )
+for i = plr_cnt, 1, -1 do
+  if plr[i].trpk and plr[i].lufsi then
+    plr[i].plr = rnd(tonumber(plr[i].trpk) - tonumber(plr[i].lufsi))
+  else
+    table.remove( plr, i )
+    plr_cnt = plr_cnt - 1
+  end
 end
 
 if plr_cnt ~= 0 then
