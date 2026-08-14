ae96600662e29918adde074a05a256dce233f061
amagalma <amagalma@gmail.com>
2026-08-14 16:58:29 +0300

    Update amagalma_Normalize selected tracks - calculate only for the time selection, if present (peakRMSLUFS)....lua

--- Tracks Properties/amagalma_Normalize selected tracks - calculate only for the time selection, if present (peakRMSLUFS)....lua
+++ Tracks Properties/amagalma_Normalize selected tracks - calculate only for the time selection, if present (peakRMSLUFS)....lua
@@ -1,17 +1,15 @@
 -- @description Normalize selected tracks - calculate only for the time selection, if present (peak/RMS/LUFS)...
 -- @author amagalma
--- @version 1.07
+-- @version 1.09
 -- @changelog
---    - Added True Peak
---    - Removed CSV export
---    - Improve ReaConsole output
+--    - Added new v7.79 stereo actions
+--    - Improved working with unnamed tracks
 -- @donation https://www.paypal.me/amagalma
 -- @about
 --   Normalizes the selected tracks' volume to hit the desired value with the desired method.
 --   If a time selection is present, then the calculations will be based only on the part of the tracks that is inside the time selection.
 --
 --   - Requires ReaImGui and SWS
-
 
 dofile(reaper.GetResourcePath() ..
        '/Scripts/ReaTeam Extensions/API/imgui.lua')
@@ -21,6 +19,8 @@ dofile(reaper.GetResourcePath() ..
 -- first time run values
 local wanted_value = -23
 local selected_method, wanted_method = 1, "LUFSI"
+local Channels = { "1 & 2 Only (Stereo)", "All channels" }
+local wanted_channels = 1
 
 local ext_state = reaper.GetExtState( "amagalma_NormalizeTracks", "last settings" )
 if ext_state ~= "" then
@@ -28,10 +28,9 @@ if ext_state ~= "" then
   selected_method = tonumber(selected_method)
 end
 
-
 -------------------------------------------------------------------------------------
 
-
+local max_name = 0
 local function Normalize()
   reaper.PreventUIRefresh( 1 )
   local _, project_filename = reaper.EnumProjects( -1 )
@@ -43,18 +42,24 @@ local function Normalize()
     ({reaper.BR_Win32_GetPrivateProfileString("reaper", "defrenderpath", "", reaper.get_ini_file())})[2] .. sep
   end
 
-
   -- Calculate loudness of selected tracks within time selection via dry run render
   local track_cnt = reaper.CountSelectedTracks( 0 )
   if track_cnt == 0 then return reaper.defer(function() end) end
 
   local tracks = {}
+  local tracks_with_change = {}
 
   -- Set vol to 0dB to avoid miscalculations for tracks with -inf volume
   for i = 0, track_cnt-1 do
     local track = reaper.GetSelectedTrack(0, i )
     local vol = reaper.GetMediaTrackInfo_Value( track, "D_VOL" )
-    tracks[i+1] = {ptr = track, prev_vol = vol}
+    local _, name = reaper.GetTrackName( track )
+    local _, orig_name = reaper.GetSetMediaTrackInfo_String( track, "P_NAME", "", false )
+    if #name > max_name then max_name = #name end
+    local guid = reaper.GetTrackGUID( track )
+    local id = math.tointeger(reaper.GetMediaTrackInfo_Value( track, "IP_TRACKNUMBER" ))
+    tracks[guid] = {ptr = track, prev_vol = vol, name = name, id = id, orig_name = orig_name}
+    reaper.GetSetMediaTrackInfo_String( track, "P_NAME", guid, true )
     reaper.SetMediaTrackInfo_Value( track, "D_VOL", 1 )
   end
   
@@ -78,21 +83,24 @@ local function Normalize()
   -- Set new render settings
   reaper.SNM_SetIntConfigVar( "renderclosewhendone", new_render_settings )
 
-  local ok, result = reaper.GetSetProjectInfo_String(0, "RENDER_STATS", "42439", false)
+  local ok, result = reaper.GetSetProjectInfo_String(0, "RENDER_STATS", wanted_channels == 1 and "43811" or "42439", false)
 
   -- Restore previous render settings
   reaper.SNM_SetIntConfigVar( "renderclosewhendone", render_settings )
 
-
-  local function Restore_volumes()
-    for i = 1, track_cnt do
-      reaper.SetMediaTrackInfo_Value( tracks[i].ptr, "D_VOL", tracks[i].prev_vol )
+  local function Restore_volumes_names( volumes, names )
+    for guid, info in pairs(tracks) do
+      if volumes then
+        reaper.SetMediaTrackInfo_Value( info.ptr, "D_VOL", info.prev_vol )
+      end
+      if names then
+        reaper.GetSetMediaTrackInfo_String( info.ptr, "P_NAME", info.orig_name, true )
+      end
     end
   end
 
-
   if not ok or result == "" or result:match(":[%d%-%.]-;") == nil then
-    Restore_volumes()
+    Restore_volumes_names( true, true )
     return reaper.defer(function() end)
   end
 
@@ -103,38 +111,25 @@ local function Normalize()
     else return ceil(num * 10 - 0.5) / 10 end
   end
 
-  local cnt = 0
   local fields = {}
 
   reaper.Undo_BeginBlock()
 
+  local current_guid, current_track
   for field in result:gmatch("[^;]+") do
     local name, value = field:match("(%u+):(.+)")
     if name == "FILE" then
-      cnt = cnt + 1
-      local id_from_field = math.tointeger(reaper.GetMediaTrackInfo_Value( tracks[cnt].ptr, "IP_TRACKNUMBER" ))
-      local name_from_field = value:match(".+[\\/](.*)") or value
-      local current_track = reaper.GetSelectedTrack( 0, cnt-1 )
-      -- comparison needed to make sure that the results are NOT from a previous run
-      if ({reaper.GetSetMediaTrackInfo_String( current_track, "P_NAME", "", false )})[2] ~= name_from_field or
-         math.tointeger(reaper.GetMediaTrackInfo_Value( current_track, "IP_TRACKNUMBER" )) ~= id_from_field
-      then
-        -- mismatch : the results are from a previous run
-        mismatch = true
-        Restore_volumes()
-        reaper.Undo_EndBlock( "Failed to normalize tracks", 1)
-        return reaper.defer(function() end)
-      end
-      tracks[cnt].id = id_from_field
-      tracks[cnt].name = name_from_field
+      current_guid = value
+      current_track = tracks[current_guid].ptr
     else
       if not fields[name] then fields[name] = true end
       value = tonumber(value)
-      tracks[cnt][name] = value
+      tracks[current_guid][name] = value
       if name == wanted_method then
         local change = wanted_value - value
-        tracks[cnt].change = change
-        reaper.SetMediaTrackInfo_Value( tracks[cnt].ptr, "D_VOL", 10^((change)/20) )
+        tracks_with_change[current_guid] = true
+        tracks[current_guid].change = change
+        reaper.SetMediaTrackInfo_Value( current_track, "D_VOL", 10^((change)/20) )
       end
     end
   end
@@ -153,11 +148,6 @@ local function Normalize()
 
 
   -- Show info
-  local max_name = 0
-  for i = 1, cnt do
-    local char_num = #tracks[i].name
-    if char_num > max_name then max_name = char_num end
-  end
   max_name = max_name + 3
   
   local function Sp( str ) -- add spaces
@@ -170,32 +160,51 @@ local function Normalize()
     fields_with_space[i] = Sp(Fields[i])
   end
   
-  local info = {[1] = Sp("Track") .. Sp("Name") .. table.concat(fields_with_space)}
-  for i = 1, cnt do
-    local t = {[1] = Sp(tracks[i].id), [2] = Sp(tracks[i].name)}
+  local info = {[1] = "0"}
+  local i = 1
+  for guid in pairs(tracks) do
+    i = i + 1
+    local t = {[1] = Sp(tracks[guid].id), [2] = Sp(tracks[guid].name)}
     for f = 1, Fields_cnt do
       if Fields[f] == "LRA" then
-        t[#t+1] = Sp(tracks[i].LRA)
-      elseif tracks[i][Fields[f]] and tracks[i].change then
-        t[#t+1] = Sp(round(tracks[i][Fields[f]] + tracks[i].change))
+        t[#t+1] = Sp(tracks[guid].LRA)
+      elseif tracks[guid][Fields[f]] and tracks[guid].change then
+        t[#t+1] = Sp(round(tracks[guid][Fields[f]] + tracks[guid].change))
       else
         t[#t+1] = Sp("-")
       end
     end
-    info[i+1] = table.concat(t)
+    info[i] = table.concat(t)
   end
 
   if #info ~= 1 then
     reaper.Main_OnCommand(42663, 0) -- Show ReaScript console
     reaper.ClearConsole()
+    table.sort( info, function(a,b) 
+      local A = a:match("^%d+")
+      local B = b:match("^%d+")
+      if A and B then
+        return A < B
+      else
+        return b > a
+      end
+    end)
+    info[1] = Sp("Track") .. Sp("Name") .. table.concat(fields_with_space)
     info = table.concat(info, "\n")
     reaper.ShowConsoleMsg( info )
+  end
+  
+  Restore_volumes_names( false, true )
+  
+  for guid, info in pairs(tracks) do
+    if not tracks_with_change[guid] then
+      reaper.SetMediaTrackInfo_Value( info.ptr, "D_VOL", info.prev_vol )
+    end
   end
 
   reaper.PreventUIRefresh( -1 )
   reaper.Undo_EndBlock( "Set selected tracks to " .. wanted_value .. "dB " .. wanted_method, 1)
 end
-
 
 -- GUI --------------------------------------------------------------------------------
 
@@ -223,7 +232,6 @@ local scr_center_x, scr_center_y = reaper.ImGui_Viewport_GetWorkCenter(reaper.Im
 reaper.ImGui_SetNextWindowPos(ctx, scr_center_x, scr_center_y, nil, 0.5, 0.5)
 
 
-
 local function loop()
   reaper.ImGui_PushFont(ctx, font)
   local visible, open = reaper.ImGui_Begin(ctx, 'Normalize Selected Tracks', true, WhatToDo_flags)
@@ -233,13 +241,11 @@ local function loop()
 
     reaper.ImGui_Spacing( ctx )
 
-
     reaper.ImGui_AlignTextToFramePadding( ctx )
     reaper.ImGui_Text(ctx, 'Normalize to:')
+    local txt_w = reaper.ImGui_CalcTextSize( ctx, "Use Channels:    " )
 
-
-    reaper.ImGui_SameLine( ctx )
-
+    reaper.ImGui_SameLine( ctx, txt_w )
 
     reaper.ImGui_PushItemWidth(ctx, 100)
     if reaper.ImGui_BeginCombo(ctx, '##Methods', Methods[selected_method][1], NoArrowButton) then
@@ -276,9 +282,35 @@ local function loop()
     end
     reaper.ImGui_PopItemWidth(ctx)
 
-
+    reaper.ImGui_Text(ctx, 'Use Channels:')
+    reaper.ImGui_SameLine( ctx, txt_w )
+    
+    reaper.ImGui_PushItemWidth(ctx, 158)
+    if reaper.ImGui_BeginCombo(ctx, '##Channels', Channels[wanted_channels], NoArrowButton) then
+      for i = 1, 2 do
+        local is_selected = wanted_channels == i
+        if reaper.ImGui_Selectable(ctx, Channels[i], is_selected) then
+          wanted_channels = i
+        end
+        if is_selected then
+          reaper.ImGui_SetItemDefaultFocus(ctx)
+        end
+      end
+      reaper.ImGui_EndCombo(ctx)
+    end
+    reaper.ImGui_PopItemWidth(ctx)
+    
+    if reaper.ImGui_IsItemHovered(ctx) then
+      if wheel > 0 then
+        wanted_channels = wanted_channels-1
+        if wanted_channels < 1 then wanted_channels = 1 end
+      elseif wheel < 0 then
+        wanted_channels = wanted_channels + 1
+        if wanted_channels > 2 then wanted_channels = 2 end
+      end
+    end
+    
     reaper.ImGui_Spacing( ctx )
-
 
     reaper.ImGui_PushFont(ctx, font2)
     reaper.ImGui_PushTextWrapPos(ctx, 300)
@@ -289,7 +321,6 @@ local function loop()
 
     reaper.ImGui_Spacing( ctx ) ; reaper.ImGui_Spacing( ctx )
 
-
     reaper.ImGui_SetCursorPosX( ctx, 30 )
     if reaper.ImGui_Button( ctx, "OK", 100 ) then
       wanted_value = tonumber(wanted_value)
@@ -298,9 +329,7 @@ local function loop()
       run_action = true
     end
 
-
     reaper.ImGui_SameLine( ctx, 130, 29 )
-
 
     if reaper.ImGui_Button( ctx, "Cancel", 100 ) then
       open = false
