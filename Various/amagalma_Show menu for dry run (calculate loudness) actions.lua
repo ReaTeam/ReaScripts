ff379a73cbebde6e03e5962d9b0086c245705d8a
amagalma <amagalma@gmail.com>
2026-08-14 14:53:02 +0300

    Update amagalma_Show menu for dry run (calculate loudness) actions.lua

--- Various/amagalma_Show menu for dry run (calculate loudness) actions.lua
+++ Various/amagalma_Show menu for dry run (calculate loudness) actions.lua
@@ -1,7 +1,7 @@
 -- @description Show menu for dry run (calculate loudness) actions
 -- @author amagalma
--- @version 1.03
--- @changelog No JS_ReaScriptAPI dependency
+-- @version 1.04
+-- @changelog Added new v7.79 stereo actions
 -- @link https://forum.cockos.com/showthread.php?t=239556
 -- @screenshot https://i.ibb.co/9w5VJwZ/dry-run-menu.gif
 -- @donation https://www.paypal.me/amagalma
@@ -11,13 +11,15 @@
 local t = {
   { "#Calculate loudness via dry run render|" },
   { "Master Mix", 42440 },
-  { "Selected Tracks", 42438 },
+  { "Selected Tracks (multichannel)", 42438 },
+  { "Selected Tracks (stereo)", 43810 },
   { "Selected Tracks (mono)", 42447 },
   { "Selected Items", 42468 },
   { "Selected Items (including take/track FX and settings)|", 42437 },
   { ">Within Time Selection" },
   { "Master Mix", 42441 },
-  { "Selected Tracks", 42439 },
+  { "Selected Tracks (multichannel)", 42439 },
+  { "Selected Tracks (stereo)", 43811 },
   { "<Selected Tracks (mono)", 42448 },
 }
 
@@ -29,7 +31,7 @@ local selection = gfx.showmenu(menu)
 gfx.quit()
 
 if selection > 0 then
-  if selection > 6 then selection = selection + 1 end
+  if selection > 7 then selection = selection + 1 end
   if t[selection][2] then
     reaper.Main_OnCommand(t[selection][2], 0)
   end
