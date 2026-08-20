af9417919ad27fbe6fd5d2ea9ac81794d7547476
amagalma <amagalma@gmail.com>
2026-08-20 11:59:48 +0300

    Update amagalma_Move edit cursor to next possible bad or missing items crossfade on selected tracks.lua

--- Cursor/amagalma_Move edit cursor to next possible bad or missing items crossfade on selected tracks.lua
+++ Cursor/amagalma_Move edit cursor to next possible bad or missing items crossfade on selected tracks.lua
@@ -1,85 +1,147 @@
--- @description Move edit cursor to next possible bad or missing items crossfade on selected tracks
+-- @description Move edit cursor to next dubious or missing items crossfade on selected tracks
 -- @author amagalma
--- @version 1.06
--- @changelog Automatic crossfades should get priority over manual ones
+-- @version 2.00
+-- @changelog Complete re-write for better speed and fixed lanes support
 -- @link https://forum.cockos.com/showthread.php?t=241010
 -- @donation https://www.paypal.com/paypalme/amagalma
 -- @about
---   - Moves the edit cursor to the next possible bad or missing crossfade between two items on the selected tracks.
---   - A possible bad crossfade is considered one whose duration is not the same as the overlap between the two items.
+--   - Moves the edit cursor to the next dubious or missing crossfade between two items on the selected tracks.
+--   - Dubious crossfade is considered one whose duration is not the same as the overlap between the two items.
 --   - A crossfade is considered missing if two items are adjacent or there is a very small gap between them and they do not crossfade.
+--   - All playing fixed item lanes are taken into consideration
+--   - The two items are automatically selected
 --   - You can set inside the script the maximum gap duration between two "adjacent" items for them to be considered as requiring a crossfade. Default value is 100ms.
 
----------------------------------------------------------------
+----------------------------------------------------------------------------------
 
--- SET HERE maximum space between "adjacent" items that should be checked for crossfades (in seconds)
-local max_space = 0.1
+-- SET HERE maximum allowed gap between items to be considered as "adjacent" (in seconds)
+local max_gap = 0.1
 
----------------------------------------------------------------
+----------------------------------------------------------------------------------
 
 local track_cnt = reaper.CountSelectedTracks( 0 )
 if track_cnt == 0 then 
-  reaper.MB("Please, select a track.", "No track is selected!", 0)
+  reaper.MB("Please, select one or more tracks.", "No track is selected!", 0)
   return reaper.defer(function () end)
 end
 
+local cur_pos = reaper.GetCursorPosition()
+local bad_fade, bf_cnt = {}, 0
+local abs = math.abs
+
+----------------------------------------------------------------------------------
+
 local function eq( a, b )
-  if math.abs(a - b) < 0.00001 then return true end
-  return false
+  return abs(a - b) < 0.00001
 end
 
-local GetVal = reaper.GetMediaItemInfo_Value
+local function AddBadFade( prev_item, item )
+  bf_cnt = bf_cnt + 1
+  bad_fade[bf_cnt] = {
+  items = { [prev_item.ptr] = true, [item.ptr] = true },
+  time = item.start
+  }
+end
 
-local cur_pos = reaper.GetCursorPosition()
-local bad_fade = {}
-local bf = 0
+local function IsDubiousIntersection( prev_item, item )
+  local overlap = prev_item.ending - item.start
+  if overlap > 0.00001 then
+  -- items overlap
+    item.fadein = reaper.GetMediaItemInfo_Value( item.ptr, "D_FADEINLEN_AUTO" )
+    if item.fadein == 0 then
+      item.fadein = reaper.GetMediaItemInfo_Value( item.ptr, "D_FADEINLEN" )
+    end
+    prev_item.fadeout = reaper.GetMediaItemInfo_Value( prev_item.ptr, "D_FADEOUTLEN_AUTO" )
+    if prev_item.fadeout == 0 then
+      prev_item.fadeout = reaper.GetMediaItemInfo_Value( prev_item.ptr, "D_FADEOUTLEN" )
+    end
+    if (not eq(item.fadein, overlap)) or (not eq(prev_item.fadeout, overlap)) then
+      AddBadFade( prev_item, item )
+      return true
+    end
+  elseif abs(overlap) <= max_gap then
+  -- items are less than max_space apart
+    AddBadFade( prev_item, item )
+    return true
+  end
+end
+
+local function GetNextDubiousIntersection( track, wanted_lanes )
+  -- wanted_lanes: leave unspecified to search in all fixed lanes
+  local item_cnt = reaper.CountTrackMediaItems( track )
+  if item_cnt == 0 then return end
+  local lanes_to_search = {}
+  if type(wanted_lanes) == "number" then
+    lanes_to_search[wanted_lanes] = true
+  else
+    lanes_to_search = -1
+  end
+  local previous_item_in_lane = {}
+  for it = 0, item_cnt-1 do
+    local ptr = reaper.GetTrackMediaItem( track, it )
+    local item = {
+      ptr = ptr,
+      lane = reaper.GetMediaItemInfo_Value( ptr, "I_FIXEDLANE" ),
+      start = reaper.GetMediaItemInfo_Value( ptr, "D_POSITION" )
+    }
+    if item.start > cur_pos and previous_item_in_lane[item.lane] then
+      if IsDubiousIntersection( previous_item_in_lane[item.lane], item ) then
+        break
+      end
+    end
+    if lanes_to_search == -1 or lanes_to_search[item.lane] then
+      item.ending = item.start + reaper.GetMediaItemInfo_Value( item.ptr, "D_LENGTH" )
+      previous_item_in_lane[item.lane] = item
+    end
+  end
+end
+
+----------------------------------------------------------------------------------
 
 for tr = 0, track_cnt-1 do
   local track = reaper.GetSelectedTrack( 0, tr )
-  local item_cnt = reaper.CountTrackMediaItems( track )
-  if item_cnt > 1 then
-    for it = 0, item_cnt-2 do
-      local item = reaper.GetTrackMediaItem( track, it )
-      local item_end = GetVal( item, "D_POSITION" ) + GetVal( item, "D_LENGTH" )
-      local next_item = reaper.GetTrackMediaItem( track, it+1 )
-      local next_item_start = GetVal( next_item, "D_POSITION" )
-      local overlap = item_end - next_item_start
-      if overlap <= 0 and overlap >= -max_space then
-      -- items are less than max_space apart
-        bf = bf + 1
-        bad_fade[bf] = item_end
-      elseif overlap > 0 then
-      -- items overlap
-        local next_item_fadein = GetVal( next_item, "D_FADEINLEN_AUTO" )
-        if eq(next_item_fadein, 0) then
-          next_item_fadein = GetVal( next_item, "D_FADEINLEN" )
-        end
-        local item_fadeout = GetVal( item, "D_FADEOUTLEN_AUTO" )
-        if eq(item_fadeout, 0) then
-          item_fadeout = GetVal( item, "D_FADEOUTLEN" )
-        end
-        if (not eq(next_item_fadein, overlap)) or (not eq(item_fadeout, overlap)) then
-          bf = bf + 1
-          bad_fade[bf] = next_item_start
+  local lane_cnt = reaper.GetMediaTrackInfo_Value( track, "I_NUMFIXEDLANES" )
+  local wanted_lanes
+  if lane_cnt > 1 then
+    if reaper.GetMediaTrackInfo_Value( track, "C_ALLLANESPLAY" ) == 2 then
+      wanted_lanes = {}
+      for i = 0, lane_cnt-1 do
+        local state = reaper.GetMediaTrackInfo_Value( track, string.format("C_LANEPLAYS:%i",i) )
+        if state == 1 then
+          wanted_lanes = i
+          break
+        elseif state == 2 then
+          wanted_lanes[i] = true
         end
       end
     end
-  else
-    reaper.MB("Please, select a track with more than one item.", "Not enough items!", 0)
-    return reaper.defer(function () end)
   end
+  GetNextDubiousIntersection( track, wanted_lanes )
 end
 
--- Move edit cursor
-if #bad_fade > 0 then
-  table.sort(bad_fade)
-  for i = 1, #bad_fade do
-    if bad_fade[i] > cur_pos then
-      reaper.SetEditCurPos( bad_fade[i], true, false )
-      break
+if bf_cnt > 1 then
+  table.sort( bad_fade, function( a, b )
+    if eq( a.time, b.time ) then
+      for item in pairs(b.items) do
+        a.items[item] = true
+      end
+      return true
+    else
+      return a.time < b.time
     end
-  end
-else
-  reaper.MB("No problematic or missing crossfades.", "All OK!", 0)
+  end)
 end
-reaper.defer(function () end)
+
+if bad_fade[1] then
+  reaper.PreventUIRefresh( 1 )
+  reaper.SetEditCurPos( bad_fade[1].time, true, false )
+  reaper.SelectAllMediaItems( 0, false )
+  for item in pairs(bad_fade[1].items) do
+    reaper.SetMediaItemSelected( item, true )
+  end
+  reaper.PreventUIRefresh( -1 )
+  reaper.UpdateArrange()
+  reaper.Undo_OnStateChange( "Move to next dubious item intersection" )
+else  
+  return reaper.defer(function() end)
+end
